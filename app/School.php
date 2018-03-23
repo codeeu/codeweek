0cdd55ad129a2a62eb21acaa88e746670439fc0c
Alain Van Driessche <alainvd@gmail.com>
2018-03-23 10:37:53 +0100

    Logging on schools

--- app/School.php
+++ app/School.php
@@ -3,11 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class School extends Model
 {
+
+    use LogsActivity;
+
     protected $fillable=['name','description','location','description','country'];
 
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return "School " . $this->id . " has been {$eventName}";
+    }
+
+    protected static $logFillable = true;
 
     public function path()
     {
