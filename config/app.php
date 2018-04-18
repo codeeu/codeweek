8156e5f086b4a73099975f6723d980af2a449993
Alain Van Driessche <alainvd@gmail.com>
2018-04-18 14:40:56 +0200

    typo

--- config/app.php
+++ config/app.php
@@ -147,7 +147,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Package Service Providers...
