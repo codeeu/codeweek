f36e270440fcc53f1dcbc4561c3d5d8927d77d1d
Alain Van Driessche <alainvd@gmail.com>
2018-05-30 15:41:09 +0200

    Certificate generation

--- app/Certificate.php
+++ app/Certificate.php
@@ -4,55 +4,111 @@ namespace App;
 
 
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class Certificate
 {
 
-    protected $templateName = "template.tex";
-    protected $name_of_certificate_holder;
-    protected $filename;
+    private $templateName = "template.tex";
+
+    private $name_of_certificate_holder;
+    private $resource_path;
+    private $pdflatex;
+    private $personalized_template_name;
+    private $event;
+    private $id;
 
     public function __construct(Event $event)
     {
         $this->name_of_certificate_holder = $event->name_for_certificate;
+        $this->personalized_template_name = "resources/" . $event->id . ".tex";
+        $this->resource_path = storage_path() . "/app/public/resources";
+        $this->pdflatex = env("PDFLATEX_PATH");
+        $this->event = $event;
+        $this->id = $event->id . '-' . str_random(10);
     }
 
     public function generate()
     {
 
-        $base_template = Storage::disk('public')->get('resources/'.$this->templateName);
-        $template = str_replace('<CERTIFICATE_HOLDER_NAME>', $this->tex_escape($this->name_of_certificate_holder), $base_template);
+        $this->customize_and_save_latex();
+        $this->run_pdf_creation();
+        $s3path =  $this->copy_to_s3();
 
-        return $template;
-
-        //return $this->name_of_certificate_holder;
     }
 
 
-    private function tex_escape( $string )
+    private function tex_escape($string)
     {
         $map = array(
-            "#"=>"\\#",
-            "$"=>"\\$",
-            "%"=>"\\%",
-            "&"=>"\\&",
-            "~"=>"\\~{}",
-            "_"=>"\\_",
-            "^"=>"\\^{}",
-            "\\"=>"\\textbackslash",
-            "{"=>"\\{",
-            "}"=>"\\}",
+            "#" => "\\#",
+            "$" => "\\$",
+            "%" => "\\%",
+            "&" => "\\&",
+            "~" => "\\~{}",
+            "_" => "\\_",
+            "^" => "\\^{}",
+            "\\" => "\\textbackslash",
+            "{" => "\\{",
+            "}" => "\\}",
         );
-        return preg_replace_callback( "/([\^\%~\\\\#\$%&_\{\}])/",
-            function($matches) use($map){
-                foreach($matches as $match) {
+        return preg_replace_callback("/([\^\%~\\\\#\$%&_\{\}])/",
+            function ($matches) use ($map) {
+                foreach ($matches as $match) {
                     return ($map[$match]);
                 }
             }
-            , $string );
+            , $string);
     }
-    //open the latex template
-    //replace the text in template
+
+    /**
+     * @return string
+     * @throws \League\Flysystem\FileNotFoundException
+     */
+    protected function copy_to_s3(): string
+    {
+        $inputStream = Storage::disk('public')->getDriver()->readStream('/resources/' . $this->event->id . '.pdf');
+        $destination = Storage::disk('s3')->getDriver()->getAdapter()->getPathPrefix() . '/certificates/' . $this->id . '.pdf';
+        Storage::disk('s3')->getDriver()->putStream($destination, $inputStream);
+
+        return Storage::disk('s3')->url('certificates/' . $this->id . '.pdf');
+    }
+
+    /**
+     * @return mixed
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    protected function customize_and_save_latex()
+    {
+        //open the latex template
+        $base_template = Storage::disk('public')->get('resources/' . $this->templateName);
+
+        //replace the text in template
+        $template = str_replace('<CERTIFICATE_HOLDER_NAME>', $this->tex_escape($this->name_of_certificate_holder), $base_template);
+
+        //save it locally
+        Storage::disk('public')->put($this->personalized_template_name, $template);
+    }
+
+    protected function run_pdf_creation(): void
+    {
+//call the pdflatex command
+        $command = $this->pdflatex . " -interaction=nonstopmode -output-directory " . $this->resource_path . " " . $this->resource_path . "/" . $this->event->id . ".tex";
+
+        $cwd = $this->resource_path;
+
+        $process = new Process($command, $cwd);
+        $process->run();
+
+
+        // executes after the command finishes
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+    }
+
+
     //call the pdflatex command
 
     /*
