61d78f551e24e36cc07ec0738036878a00c839bf
Alain Van Driessche <alainvd@gmail.com>
2018-06-06 16:58:04 +0200

    Mail queues

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Event;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -10,9 +11,17 @@ class EmailController extends Controller
 {
     public function create(Event $event)
     {
-        $user = auth()->user();
-        //Mail::to('alainvd@gmail.com')->send(new \App\Mail\EventCreated($event));
-        return (new \App\Mail\EventCreated($event, $user));
+        //Get the ambassador list based on the event country
+
+        $ambassadors = User::role('ambassador')->where('country_iso', $event->country->iso)->paginate(10);
+
+
+//send emails
+        foreach ($ambassadors as $ambassador) {
+            Mail::to($ambassador->email)->queue(new \App\Mail\EventCreated($event, $ambassador));
+        }
+
+        //return (new \App\Mail\EventCreated($event, $user));
 
 
     }
--- resources/views/emails/event-created.blade.php
+++ resources/views/emails/event-created.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 Hello {{$user->firstname}}!<br/><br/>
 
-A new event {{ $event->title }} has been added to the codeweek.eu site and it needs your revision. Would you be kind enough to check if everything is ok and approve it, or otherwise request an update: PATH_TO_DO<br/>
+A new event {{ $event->title }} has been added to the codeweek.eu site and it needs your revision. Would you be kind enough to check if everything is ok and approve it, or otherwise request an update<br/>
 
 
 @component('mail::button', ['url' => env('APP_URL') . $event->path()])
