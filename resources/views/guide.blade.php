9f6ddc982d56ffcd471525cf7c3e3fdddad77ba6
Alain Van Driessche <alainvd@gmail.com>
2018-08-02 14:47:48 +0200

    Guide Page Design + CSS closes #4

--- resources/views/guide.blade.php
+++ resources/views/guide.blade.php
@@ -1,118 +1,91 @@
 @extends('layout.base')
 
 @section('content')
-    <section>
-        <div class="container">
+    <div class="container">
 
 
-            <div class="heading-block center">
-                <h1>How to organize your own event?</h1>
-                <span></span>
+        <div class="content-wrap">
+
+            <div class="container clearfix breathe">
+
+                <div class="heading-block center">
+                    <h1>Join EU Code Week and organise #codeEU events</h1>
+
+                    <div>
+                        <a href="/add" target="_blank" class="btn btn-xl mt-8">Register your event here</a>
+                    </div>
+
+
+                </div>
+
+
+
+
+                <h4><a href="https://s3-eu-west-1.amazonaws.com/codeweek-s3/docs/codeeu_toolkit.pdf"
+                       alt="#codeEU Toolkit"><i class="fa fa-download"></i> Download the complete #codeEU Toolkit in PDF</a></h4>
+                <h2>#codeEU toolkit</h2>
+                <h2>What is EU Code Week?</h2>
+                <p>EU Code Week is a grass-root movement run by volunteers who promote coding in their countries as <a href="/ambassadors">Code Week Ambassadors</a>. Anyone – schools, teachers, libraries, code clubs, businesses, public authorities – can organise a #CodeEU event and add it to the <a href="/">codeweek.eu</a> map.</p>
+                <h2>Who can organise a Code Week event?</h2>
+                <p>Anyone can organise a Code Week event. </p>
+                <ul>
+                    <li><strong>Kids/teenagers/adults</strong> can organise coding events to show others how you create with code.</li>
+                    <li><strong>Schools / after school clubs / adult evening classes</strong> can organise events for their students. Coding online or unplugged is fun and teaches computational thinking! </li>
+                    <li><strong>Teachers and librarians</strong> who code can hold coding classes, share their lessons plans, organise workshops for colleagues.</li>
+                    <li><strong>Teachers and librarians who don't code</strong> can invite parents or students to teach participants coding.</li>
+                    <li><strong>Coders</strong> can organise workshops in local schools, hack spaces or community centres and invite people to create with code.</li>
+                    <li><strong>Code clubs</strong> can organise workshops for new participants and show then how to make a game or an app with code. </li>
+                    <li><strong>Businesses and non-profit organisations</strong> can host coding workshops, lend their staff to coach at different events, organise fun coding challenges, offer sponsorship for coding events.</li>
+                </ul>
+
+                <h2>What do you need?</h2>
+                <ul>
+                    <li><strong>A group of people willing to learn.</strong> For example, your friends, kids, teenagers, adults colleagues, friends, parents or grandparents. Remember, two is a group already!</li>
+                    <li><strong>Coaches or trainers</strong> who know coding, how to teach and inspire others. The number depends on the type and size of the event. For hands-on workshops, you may need one coach per 5-8 participants, but it all depends. Participants can also help each other! For larger events, it might be a good idea to have a host that makes sure everyone has what they need and keep things running smoothly.</li>
+                    <li><strong>A place to be.</strong> Classrooms, libraries, conference rooms and various public spaces all make a great event venue. </li>
+                    <li><strong>Computers.</strong> Depending on your target group, you might ask participants to bring their own laptops, in which case don't forget to provide enough power outlets.</li>
+                    <li><strong>Internet connection</strong> – WiFi or fixed connections. Make sure your connections will be able to handle the traffic coming from your group</li>
+                    <li><strong>Coding unplugged.</strong> You actually don't need computers and internet connection to learn computational thinking. Try out some off-line coding with our <a href="http://codeweek.it/codyroby/">Cody-Roby</a> kit. </li>
+                    <li><strong>Something to work with and learn.</strong>Show participants how fun it can be to create something on their own. Check out our <a href="http://codeweek.eu/resources/">list of resources</a> and search the web for existing lesson plans, workshop programs and adjust them to your group's needs. If you have existing computer equipment at the venue, make sure they already have the necessary software installed and provide participants with instructions on how the installation can be done on their own devices.</li>
+                    <li><strong>Register participants.</strong> If you have limited spaces available, you can use tools like online forms like Wufoo, Google Forms or event pages on Facebook or Eventbrite to collect registrations. While we do favour free to attend events, you can charge a small fee to cover the costs of the event. Alternately, you can turn to local IT companies or startups for sponsorship.</li>
+                    <li><a href="/add">Pin your event</a> on the <a href="/">Code Week map</a>!</li>
+                </ul>
+
+
+
+                <h2>How to organise your event?</h2>
+                <ul>
+                    <li>The format of your coding event is up to you, but we do recommend that you include some <strong>practical, hands-on time</strong>, where participants can create on their own and, or tinker with some hardware.</li>
+                    <li>You can use whatever <strong>tools and technologies</strong> you like, but we do favour <a href="http://codeweek.eu/resources/">freely available open source material</a>.</li>
+                    <li><strong>Smile!</strong> A friendly atmosphere will break the ice and motivate learning.</li>
+                    <li><strong>Snacks and drinks</strong>. If your event is a couple of hours your participants will need a break. If you can't provide snacks and drinks, ask participants to bring and share with each other.</li>
+                    <li>Ask participants to <strong>show and present</strong> what they have created to each other at the end of your event. Makes everyone proud!</li>
+                    <li><strong>Talk about your event!</strong> Talk about your event on social media. Use #CodeEU and @CodeWeekEU. Talk to your friends, the local press, make a press release!</li>
+                    <li>Don't forget to <a href="/add">add your event</a> on the <a href="/">Code Week map</a>!</li>
+                </ul>
+
+
+
+
+                <h2>Promotion material</h2>
+                <p>Feel free to use any parts of the following press release for your own marketing:</p>
+                <ul>
+                    <li><a href="https://ec.europa.eu/digital-single-market/en/news/eu-code-week-celebrating-its-5th-birthday-7-22-october-get-ready-join-and-learn-how-create-code">EU Code Week is celebrating its 5th birthday on 7-22 October – get ready, join in and learn how to create with code!</a></li>
+                    <li><a href="https://ec.europa.eu/digital-single-market/en/news/million-coded-in-record-2016-EU-code-week">New EU Code Week record: A million coded during the 2016 edition</a></li>
+                    <li><a href="https://ec.europa.eu/digital-single-market/en/news/save-date-eu-code-week-10-18-october-2015-bringing-ideas-life-codeeu">EU Code Week: Bringing ideas to life with #codeEU</a></li>
+                </ul>
+                <p>You can also use the <a href="https://github.com/codeeu/codeeu-resources/tree/master/Logo - generic">Europe Code week logo</a>, as long as the event you are planning fits with our guidelines.</p>
+
+
+
+
+                <h2>Questions?</h2>
+                <p>If you have questions about organising and promoting your #codeEU event, get in touch with one of the <a href="/ambassadors">EU Code Week Ambassadors</a> from your country.</p>
+
+
+
             </div>
-
-
-            <h4><a href="{% static 'docs/codeeu_toolkit.pdf' %}"
-                   alt="#codeEU Toolkit"><i class="fa fa-download"></i> Download the complete #codeEU Toolkit in PDF</a>
-            </h4>
-            <h2>#codeEU guidelines</h2>
-            <p>If you have existing experience with organizing education events, you might just be interested in the
-                main guidelines to follow when planning your #codeEU event:</p>
-            <ul>
-                <li><strong>Make the event beginner friendly, accessible even to those with no previous programming
-                        experience</strong>. Don't focus on the technicalities, try to show the fun and practical aspect
-                    of whatever technology is being used.
-                </li>
-                <li><strong>The format of the event is up to you</strong>. We do however recommend to include at least
-                    practical, hands-on time, where participants can create something on their own. Even better if they
-                    can take what they've learned home to share with family and friends!
-                </li>
-                <li><strong>You can use whatever tools and technologies you're most familiar with</strong>, although we
-                    do favor freely available open source tools and frameworks.
-                </li>
-                <li>Learning something completely new can be intimidating. <strong>A smile and a friendly
-                        atmosphere</strong> can help break the ice and make those, who don't consider themselves
-                    "technical", more at ease.
-                </li>
-                <li>It's also a good idea to <strong>plan a follow up</strong>. How can your participants keep learning?
-                    Who can they turn to if they have more questions?
-                </li>
-            </ul>
-            <p>Never organized an event before? Keep reading, it's not as difficult as you might think!</p>
-            <h2>What you need for a #codeEU event</h2>
-            <p>Here's what you'll need if you want to organize an event:</p>
-            <ol>
-                <li><strong>A group of people willing to learn.</strong> For example, your students, coworkers, friends,
-                    or a specific group you want to help. Remember, two is a group already!
-                </li>
-                <li><strong>One or more teachers or facilitators.</strong> The number depends on the type and size of
-                    the event. For hands-on workshops, it can be a good idea to form smaller groups that can work with
-                    their own facilitators. Code Week events are usually targeted at beginners, so teachers/facilitators
-                    don't have to be professional programmers. It's more important to have a passion for sharing
-                    knowledge, the patience to answer questions and the empathy to understand a beginner's perspective.
-                    For larger events, it might also be a good idea to have a host that makes sure everyone has what
-                    they need and keep things running smoothly.
-                </li>
-                <li><strong>A place to be at.</strong> Classrooms, conference rooms and various public spaces can all
-                    make a great event venue with some preparation. When determining the amount of people a venue will
-                    accommodate, keep in mind that the room will get noisy and the air bad with during a hands on
-                    workshop, so don't try to stuff too many people in a windowless room because nobody enjoys coding
-                    with a headache.
-                </li>
-                <li><strong>Computers with broadband internet connection.</strong> Depending on your target group, you
-                    might ask participants to bring their own laptops, in which case don't forget to provide enough
-                    power outlets. If you have existing computer equipment at the venue, make sure they already have the
-                    necessary software installed and provide participants with instructions on how the installation can
-                    be done on their own devices. When planning internet access, keep in mind that participants will
-                    probably also want to Google or StackOverflow things and share the event through social media on
-                    mobile devices, so make sure your WiFi can accommodate those extra devices.
-                </li>
-                <li>And finally, <strong>something to work with/learn</strong>. Depending on your focus group, try to
-                    find topics that are age appropriate and present them in a way that's relevant to your participant's
-                    interests. Kids might enjoy making games with Scratch, teenagers making a dating app with Rails,
-                    college students using Python for data collection/analysis, adults learning the basics of HTML/CSS
-                    for a online CV or setting up their blog. You can't create programmers in a day or week, but you can
-                    show your participants how fun it can be to create something on your own. When choosing a topic,
-                    don't start with the tools you want to use, but rather focus on the outcomes you wish to achieve.
-                    Search the web for existing lesson plans, workshop programs and adjust them to your group's needs.
-                </li>
-            </ol>
-            w
-            <p><strong>Bonus hint:</strong> chocolate and snacks keep people happy, especially during longer events.</p>
-            <h2>Promotion materials</h2>
-            <p>Unless you're organizing an event for a closed group of students, coworkers or friends, you will need to
-                do some promotion to attract participants. Social media is a good promotional tool, and you can also get
-                in touch with local media outlets. Feel free to use any parts of the following press releases for that
-                purpose:</p>
-            <ul>
-                <li>
-                    <a href="https://ec.europa.eu/digital-single-market/en/news/code-week-eu-2016-skill-digital-world-codeeu">Code
-                        Week EU 2016 – Skill up for the digital world with #codeEU!</a></li>
-                <li><a href="https://youtu.be/809D1BKVdIg">Video : EU Code Week 2016 - Which project you are most proud
-                        of?</a></li>
-                <li>European Commission Press release in 22 languages: <a
-                            href="http://europa.eu/rapid/press-release_IP-14-652_en.htm">Save the date: EU Code Week
-                        11-17 October 2014. Bring your ideas to life with #coding</a></li>
-                <li>
-                    <a href="https://github.com/codeeu/codeeu-resources/blob/master/Europe_Code_Week_2014-press_release_ENG.md">Europe
-                        Code Week Returns from October 11 to 17, 2014</a></li>
-                <li><a href="https://www.youtube.com/watch?v=TNwE3FA4pdI">EU Code Week Video: Coding is fun!</a></li>
-            </ul>
-            <p>You can also use the Europe Code Week logo and modify the following resources for your event, as long as
-                the event you're planning fits in with our guidelines.</p>
-            <ul>
-                <li><a href="https://github.com/codeeu/codeeu-resources">Europe Code Week Resources on GitHub</a></li>
-            </ul>
-            <p>If you have limited spaces available, you can use tools like online forms like Wufoo, Google Forms or
-                event pages on Facebook or Eventbrite to collect registrations. While we do favor free to attend events,
-                you can charge a small fee to cover the costs of the event. Alternately, you can turn to local IT
-                companies or startups for sponsorship.</p>
-            <h2>Questions?</h2>
-            <p>If you have additional questions about organizing and promoting your #codeEU event, you can get in touch
-                with one of <a href="{{url('ambassadors')}}">EU Code Week Ambassadors</a> from your country or send us
-                an email at <a href="mailto:info@codeweek.eu">info@codeweek.eu</a>.</p>
-
         </div>
-
-    </section>
+    </div>
 
 @endsection
