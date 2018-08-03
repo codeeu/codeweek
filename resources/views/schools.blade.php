2bfb5d70dbd284ca9239a8d9126e120bbde67de8
Alain Van Driessche <alainvd@gmail.com>
2018-08-03 08:35:34 +0200

    Schools Mockup

--- resources/views/schools.blade.php
+++ resources/views/schools.blade.php
@@ -9,14 +9,14 @@
         <div class="container">
 
 
-            <div class="content-wrap nopadding">
+            <div class="content-wrap breathe">
 
 
-                <div class="container clearfix ">
+                <div class="container clearfix">
 
 
                     <div class="flex flex-col justify-center text-center w-full mb-8 uppercase">
-                        <h1>Schools Page Coming Soon</h1>
+                        <h1>Schools: bring Code Week to your students</h1>
                         <span></span>
                     </div>
 
@@ -24,9 +24,38 @@
                 </div>
 
 
+                <h2><span>Why should you bring coding to your classroom?</span> How can coding benefit your students? What is in it for you as a teacher ?</h2>
+
+
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod eleifend tortor, non vestibulum enim placerat ut. Morbi sed tincidunt eros, ac aliquet arcu. Quisque ut magna quis velit finibus cursus blandit sed ipsum. Suspendisse tincidunt, justo eget sodales euismod, eros elit hendrerit ipsum, et sagittis libero felis et sem. Nulla et urna facilisis, condimentum magna non, semper dolor. Nam scelerisque tempus tincidunt. In non dui imperdiet, tempus risus molestie, elementum dui. Aliquam at enim dapibus, mollis sapien in, interdum tellus. Sed maximus nibh at ultrices gravida. Etiam gravida gravida risus. Vivamus tempor iaculis bibendum. Mauris consequat eu mi at suscipit. Maecenas fringilla mi sit amet ante convallis, vitae efficitur nisi lacinia.</p>
+
+                <p>Phasellus quis suscipit sapien. Phasellus sed enim ut neque pellentesque congue at ut tortor. Nulla luctus sapien ut convallis semper. Donec sagittis mauris a elit eleifend dapibus. Pellentesque id aliquet tortor, at faucibus enim. Curabitur blandit libero laoreet risus posuere feugiat. Vivamus tincidunt interdum rutrum. Nunc placerat fringilla orci eget elementum. Morbi placerat ipsum a tortor malesuada, sed suscipit augue fringilla. Pellentesque et tincidunt risus, id placerat urna. In neque nulla, maximus eu neque non, semper malesuada tellus.</p>
+
+
+
+                <h2><span>Ready to get involved ?</span></h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod eleifend tortor, non vestibulum enim placerat ut. Morbi sed tincidunt eros, ac aliquet arcu. Quisque ut magna quis velit finibus cursus blandit sed ipsum. Suspendisse tincidunt, justo eget sodales euismod, eros elit hendrerit ipsum, et sagittis libero felis et sem. Nulla et urna facilisis, condimentum magna non, semper dolor. Nam scelerisque tempus tincidunt. In non dui imperdiet, tempus risus molestie, elementum dui. Aliquam at enim dapibus, mollis sapien in, interdum tellus. Sed maximus nibh at ultrices gravida. Etiam gravida gravida risus. Vivamus tempor iaculis bibendum. Mauris consequat eu mi at suscipit. Maecenas fringilla mi sit amet ante convallis, vitae efficitur nisi lacinia.</p>
+
+
+                <h2><span>New to Coding? No worries</span></h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod eleifend tortor, non vestibulum enim placerat ut. Morbi sed tincidunt eros, ac aliquet arcu. Quisque ut magna quis velit finibus cursus blandit sed ipsum. Suspendisse tincidunt, justo eget sodales euismod, eros elit hendrerit ipsum, et sagittis libero felis et sem. Nulla et urna facilisis, condimentum magna non, semper dolor. Nam scelerisque tempus tincidunt. In non dui imperdiet, tempus risus molestie, elementum dui. Aliquam at enim dapibus, mollis sapien in, interdum tellus. Sed maximus nibh at ultrices gravida. Etiam gravida gravida risus. Vivamus tempor iaculis bibendum. Mauris consequat eu mi at suscipit. Maecenas fringilla mi sit amet ante convallis, vitae efficitur nisi lacinia.</p>
+
+
+
+
+                <h2><span>Looking for an extra challenge ?</span></h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod eleifend tortor, non vestibulum enim placerat ut. Morbi sed tincidunt eros, ac aliquet arcu. Quisque ut magna quis velit finibus cursus blandit sed ipsum. Suspendisse tincidunt, justo eget sodales euismod, eros elit hendrerit ipsum, et sagittis libero felis et sem. Nulla et urna facilisis, condimentum magna non, semper dolor. Nam scelerisque tempus tincidunt. In non dui imperdiet, tempus risus molestie, elementum dui. Aliquam at enim dapibus, mollis sapien in, interdum tellus. Sed maximus nibh at ultrices gravida. Etiam gravida gravida risus. Vivamus tempor iaculis bibendum. Mauris consequat eu mi at suscipit. Maecenas fringilla mi sit amet ante convallis, vitae efficitur nisi lacinia.</p>
+
+
+
+
+
+
             </div>
         </div>
-
     </section>
 
 
