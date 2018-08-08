8d197dffb6958de69a4adae955ba145b4d63bc78
Alain Van Driessche <alainvd@gmail.com>
2018-08-08 14:18:54 +0200

    Trusted Proxy for Load Balancer

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
+    protected $proxies = ['10.0.1.186'];
 
     /**
      * The headers that should be used to detect proxies.
