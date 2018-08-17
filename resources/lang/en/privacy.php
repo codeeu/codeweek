497f622df34adc7ab1910293e31daebf6778af51
NET1\perezom <omar@prettyhugedegree.net>
2018-08-17 11:23:44 +0200

    Change log: Added keys to privacy view  and files for multilingual support.

---
+++ resources/lang/en/privacy.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+   'page_title' => 'Privacy policy',
+   'page_subtitle' => 'EU Code Week 2018',
+   'codeweek_link' => 'Home',
+   'paragraph_1' => [
+    'body_1' => 'This Privacy Policy governs the manner in which Europe Code Week collects, uses, maintains and discloses information collected from users (each, a "User") of the ',
+    'body_2' => 'website ("Site"). This privacy policy applies to the Site and all products and services offered by Europe Code Week.'
+   ],
+   'paragraph_2' => [
+       'title' => 'Personal identification information',
+       'body' => 'We may collect personal identification information from Users in a variety of ways, including, but not limited to, when Users visit our site, register on the site, and in connection with other activities, services, features or resources we make available on our Site. Users may be asked for, as appropriate, name, email address. Users may, however, visit our Site anonymously. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.'
+   ], 
+   'paragraph_3' => [
+       'title' => 'Non-personal identification information',
+       'body' => 'We may collect non-personal identification information about Users whenever they interact with our Site. Non-personal identification information may include the browser name, the type of computer and technical information about Users means of connection to our Site, such as the operating system and the Internet service providers utilized and other similar information.'
+   ],
+   'paragraph_4' => [
+    'title' => 'Web browser cookies',
+    'body' => "Our Site may use \"cookies\" to enhance User experience. User's web browser places cookies on their hard drive for record-keeping purposes and sometimes to track information about them. User may choose to set their web browser to refuse cookies, or to alert you when cookies are being sent. If they do so, note that some parts of the Site may not function properly."
+  ],
+  'paragraph_5' => [
+      'title' => 'How we use collected information',
+      'body' => 'Europe Code Week may collect and use Users personal information for the following purposes:',
+      'italics_item_1' => 'To improve customer service: ',
+      'list_item_1' => 'Information you provide helps us respond to your customer service requests and support needs more efficiently.',
+      'italics_item_2' => 'To personalize user experience: ',
+      'list_item_2' => 'We may use information in the aggregate to understand how our Users as a group use the services and resources provided on our Site.',
+      'italics_item_3' => 'To send periodic emails: ',
+      'list_item_3' => 'We may use the email address to respond to their inquiries, questions, and/or other requests. '
+  ],
+  'paragraph_6' => [
+      'title' => 'How we protect your information',
+      'body' => 'We adopt appropriate data collection, storage and processing practices and security measures to protect against unauthorized access, alteration, disclosure or destruction of your personal information, username, password, transaction information and data stored on our Site.'
+  ],
+  'paragraph_7' => [
+      'title' => 'Sharing your personal information',
+      'body' => 'We do not sell, trade, or rent Users personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates and advertisers for the purposes outlined above.We may use third party service providers to help us operate our business and the Site or administer activities on our behalf, such as sending out newsletters or surveys. We may share your information with these third parties for those limited purposes provided that you have given us your permission.'
+  ],
+  'paragraph_8' => [
+    'title' => 'Changes to this privacy policy',
+    'body' => 'Europe Code Week has the discretion to update this privacy policy at any time. When we do, we will revise the updated date at the bottom of this page. We encourage Users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this privacy policy periodically and become aware of modifications.'
+  ],
+  'paragraph_9' => [
+    'title' => 'Your acceptance of these terms',
+    'body' => 'By using this Site, you signify your acceptance of this policy. If you do not agree to this policy, please do not use our Site. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes.'
+  ],
+  'paragraph_10' => [
+      'title' => 'Contacting us',
+      'body' => 'If you have any questions about this Privacy Policy, the practices of this site, or your dealings with this site, please contact us at:',
+  ],
+  'last_updated' => 'This document was last updated on October 12, 2014',
+  'privacy_generated_by' => 'Privacy policy created by ',
+  'privacy_generated_link' => 'Generate Privacy Policy'
+];
