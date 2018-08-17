7ab011f95142b0c3e372245c1fccb7658667ee6e
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2018-08-17 09:50:24 +0200

    Translate the pages, add key for each string #3

--- resources/views/guide.blade.php
+++ resources/views/guide.blade.php
@@ -15,10 +15,9 @@
 
 
                     <div class="flex flex-col justify-center text-center w-full mb-8 uppercase">
-                        <h1>Join EU Code Week and organise #codeEU events</h1>
-                        <span></span>
+                        <h1>@lang('guide.title')</h1>
                         <div>
-                            <a href="/add" target="_blank" class="btn btn-xl mt-8">Register your event here</a>
+                            <a href="/add" target="_blank" class="btn btn-xl mt-8">@lang('guide.register_button')</a>
                         </div>
                     </div>
 
@@ -26,127 +25,148 @@
                 </div>
 
 
-                <h4><a href="https://s3-eu-west-1.amazonaws.com/codeweek-s3/docs/codeeu_toolkit.pdf"
-                       alt="#codeEU Toolkit"><i class="fa fa-download"></i> Download the complete #codeEU Toolkit in PDF</a>
+                <h4>
+                    <a href="https://s3-eu-west-1.amazonaws.com/codeweek-s3/docs/codeeu_toolkit.pdf" alt="#codeEU Toolkit">
+                        <i class="fa fa-download"></i>
+                        @lang('guide.download_guide_button')
+                    </a>
                 </h4>
-                <h2>#codeEU toolkit</h2>
-                <h2>What is EU Code Week?</h2>
-                <p>EU Code Week is a grass-root movement run by volunteers who promote coding in their countries as <a
-                            href="/ambassadors">Code Week Ambassadors</a>. Anyone – schools, teachers, libraries, code
-                    clubs, businesses, public authorities – can organise a #CodeEU event and add it to the <a href="/">codeweek.eu</a>
-                    map.</p>
-                <h2>Who can organise a Code Week event?</h2>
-                <p>Anyone can organise a Code Week event. </p>
+                <h2>@lang('guide.code_eu_toolkit')</h2>
+
+                <h2>@lang('guide.what.title')</h2>
+                <p>
+                    @lang('guide.what.paragraphs.1-1')
+                    <a href="/ambassadors">@lang('guide.what.paragraphs.1-2')</a>.
+                    @lang('guide.what.paragraphs.1-3')
+                    <a href="/">codeweek.eu</a>
+                    @lang('guide.what.paragraphs.1-4')
+                </p>
+
+                <h2>@lang('guide.who.title')</h2>
+                <p>@lang('guide.who.content')</p>
                 <ul>
-                    <li><strong>Kids/teenagers/adults</strong> can organise coding events to show others how you create
-                        with code.
-                    </li>
-                    <li><strong>Schools / after school clubs / adult evening classes</strong> can organise events for
-                        their students. Coding online or unplugged is fun and teaches computational thinking!
-                    </li>
-                    <li><strong>Teachers and librarians</strong> who code can hold coding classes, share their lessons
-                        plans, organise workshops for colleagues.
-                    </li>
-                    <li><strong>Teachers and librarians who don't code</strong> can invite parents or students to teach
-                        participants coding.
-                    </li>
-                    <li><strong>Coders</strong> can organise workshops in local schools, hack spaces or community
-                        centres and invite people to create with code.
-                    </li>
-                    <li><strong>Code clubs</strong> can organise workshops for new participants and show then how to
-                        make a game or an app with code.
-                    </li>
-                    <li><strong>Businesses and non-profit organisations</strong> can host coding workshops, lend their
-                        staff to coach at different events, organise fun coding challenges, offer sponsorship for coding
-                        events.
-                    </li>
+                    <li><strong>@lang('guide.who.points.1-1')</strong> @lang('guide.who.points.1-2')</li>
+                    <li><strong>@lang('guide.who.points.2-1')</strong> @lang('guide.who.points.2-2')</li>
+                    <li><strong>@lang('guide.who.points.3-1')</strong> @lang('guide.who.points.3-2')</li>
+                    <li><strong>@lang('guide.who.points.4-1')</strong> @lang('guide.who.points.4-2')</li>
+                    <li><strong>@lang('guide.who.points.5-1')</strong> @lang('guide.who.points.5-2')</li>
+                    <li><strong>@lang('guide.who.points.6-1')</strong> @lang('guide.who.points.6-2')</li>
+                    <li><strong>@lang('guide.who.points.7-1')</strong> @lang('guide.who.points.7-2')</li>
                 </ul>
 
-                <h2>What do you need?</h2>
-                <ul>
-                    <li><strong>A group of people willing to learn.</strong> For example, your friends, kids, teenagers,
-                        adults colleagues, friends, parents or grandparents. Remember, two is a group already!
-                    </li>
-                    <li><strong>Coaches or trainers</strong> who know coding, how to teach and inspire others. The
-                        number depends on the type and size of the event. For hands-on workshops, you may need one coach
-                        per 5-8 participants, but it all depends. Participants can also help each other! For larger
-                        events, it might be a good idea to have a host that makes sure everyone has what they need and
-                        keep things running smoothly.
-                    </li>
-                    <li><strong>A place to be.</strong> Classrooms, libraries, conference rooms and various public
-                        spaces all make a great event venue.
-                    </li>
-                    <li><strong>Computers.</strong> Depending on your target group, you might ask participants to bring
-                        their own laptops, in which case don't forget to provide enough power outlets.
-                    </li>
-                    <li><strong>Internet connection</strong> – WiFi or fixed connections. Make sure your connections
-                        will be able to handle the traffic coming from your group
-                    </li>
-                    <li><strong>Coding unplugged.</strong> You actually don't need computers and internet connection to
-                        learn computational thinking. Try out some off-line coding with our <a
-                                href="http://codeweek.it/codyroby/">Cody-Roby</a> kit.
-                    </li>
-                    <li><strong>Something to work with and learn.</strong>Show participants how fun it can be to create
-                        something on their own. Check out our <a href="http://codeweek.eu/resources/">list of
-                            resources</a> and search the web for existing lesson plans, workshop programs and adjust
-                        them to your group's needs. If you have existing computer equipment at the venue, make sure they
-                        already have the necessary software installed and provide participants with instructions on how
-                        the installation can be done on their own devices.
-                    </li>
-                    <li><strong>Register participants.</strong> If you have limited spaces available, you can use tools
-                        like online forms like Wufoo, Google Forms or event pages on Facebook or Eventbrite to collect
-                        registrations. While we do favour free to attend events, you can charge a small fee to cover the
-                        costs of the event. Alternately, you can turn to local IT companies or startups for sponsorship.
-                    </li>
-                    <li><a href="/add">Pin your event</a> on the <a href="/">Code Week map</a>!</li>
-                </ul>
-
-
-                <h2>How to organise your event?</h2>
-                <ul>
-                    <li>The format of your coding event is up to you, but we do recommend that you include some <strong>practical,
-                            hands-on time</strong>, where participants can create on their own and, or tinker with some
-                        hardware.
-                    </li>
-                    <li>You can use whatever <strong>tools and technologies</strong> you like, but we do favour <a
-                                href="http://codeweek.eu/resources/">freely available open source material</a>.
-                    </li>
-                    <li><strong>Smile!</strong> A friendly atmosphere will break the ice and motivate learning.</li>
-                    <li><strong>Snacks and drinks</strong>. If your event is a couple of hours your participants will
-                        need a break. If you can't provide snacks and drinks, ask participants to bring and share with
-                        each other.
-                    </li>
-                    <li>Ask participants to <strong>show and present</strong> what they have created to each other at
-                        the end of your event. Makes everyone proud!
-                    </li>
-                    <li><strong>Talk about your event!</strong> Talk about your event on social media. Use #CodeEU and
-                        @CodeWeekEU. Talk to your friends, the local press, make a press release!
-                    </li>
-                    <li>Don't forget to <a href="/add">add your event</a> on the <a href="/">Code Week map</a>!</li>
-                </ul>
-
-
-                <h2>Promotion material</h2>
-                <p>Feel free to use any parts of the following press release for your own marketing:</p>
+                <h2>@lang('guide.what_do_you_need.title')</h2>
                 <ul>
                     <li>
-                        <a href="https://ec.europa.eu/digital-single-market/en/news/eu-code-week-celebrating-its-5th-birthday-7-22-october-get-ready-join-and-learn-how-create-code">EU
-                            Code Week is celebrating its 5th birthday on 7-22 October – get ready, join in and learn how
-                            to create with code!</a></li>
+                        <strong>@lang('guide.what_do_you_need.points.1-1')</strong>
+                        @lang('guide.what_do_you_need.points.1-2')
+                    </li>
                     <li>
-                        <a href="https://ec.europa.eu/digital-single-market/en/news/million-coded-in-record-2016-EU-code-week">New
-                            EU Code Week record: A million coded during the 2016 edition</a></li>
+                        <strong>@lang('guide.what_do_you_need.points.2-1')</strong>
+                        @lang('guide.what_do_you_need.points.2-2')
+                    </li>
                     <li>
-                        <a href="https://ec.europa.eu/digital-single-market/en/news/save-date-eu-code-week-10-18-october-2015-bringing-ideas-life-codeeu">EU
-                            Code Week: Bringing ideas to life with #codeEU</a></li>
+                        <strong>@lang('guide.what_do_you_need.points.3-1')</strong>
+                        @lang('guide.what_do_you_need.points.3-2')
+                    </li>
+                    <li>
+                        <strong>@lang('guide.what_do_you_need.points.4-1')</strong>
+                        @lang('guide.what_do_you_need.points.4-2')
+                    </li>
+                    <li>
+                        <strong>@lang('guide.what_do_you_need.points.5-1')</strong>
+                        @lang('guide.what_do_you_need.points.5-2')
+                    </li>
+                    <li>
+                        <strong>@lang('guide.what_do_you_need.points.6-1')</strong>
+                        @lang('guide.what_do_you_need.points.6-2')
+                        <a href="http://codeweek.it/codyroby/">Cody-Roby</a>
+                        @lang('guide.what_do_you_need.points.6-3')
+                    </li>
+                    <li>
+                        <strong>@lang('guide.what_do_you_need.points.7-1')</strong>
+                        @lang('guide.what_do_you_need.points.7-2')
+                        <a href="http://codeweek.eu/resources/">@lang('guide.what_do_you_need.points.7-3')</a>
+                        @lang('guide.what_do_you_need.points.7-4')
+                    </li>
+                    <li>
+                        <strong>@lang('guide.what_do_you_need.points.8-1')</strong>
+                        @lang('guide.what_do_you_need.points.8-2')
+                    </li>
+                    <li>
+                        <a href="/add">@lang('guide.what_do_you_need.points.9')</a>!
+                    </li>
                 </ul>
-                <p>You can also use the <a href="https://github.com/codeeu/codeeu-resources/tree/master/Logo - generic">Europe
-                        Code week logo</a>, as long as the event you are planning fits with our guidelines.</p>
 
 
-                <h2>Questions?</h2>
-                <p>If you have questions about organising and promoting your #codeEU event, get in touch with one of the
-                    <a href="/ambassadors">EU Code Week Ambassadors</a> from your country.</p>
+                <h2>@lang('guide.how.title')</h2>
+                <ul>
+                    <li>
+                        @lang('guide.how.points.1-1')
+                        <strong>@lang('guide.how.points.1-2')</strong>
+                        @lang('guide.how.points.1-3')
+                    </li>
+                    <li>
+                        @lang('guide.how.points.2-1')
+                        <strong>@lang('guide.how.points.2-2')</strong>
+                        @lang('guide.how.points.2-3')
+                        <a href="http://codeweek.eu/resources/">@lang('guide.how.points.2-4')</a>.
+                    </li>
+                    <li>
+                        <strong>@lang('guide.how.points.3-1')</strong>
+                        @lang('guide.how.points.3-2')
+                    </li>
+                    <li>
+                        <strong>@lang('guide.how.points.4-1')</strong>
+                        @lang('guide.how.points.4-2')
+                    </li>
+                    <li>
+                        @lang('guide.how.points.5-1')
+                        <strong>@lang('guide.how.points.5-2')</strong>
+                        @lang('guide.how.points.5-3')
+                    </li>
+                    <li>
+                        <strong>@lang('guide.how.points.6-1')</strong>
+                        @lang('guide.how.points.6-2')
+                    </li>
+                    <li>
+                        @lang('guide.how.points.7-1')
+                        <a href="/add">@lang('guide.how.points.7-2')</a>!
+                    </li>
+                </ul>
+
+
+                <h2>@lang('guide.promotion_material.title')</h2>
+                <p>@lang('guide.promotion_material.content1')</p>
+                <ul>
+                    <li>
+                        <a href="https://ec.europa.eu/digital-single-market/en/news/eu-code-week-celebrating-its-5th-birthday-7-22-october-get-ready-join-and-learn-how-create-code">
+                            @lang('guide.promotion_material.points.1')
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://ec.europa.eu/digital-single-market/en/news/million-coded-in-record-2016-EU-code-week">
+                            @lang('guide.promotion_material.points.2')
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://ec.europa.eu/digital-single-market/en/news/save-date-eu-code-week-10-18-october-2015-bringing-ideas-life-codeeu">
+                            @lang('guide.promotion_material.points.3')
+                        </a>
+                    </li>
+                </ul>
+                <p>
+                    @lang('guide.promotion_material.content2')
+                    <a href="https://github.com/codeeu/codeeu-resources/tree/master/Logo - generic">@lang('guide.promotion_material.content3')</a>
+                    @lang('guide.promotion_material.content4')
+                </p>
+
+
+                <h2>@lang('guide.questions.title')</h2>
+                <p>
+                    @lang('guide.questions.part1')
+                    <a href="/ambassadors">@lang('guide.questions.part2')</a>
+                    @lang('guide.questions.part3')
+                </p>
 
 
             </div>
