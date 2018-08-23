7747379273a56dd1fedbb9bba7d766c1a08c047c
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2018-08-23 09:57:30 +0200

    Translate the pages, add key for each string #3

--- resources/lang/en/base.php
+++ resources/lang/en/base.php
@@ -14,6 +14,36 @@ return [
     'languages' => [
         'en' => 'English',
         'fr' => 'French',
-    ]
+        'bg' => 'Bulgarian',
+        'mt' => 'Maltese',
+        'hr' => 'Croatian',
+        'de' => 'German',
+        'pl' => 'Polish',
+        'cs' => 'Czech',
+        'el' => 'Greek',
+        'pt' => 'Portuguese',
+        'da' => 'Danish',
+        'hu' => 'Hungarian',
+        'ro' => 'Romanian',
+        'nl' => 'Dutch',
+        'ga' => 'Irish',
+        'sk' => 'Slovak',
+        'it' => 'Italian',
+        'sl' => 'Slovenian',
+        'et' => 'Estonian',
+        'lv' => 'Latvian',
+        'es' => 'Spanish',
+        'fi' => 'Finnish',
+        'lt' => 'Lithuanian',
+        'sv' => 'Swedish'
+    ],
+
+    'resources_local_title'=>'Coding resources from',
+    'are_you_a_teacher' => 'Are you a teacher?',
+    'get_involved_banner' => 'Click here to find out how to get involved!',
+    'days'=>'Days',
+    'hours'=>'Hours',
+    'minutes'=>'Minutes',
+    'seconds'=>'Seconds'
 
 ];
