0abdb19392b99352a2a04c49836871c93f01786f
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2018-08-24 10:15:20 +0200

    Fix some layout details

--- resources/views/report/list.blade.php
+++ resources/views/report/list.blade.php
@@ -1,32 +1,30 @@
 @extends('layout.base')
 
 @section('content')
+    <section>
 
+        <div class="container">
 
-    <div class=" mt-6   flex justify-around">
-        <div class="w-4/5">
-
-            <h1>@lang('eventreports.reports_by'){{ Auth::user()->fullName }}</h1>
+            <h1 style="display:inline-block;">@lang('eventreports.reports_by'){{ Auth::user()->fullName }}</h1>
+            <p>@lang('eventreports.report')</p>
+            <hr>
 
             @if($events->isEmpty())
                 <div class="row">
                     <p>@lang('eventreports.no_reports')</p>
                 </div>
             @else
-
-
                 <div class="row">
-                    <p>@lang('eventreports.report')</p>
                     @foreach($events as $event)
                         @component('event.event_tile', ['event'=>$event])
                         @endcomponent
                     @endforeach
-
                 </div>
                 {{ $events->links() }}
             @endif
 
         </div>
-    </div>
+
+    </section>
 
 @endsection
