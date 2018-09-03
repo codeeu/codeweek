b7eeec4caea9c667c203ecb5590f0d00f6a2560d
Alain Van Driessche <alainvd@gmail.com>
2018-09-03 11:21:00 +0200

    Google Analytics

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en" class="no-js" >
 <head>
+    @include('layout.analytics')
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
