4682ecd98272c752de91b823867ad9ca5a329754
Alain Van Driessche <alainvd@gmail.com>
2018-09-03 13:43:16 +0200

    Do not show codeweek4all if not there

--- resources/views/event/thankyou.blade.php
+++ resources/views/event/thankyou.blade.php
@@ -19,7 +19,7 @@
                 <a href="mailto:info@codeweek.eu?subject=Code Week events">@lang('event.thanks_page.phrase6')</a>.</p>
 
             <p>
-                @if($event->title)
+                @if($event->codeweek_for_all_participation_code)
                     @lang('event.thanks_page.phrase7')
                     <strong>{{$event->codeweek_for_all_participation_code}}</strong>
                 @endif
