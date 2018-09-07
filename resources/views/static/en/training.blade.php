bfcc9ea11c34d69c520ba5c26f334e87578f210b
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2018-09-07 16:35:11 +0200

    First pages of training modules

--- resources/views/static/en/training.blade.php
+++ resources/views/static/en/training.blade.php
@@ -31,13 +31,43 @@
                         No previous experience on coding or programming is needed, each module takes only around 15’ to go through, and they introduce you to key concepts related to coding and computational thinking activities. Besides, the modules also give you practical tips and advices on how to integrate them in your classroom.
                     </p>
 
-                    <ul>
-                        <li><a href="/training/1">Computational thinking and problem solving</a> - By Miles Berry</li>
-                        <li><a href="/training/2">Coding without digital technology (unplugged)</a> - By Alessandro Bogliolo</li>
-                        <li><a href="/training/3">Visual programming – introduction to Scratch</a> - By Margo Tinawi</li>
-                        <li><a href="/training/4">Creating educational games with Scratch</a> - By Jesús Moreno León</li>
-                        <li><a href="/training/5">Making, robotics and tinkering in the classroom</a> - By Tullia Urschitz</li>
-                    </ul>
+                    <div class="flex flex-wrap" style="margin-bottom: 30px;">
+                        <div class="learning-card">
+                            <a href="/training/computational-thinking-and-problem-solving">
+                                <img src="img/learning_card_default.png">
+                                <div class="text-xl">Computational thinking and problem solving</div>
+                                <div class="text-base text-black">By Miles Berry</div>
+                            </a>
+                        </div>
+                        <div class="learning-card">
+                            <a href="/training/coding-without-digital-technology">
+                                <img src="img/learning_card_default.png">
+                                <div class="text-xl">Coding without digital technology (unplugged)</div>
+                                <div class="text-base text-black">By Alessandro Bogliolo</div>
+                            </a>
+                        </div>
+                        <div class="learning-card">
+                            <a href="/training/visual-programming-introduction-to-scratch">
+                                <img src="img/learning_card_default.png">
+                                <div class="text-xl">Visual programming – introduction to Scratch</div>
+                                <div class="text-base text-black">By Margo Tinawi</div>
+                            </a>
+                        </div>
+                        <div class="learning-card">
+                            <a href="/training/creating-educational-games-with-scratch">
+                                <img src="img/learning_card_default.png">
+                                <div class="text-xl">Creating educational games with Scratch</div>
+                                <div class="text-base text-black">By Jesús Moreno León</div>
+                            </a>
+                        </div>
+                        <div class="learning-card">
+                            <a href="/training/making-robotics-and-tinkering-in-the-classroom">
+                                <img src="img/learning_card_default.png">
+                                <div class="text-xl">Making, robotics and tinkering in the classroom</div>
+                                <div class="text-base text-black">By Tullia Urschitz</div>
+                            </a>
+                        </div>
+                    </div>
 
                     <p>
                         Now that you have taken one or more Code Week Learning Bits, we hope that you feel comfortable enough to bring some digital creativity to your classroom and pin your activity on the <a href="/events">Code Week Map</a>!
