1a19a82895568fec5d17278a6a095f42df8d7642
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2018-10-04 15:41:53 +0200

    Remind Ambassadors to approve and reject events #9

--- resources/views/emails/en/remind-ambassador.blade.php
+++ resources/views/emails/en/remind-ambassador.blade.php
@@ -1,14 +1,16 @@
 @component('mail::message')
-    Hello {{$user->firstname}}!<br/><br/>
+Hello {{$user->firstname}}!<br/><br/>
 
-    There are pending events that still need your attention.<br/>
+There are pending activities that still need your attention.<br/>
 
-    Can you please access in the follow url to approve or reject the events:<br/>
+Can you please review this activities in order to approve or reject<br/>
 
-    <a href="https://codeweek.eu/pending/">https://codeweek.eu/pending/</a><br/>
+@component('mail::button', ['url' => env('APP_URL') . "pending/"])
+    Review activities
+@endcomponent
 
-    Thank you for your help!<br/>
+Thank you for your help!<br/>
 
-    Codeweek.eu Team<br/>
+Codeweek.eu Team<br/>
 
 @endcomponent
