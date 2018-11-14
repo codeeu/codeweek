f2bc1d91404e80fd2957890fc95954bf6a259dc5
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2018-11-14 14:01:38 +0100

    add Makeblock logo

--- resources/views/static/sponsors.blade.php
+++ resources/views/static/sponsors.blade.php
@@ -1,34 +1,41 @@
 <ul class="clients-grid grid-4 nobottommargin clearfix">
-    <li><a href="https://www.apple.com/uk/everyone-can-code/"><img src="img/partners/apple.png"
-                                                                   alt="Apple"></a></li>
-    <li><a href="http://www.publiclibraries2020.eu"><img
-                    src="img/partners/colabora_PublicLibraries2020.png" alt="Public Libraries 2020"></a></li>
-    <li><a href="http://ec.europa.eu/digital-agenda/en/grand-coalition-digital-jobs-0"><img
-                    src="img/partners/digital-skills.png"
-                    alt="Grote coalitie van digitale banen"></a></li>
-    <li><a href="http://coderdojo.org"><img src="img/partners/colabora_coderdojo.png"
-                                            alt="CoderDojo"></a></li>
-    <li><a href="http://www.africacodeweek.org/"><img src="img/partners/colabora_africacodeweek.png"
-                                                      alt="Africa Code Week"></a></li>
-    <li><a href="http://www.allyouneediscode.eu/"><img src="img/partners/colabora_aynic.png"
-                                                       alt="All you need is code"></a></li>
-    <li><a href="http://www.eun.org/"><img src="img/partners/colabora_eun.png"
-                                           alt="European Schoolnet"></a></li>
-    <li><a href="http://scratch.mit.edu/codeweekeu"><img src="img/partners/colabora_scratch.png"
-                                                         alt="Scratch"></a></li>
-    <li><a href="https://edu.google.com/resources/computerscience"><img
-                    src="img/partners/google.png" alt="Google"></a></li>
-
-    <li><a href="https://mooc.uniurb.it/wp/codeweekmooc/"><img
-                    src="img/partners/urbinocarlo.png" alt="CodeWeek in Your Classroom, Now!"></a></li>
-
-    <li><a href="https://facebook.com/"><img
-                    src="img/partners/facebook.png" alt="Facebook"></a></li>
-
-    <li><a href="https://education.ti.com/en/select-country/europe"><img
-                    src="img/partners/texas-instruments.png" alt="Texas Instruments"></a></li>
-
-
-
-
+    <li>
+        <a href="https://www.apple.com/uk/everyone-can-code/" target="_blank"><img src="img/partners/apple.png" alt="Apple"></a>
+    </li>
+    <li>
+        <a href="http://www.publiclibraries2020.eu" target="_blank"><img src="img/partners/colabora_PublicLibraries2020.png" alt="Public Libraries 2020"></a>
+    </li>
+    <li>
+        <a href="http://ec.europa.eu/digital-agenda/en/grand-coalition-digital-jobs-0" target="_blank"><img src="img/partners/digital-skills.png" alt="Grote coalitie van digitale banen"></a>
+    </li>
+    <li>
+        <a href="http://coderdojo.org" target="_blank"><img src="img/partners/colabora_coderdojo.png" alt="CoderDojo"></a>
+    </li>
+    <li>
+        <a href="http://www.africacodeweek.org/" target="_blank"><img src="img/partners/colabora_africacodeweek.png" alt="Africa Code Week"></a>
+    </li>
+    <li>
+        <a href="http://www.allyouneediscode.eu/" target="_blank"><img src="img/partners/colabora_aynic.png" alt="All you need is code"></a>
+    </li>
+    <li>
+        <a href="http://www.eun.org/" target="_blank"><img src="img/partners/colabora_eun.png" alt="European Schoolnet"></a>
+    </li>
+    <li>
+        <a href="http://scratch.mit.edu/codeweekeu" target="_blank"><img src="img/partners/colabora_scratch.png" alt="Scratch"></a>
+    </li>
+    <li>
+        <a href="https://edu.google.com/resources/computerscience" target="_blank"><img src="img/partners/google.png" alt="Google"></a>
+    </li>
+    <li>
+        <a href="https://mooc.uniurb.it/wp/codeweekmooc/" target="_blank"><img src="img/partners/urbinocarlo.png" alt="CodeWeek in Your Classroom, Now!"></a>
+    </li>
+    <li>
+        <a href="https://facebook.com/" target="_blank"><img src="img/partners/facebook.png" alt="Facebook"></a>
+    </li>
+    <li>
+        <a href="https://education.ti.com/en/select-country/europe" target="_blank"><img src="img/partners/texas-instruments.png" alt="Texas Instruments"></a>
+    </li>
+    <li>
+        <a href="https://www.makeblock.com/" target="_blank"><img src="img/partners/makeblock.png" alt="Makeblock"></a>
+    </li>
 </ul>
