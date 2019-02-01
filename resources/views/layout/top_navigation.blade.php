6bec016bc6a3d5249367f0d243f75d37b8409f17
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2019-02-01 09:59:45 +0100

    Facebook Feed on Top White Bar #236

--- resources/views/layout/top_navigation.blade.php
+++ resources/views/layout/top_navigation.blade.php
@@ -3,7 +3,7 @@
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/pt_PT/sdk.js#xfbml=1&version=v3.2';
+        js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.2';
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
 
