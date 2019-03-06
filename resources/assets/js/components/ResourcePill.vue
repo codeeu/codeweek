a170eac3193e41b49245597e2a994ff48eeca3a4
Alain Van Driessche <alainvd@gmail.com>
2019-03-06 11:49:31 +0100

    Remove link on the pills

--- resources/assets/js/components/ResourcePill.vue
+++ resources/assets/js/components/ResourcePill.vue
@@ -1,7 +1,7 @@
 <template>
 
 
-                    <a class="inline-block border text-sm m-1 border-blue rounded hover:border-grey-lighter text-blue hover:bg-blue hover:text-white py-0 px-1" href="#">{{ property.name }}</a>
+                    <span class="inline-block border text-sm m-1 border-blue rounded cursor-default hover:border-grey-lighter text-blue hover:bg-blue hover:text-white py-0 px-1">{{ property.name }}</span>
 
 
 
