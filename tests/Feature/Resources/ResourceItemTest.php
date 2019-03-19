ccae9660b4ac34921e8588aa583ae8412428195a
Alain Van Driessche <alainvd@gmail.com>
2019-03-19 16:10:07 +0100

    Fix Unit Tests

--- tests/Feature/Resources/ResourceItemTest.php
+++ tests/Feature/Resources/ResourceItemTest.php
@@ -268,7 +268,7 @@ class ResourceItemTest extends TestCase
 
         $languages = $item->fresh()->languages;
 
-        $expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+        $expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
         $this->assertEquals($expected, $languages->pluck('id')->toArray());
 
     }
