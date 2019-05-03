12b203b20fc20cbbf9344e7bae9320207ab192e6
Alain Van Driessche <alainvd@gmail.com>
2019-05-03 13:59:23 +0200

    lock files and syntax fixes in vue files.

--- resources/assets/js/components/SearchPageComponent.vue
+++ resources/assets/js/components/SearchPageComponent.vue
@@ -262,7 +262,7 @@
         margin-left: 20px;
         font-weight: bold;
         flex: 1;
-        justify-content: end;
+        justify-content: flex-end;
         display: flex;
         display: -webkit-box;
     }
