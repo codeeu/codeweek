60b3294bd119f06bc4342f9bcbdcf2d99ead82c9
Alain Van Driessche <alainvd@gmail.com>
2019-06-17 16:42:33 +0200

    Generate Certificates

---
+++ resources/lang/en/participation.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'title' => 'Generate Certificates of Participation for your class',
+    'phrase1' => 'Fill the form with the name of your students separated by a comma and you will receive the individual certificates of participation',
+    'names' => [
+        'label' => 'Names for certificate',
+        'help' => 'Use a comma between each class member'
+    ],
+    'event_name' => [
+        'label' => 'Activity Name',
+        'help' => 'The name of your activity to be printed on the certificate'
+    ],
+    'event_date' => [
+        'label' => 'Activity Date',
+        'help' => 'The date of your activity to be printed on the certificate'
+    ],
+    'submit' => 'Generate Certificates',
+    'thanks_page'=>[
+        'title'=>'Your certificates have been generated!',
+        'phrase1'=>'Click on this link to download the zip file with all the certificates',
+
+    ],
+];
--- resources/views/components/report/form-field-simple.blade.php
+++ resources/views/components/report/form-field-simple.blade.php
@@ -1,7 +1,7 @@
 <div class="mb-8">
     <div class="block text-grey-darkest text-3xl font-normal mb-2 font-bold" for="{{$field_name}}">
         @if($required)*@endif
-        @lang('report.'.$field_name.'.label')
+        @lang($section.'.'.$field_name.'.label')
     </div>
     <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-dark mb-2 fborder-red border-2"
            id="{{$field_name}}" type="{{$type}}" name="{{$field_name}}" value="{{old($field_name)}}">
@@ -12,7 +12,7 @@
             <span class="text-grey-dark">
         @endif
 
-        @lang('report.'.$help)
+        @lang($section.'.'.$help)
 
         </span>
 </div>
