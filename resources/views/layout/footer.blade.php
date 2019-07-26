3d9c6abd88ac903bbf7175fbf4aa06242e90a2b3
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2019-07-26 16:23:49 +0200

    home page

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,21 @@
 <!-- Footer -->
 <footer id="footer">
-
+    <div class="content">
+        <div class="question">
+            <div class="text">
+                Still have a question? Just drop us a line!
+            </div>
+            <div class="get-in-touch">
+                <div class="button">GET IN TOUCH</div>
+                <img src="/images/get_in_touch.svg" class="static-image">
+            </div>
+        </div>
+        <div class="about">
+            <img src="/images/EU_logo.png">
+            <div class="phrase">A grassroots initiative run by volunteers and supported by the European Commission.</div>
+            <img src="/images/logo.svg" class="logo_footer">
+            <img src="/images/bubbles_footer.svg" class="static-image bubbles_footer">
+        </div>
+    </div>
 </footer>
 <!-- Footer End -->
--- resources/views/static/en/home.blade.php
+++ resources/views/static/en/home.blade.php
@@ -28,20 +28,27 @@
                 Find out how to get involved!
             </div>
         </section>
+        <section class="sub-section" id="organize-activity">
+            <div class="text">Anyone is welcome to organise or join  an activity. Just pick a topic and a target audience and add your activity to the map, or browse for events in your area.</div>
+            <img src="/images/organize_your_activity.svg" class="static-image">
+            <div class="title">Organise or join an activity</div>
+        </section>
+        <div class="mobile-arrow"><img src="/images/arrow_down.svg"></div>
+        <section class="sub-section" id="get-started">
+            <div class="text">Not sure how to get started? Take a look at the how-to page, and download our toolkits for organisers to get prepared and spread the word.</div>
+            <img src="/images/get_started.svg" class="static-image">
+            <div class="title">Get started</div>
+        </section>
+        <div class="mobile-arrow"><img src="/images/arrow_down.svg"></div>
+        <section class="sub-section" id="access-resources">
+            <div class="text">If you are not sure how to organise an activity, visit our teaching resources page and learning bits training materials for guidance and tailored lesson plans.</div>
+            <img src="/images/access_resources.svg" class="static-image">
+            <div class="title">Access resources and training</div>
+        </section>
+        <div class="mobile-arrow"><img src="/images/arrow_down.svg"></div>
     </section>
 @endsection
 
 @push('scripts')
     @include('static.countdown')
 @endpush
-
-@section("extra-css")
-    <style>
-        .section-intro, .section-banner {
-
-            background: transparent;
-
-        }
-    </style>
-@endsection
-
