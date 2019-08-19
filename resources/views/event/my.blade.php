9829c0727a6ef47075d6e6170b38abd277640a70
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2019-08-19 16:11:48 +0200

    fix bugs in tag input and autocomplete address component

--- resources/views/event/my.blade.php
+++ resources/views/event/my.blade.php
@@ -1,7 +1,37 @@
 @extends('layout.base')
 
 @section('content')
-    <section>
+
+    <section id="codeweek-myevents-page" class="codeweek-page">
+
+        <section class="codeweek-content-wrapper">
+
+            <h1>@lang('myevents.created_by') {{ Auth::user()->fullName }}</h1>
+
+            @if(!$events || (count($events) == 0))
+                <div class="row">
+                    <p>@lang('myevents.no_events.first_call_to_action') <a href="{{route('create_event')}}">@lang('myevents.no_events.first_link')</a> @lang('myevents.no_events.second_call_to_action') <a href="{{route('guide')}}">@lang('myevents.no_events.second_link')</a>?</p>
+                </div>
+            @else
+
+
+                <div class="row">
+                    @foreach($events as $event)
+                        @component('event.event_tile', ['event'=>$event])
+                        @endcomponent
+                    @endforeach
+
+                </div>
+                {{ $events->links() }}
+            @endif
+
+        </section>
+
+
+    </section>
+
+
+    {{--<section>
 
         <div class="container">
             <h1 style="display: inline-block;">@lang('myevents.created_by') {{ Auth::user()->fullName }}</h1>
@@ -26,7 +56,7 @@
 
         </div>
 
-    </section>
+    </section>--}}
 
 
 @endsection
