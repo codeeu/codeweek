a0b2b66cc0f112b0cef8c3e2d57db07a7fe1309f
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2019-08-20 15:38:04 +0200

    unescape html entities

--- resources/lang/al/about.php
+++ resources/lang/al/about.php
@@ -2,90 +2,90 @@
 
 
 return [
-    'when-title'=> 'P&euml;r vitin 2019, EU Code Week do t&euml; zhvillohet mes datave 5 dhe 20 tetor.',
-    'when-text'=>'<p>EU Code Week &euml;sht&euml; nj&euml; l&euml;vizje n&euml; terren q&euml; feston aft&euml;sin&euml;
-                        krijuese, zgjidhjen e problemeve dhe bashk&euml;punimin p&euml;rmes programimit dhe aktiviteteve
-                        t&euml; tjera teknologjike. Ideja &euml;sht&euml; q&euml; programimi t&euml; b&euml;het m&euml;
-                        i duksh&euml;m, p&euml;r t&rsquo;u treguar t&euml; rinjve, t&euml; rriturve dhe t&euml;
-                        moshuarve se si t&euml; sjellin n&euml; jet&euml; idet&euml; n&euml;p&euml;rmjet kodit, p&euml;r
-                        t&euml; zbuluar misterin e k&euml;tyre aft&euml;sive dhe p&euml;r t&euml; b&euml;r&euml; bashk&euml;
-                        njer&euml;zit e motivuar q&euml; t&euml; m&euml;sojn&euml;.</p>',
-    'codeweek_in_numbers-title'=>'Code Week n&euml; shifra',
-    'codeweek_in_numbers-text'=>'<p>N&euml; vitin 2018, 2,7 milion&euml; persona n&euml; m&euml; shum&euml; se 70 vende n&euml; mbar&euml;
-                        bot&euml;n mor&euml;n pjes&euml; n&euml; EU Code Week. Edhe 2,3 milion&euml; t&euml; rinj t&euml;
-                        tjer&euml; u p&euml;rfshin&euml; n&euml; <a href="http://africacodeweek.org/">Africa Code
-                            Week</a>, q&euml; &euml;sht&euml; nj&euml; nism&euml; bij&euml;, e drejtuar nga SAP dhe
-                        organizata jofitimprur&euml;se.</p>
+    'when-title'=> 'Për vitin 2019, EU Code Week do të zhvillohet mes datave 5 dhe 20 tetor.',
+    'when-text'=>'<p>EU Code Week është një lëvizje në terren që feston aftësinë
+                        krijuese, zgjidhjen e problemeve dhe bashkëpunimin përmes programimit dhe aktiviteteve
+                        të tjera teknologjike. Ideja është që programimi të bëhet më
+                        i dukshëm, për t’u treguar të rinjve, të rriturve dhe të
+                        moshuarve se si të sjellin në jetë idetë nëpërmjet kodit, për
+                        të zbuluar misterin e këtyre aftësive dhe për të bërë bashkë
+                        njerëzit e motivuar që të mësojnë.</p>',
+    'codeweek_in_numbers-title'=>'Code Week në shifra',
+    'codeweek_in_numbers-text'=>'<p>Në vitin 2018, 2,7 milionë persona në më shumë se 70 vende në mbarë
+                        botën morën pjesë në EU Code Week. Edhe 2,3 milionë të rinj të
+                        tjerë u përfshinë në <a href="http://africacodeweek.org/">Africa Code
+                            Week</a>, që është një nismë bijë, e drejtuar nga SAP dhe
+                        organizata jofitimprurëse.</p>
 
                     <p>
                         Në vitin 2018, 2,7 milionë persona nga më shumë se 70 vende të botës morën pjesë në EU Code Week. 2,3 milionë të rinj të tjerë u përfshinë në <a href="http://africacodeweek.org/">Africa Code
                             Week</a>, e frymëzuar EU Code Week dhe e drejtuar nga SAP si dhe organizata jofitimprurëse.
                     </p>',
-    'run_by_volunteers-title'=>'Drejtuar nga vullnetar&euml;',
-    'run_by_volunteers-text'=>' <p>EU Code Week drejtohet nga vullnetar&euml;. Nj&euml; ose disa <a href="/ambassadors">ambasador&euml;
-                            t&euml; Code Week</a> koordinojn&euml; nism&euml;n n&euml; vendet e tyre, por kushdo mund t&euml;
-                        organizoj&euml; aktivitetin e vet dhe ta shtoj&euml; at&euml; n&euml; hart&euml;n e <a href="/">codeweek.eu</a>.
+    'run_by_volunteers-title'=>'Drejtuar nga vullnetarë',
+    'run_by_volunteers-text'=>' <p>EU Code Week drejtohet nga vullnetarë. Një ose disa <a href="/ambassadors">ambasadorë
+                            të Code Week</a> koordinojnë nismën në vendet e tyre, por kushdo mund të
+                        organizojë aktivitetin e vet dhe ta shtojë atë në hartën e <a href="/">codeweek.eu</a>.
                     </p>',
-    'supported_by_commission-title'=>'Mb&euml;shtetur nga Komisioni Evropian',
-    'supported_by_commission-text'=>'<p>EU Code Week u prezantua n&euml; vitin 2013 nga Young Advisors for the Digital Agenda Europe.
-                        Komisioni Evropian mb&euml;shtet EU Code Week, si pjes&euml; e strategjis&euml; s&euml; tij p&euml;r
-                        nj&euml; <a href="http://ec.europa.eu/priorities/digital-single-market/">Treg t&euml; P&euml;rbashk&euml;t
-                            Dixhital</a>. N&euml; <a
+    'supported_by_commission-title'=>'Mbështetur nga Komisioni Evropian',
+    'supported_by_commission-text'=>'<p>EU Code Week u prezantua në vitin 2013 nga Young Advisors for the Digital Agenda Europe.
+                        Komisioni Evropian mbështet EU Code Week, si pjesë e strategjisë së tij për
+                        një <a href="http://ec.europa.eu/priorities/digital-single-market/">Treg të Përbashkët
+                            Dixhital</a>. Në <a
                                 href="https://ec.europa.eu/education/initiatives/european-education-area/digital-education-action-plan_en">Planin
-                            e masave t&euml; Edukimit Dixhital</a> Komisioni ve&ccedil;an&euml;risht inkurajon shkollat
-                        q&euml; t&euml; b&euml;hen pjes&euml; e nism&euml;s. Objektivi &euml;sht&euml; t&euml; arrihet
-                        50% e t&euml; gjitha shkollave n&euml; Evrop&euml; deri n&euml; vitin 2020.</p>',
+                            e masave të Edukimit Dixhital</a> Komisioni veçanërisht inkurajon shkollat
+                        që të bëhen pjesë e nismës. Objektivi është të arrihet
+                        50% e të gjitha shkollave në Evropë deri në vitin 2020.</p>',
     'schools-title'=>'Shkollat',
-    'schools-text'=>'<p>Shkollat n&euml; &ccedil;do nivel dhe m&euml;suesit e t&euml; gjitha l&euml;nd&euml;ve jan&euml;
-                        ve&ccedil;an&euml;risht t&euml; ftuar t&euml; marrin pjes&euml; n&euml; EU Code Week, p&euml;r t&rsquo;u
-                        dh&euml;n&euml; mund&euml;sin&euml; nx&euml;n&euml;sve t&euml; tyre t&euml; eksplorojn&euml; aft&euml;sit&euml;
-                        krijuese dixhitale dhe kodimin. M&euml;soni m&euml; shum&euml; rreth nism&euml;s dhe se si t&euml;
-                        organizoni aktivitetin tuaj p&euml;rmes faqes s&euml; uebit t&euml; dedikuar p&euml;r m&euml;suesit:
+    'schools-text'=>'<p>Shkollat në çdo nivel dhe mësuesit e të gjitha lëndëve janë
+                        veçanërisht të ftuar të marrin pjesë në EU Code Week, për t’u
+                        dhënë mundësinë nxënësve të tyre të eksplorojnë aftësitë
+                        krijuese dixhitale dhe kodimin. Mësoni më shumë rreth nismës dhe se si të
+                        organizoni aktivitetin tuaj përmes faqes së uebit të dedikuar për mësuesit:
                         <a href="/schools">CodeWeek.eu/Schools</a></p>',
     'why_coding-title'=>'Pse nevojitet kodimi?',
-    'why_coding-text'=>'<p>&Euml;sht&euml; p&euml;r Pian, e cila zgjodhi t&euml; studionte n&euml; fush&euml;n juridike,
-                        ndon&euml;se gjithmon&euml; i ka p&euml;lqyer matematika dhe t&euml; luaj&euml; me kompjuter&euml;t.
-                        &Euml;sht&euml; p&euml;r Markun, i cili ka nj&euml; ide p&euml;r t&euml; pasur nj&euml; rrjet
-                        social m&euml; t&euml; mir&euml;, por q&euml; nuk mund ta nd&euml;rtoj&euml; vet&euml;m. &Euml;sht&euml;
-                        p&euml;r Liz&euml;n, e cila &euml;nd&euml;rron q&euml; t&euml; krijoj&euml; robot&euml; sepse
-                        prind&euml;rit nuk e lejojn&euml; t&euml; mbaj&euml; mace.</p>
+    'why_coding-text'=>'<p>Është për Pian, e cila zgjodhi të studionte në fushën juridike,
+                        ndonëse gjithmonë i ka pëlqyer matematika dhe të luajë me kompjuterët.
+                        Është për Markun, i cili ka një ide për të pasur një rrjet
+                        social më të mirë, por që nuk mund ta ndërtojë vetëm. Është
+                        për Lizën, e cila ëndërron që të krijojë robotë sepse
+                        prindërit nuk e lejojnë të mbajë mace.</p>
 
-                    <p>&Euml;sht&euml; p&euml;r ju q&euml; keni filluar q&euml; m&euml; par&euml; t&euml; ndihmoni q&euml;
-                        k&euml;to &euml;ndrra t&euml; b&euml;hen realitet.</p>
+                    <p>Është për ju që keni filluar që më parë të ndihmoni që
+                        këto ëndrra të bëhen realitet.</p>
 
-                    <p>N&euml; fakt, &euml;sht&euml; p&euml;r t&euml; gjith&euml; ne. P&euml;r t&euml; ardhmen ton&euml;.
-                        Teknologjia po i jep form&euml; jet&euml;s son&euml;, por ne po lejojm&euml; q&euml; nj&euml;
-                        pakic&euml; t&euml; vendos&euml; se p&euml;r &ccedil;far&euml; ta p&euml;rdorim at&euml;. Ne
-                        mund t&euml; b&euml;jm&euml; m&euml; shum&euml; sesa thjesht t&euml; &ldquo;ndajm&euml;&rdquo;
-                        dhe t&euml; &ldquo;p&euml;lqejm&euml;&rdquo; gj&euml;rat n&euml; internet. Ne mund t&rsquo;i
-                        sjellim n&euml; jet&euml; idet&euml; tona t&euml; &ccedil;mendura, t&euml; nd&euml;rtojm&euml;
-                        gj&euml;ra q&euml; u sjellin g&euml;zim t&euml; tjer&euml;ve.</p>
+                    <p>Në fakt, është për të gjithë ne. Për të ardhmen tonë.
+                        Teknologjia po i jep formë jetës sonë, por ne po lejojmë që një
+                        pakicë të vendosë se për çfarë ta përdorim atë. Ne
+                        mund të bëjmë më shumë sesa thjesht të “ndajmë”
+                        dhe të “pëlqejmë” gjërat në internet. Ne mund t’i
+                        sjellim në jetë idetë tona të çmendura, të ndërtojmë
+                        gjëra që u sjellin gëzim të tjerëve.</p>
 
-                    <p>Asnj&euml;her&euml; nuk ka qen&euml; m&euml; e leht&euml; q&euml; t&euml; krijoni aplikacionin
-                        tuaj, t&euml; nd&euml;rtoni robotin tuaj, ose pse jo, t&euml; shpikni makina q&euml; fluturojn&euml;!
-                        Nuk &euml;sht&euml; nj&euml; udh&euml;tim i leht&euml;, por &euml;sht&euml; nj&euml; udh&euml;tim
-                        plot me sfida krijuese, nj&euml; komunitet mb&euml;shtet&euml;s dhe shum&euml; arg&euml;tim. A
-                        jeni gati t&euml; pranoni sfid&euml;n dhe t&euml; b&euml;heni krijues?</p>
+                    <p>Asnjëherë nuk ka qenë më e lehtë që të krijoni aplikacionin
+                        tuaj, të ndërtoni robotin tuaj, ose pse jo, të shpikni makina që fluturojnë!
+                        Nuk është një udhëtim i lehtë, por është një udhëtim
+                        plot me sfida krijuese, një komunitet mbështetës dhe shumë argëtim. A
+                        jeni gati të pranoni sfidën dhe të bëheni krijues?</p>
 
-                    <p>Kodimi ndihmon gjithashtu n&euml; zhvillimin e kompetencave t&euml; tilla si mendimi kompjuterik,
-                        zgjidhja e problemeve, aft&euml;sia krijuese dhe puna n&euml; ekip, q&euml; jan&euml; aft&euml;si
-                        v&euml;rtet t&euml; mira p&euml;r t&euml; gjitha fushat e jet&euml;s.</p>',
-    'why_coding-quote'=>'<p>Alessandro Bogliolo, koordinator i ekipit t&euml; vullnetar&euml;ve ambasador&euml; t&euml; EU
+                    <p>Kodimi ndihmon gjithashtu në zhvillimin e kompetencave të tilla si mendimi kompjuterik,
+                        zgjidhja e problemeve, aftësia krijuese dhe puna në ekip, që janë aftësi
+                        vërtet të mira për të gjitha fushat e jetës.</p>',
+    'why_coding-quote'=>'<p>Alessandro Bogliolo, koordinator i ekipit të vullnetarëve ambasadorë të EU
                         Code Week tha:
                         <blockquote>
-                    <p>&ldquo;N&euml; koh&euml;t e lashta kemi b&euml;r&euml; shum&euml; gj&euml;ra duke p&euml;rdorur
-                        gurin, hekurin, letr&euml;n dhe lapsin, t&euml; cilat kan&euml; transformuar jet&euml;n ton&euml;.
-                        Tani ne jetojm&euml; n&euml; nj&euml; epok&euml; t&euml; re ku bota jon&euml; merr form&euml; p&euml;rmes
-                        kodimit. Epokat e ndryshme kan&euml; k&euml;rkesa t&euml; ndryshme p&euml;r pun&euml; dhe aft&euml;si.
-                        Gjat&euml; Code Week ne duam t&rsquo;i japim &ccedil;do evropiani mund&euml;sin&euml; t&euml;
-                        zbuloj&euml; kodimin dhe t&euml; arg&euml;tohet me t&euml;. Le t&euml; m&euml;sojm&euml; si t&euml;
-                        kodojm&euml; p&euml;r t&rsquo;i dh&euml;n&euml; form&euml; t&euml; ardhmes son&euml;&rdquo;.</p>
+                    <p>“Në kohët e lashta kemi bërë shumë gjëra duke përdorur
+                        gurin, hekurin, letrën dhe lapsin, të cilat kanë transformuar jetën tonë.
+                        Tani ne jetojmë në një epokë të re ku bota jonë merr formë përmes
+                        kodimit. Epokat e ndryshme kanë kërkesa të ndryshme për punë dhe aftësi.
+                        Gjatë Code Week ne duam t’i japim çdo evropiani mundësinë të
+                        zbulojë kodimin dhe të argëtohet me të. Le të mësojmë si të
+                        kodojmë për t’i dhënë formë të ardhmes sonë”.</p>
                     </blockquote>
                     </p>',
-    'join_codeweek-title'=>'B&euml;huni pjes&euml; e EU Code Week',
-    'join_codeweek-text'=>'<p>B&euml;huni pjes&euml; e EU Code Week <a href="/guide">duke organizuar nj&euml; aktivitet
-                            kodimi</a> n&euml; qytetin tuaj, duke u b&euml;r&euml; pjes&euml; e <a href="/codeweek4all">Sfid&euml;s
-                            Code Week 4 All</a> dhe duke u lidhur me aktivitete n&euml;p&euml;r komunitete dhe p&euml;rtej
-                        kufijve ose duke na ndihmuar t&euml; p&euml;rhapim vizionin e Code si <a href="/ambassadors">Ambasador
-                            i EU Code Week</a> p&euml;r vendin tuaj!</p>'
+    'join_codeweek-title'=>'Bëhuni pjesë e EU Code Week',
+    'join_codeweek-text'=>'<p>Bëhuni pjesë e EU Code Week <a href="/guide">duke organizuar një aktivitet
+                            kodimi</a> në qytetin tuaj, duke u bërë pjesë e <a href="/codeweek4all">Sfidës
+                            Code Week 4 All</a> dhe duke u lidhur me aktivitete nëpër komunitete dhe përtej
+                        kufijve ose duke na ndihmuar të përhapim vizionin e Code si <a href="/ambassadors">Ambasador
+                            i EU Code Week</a> për vendin tuaj!</p>'
 ];
--- resources/lang/pt/about.php
+++ resources/lang/pt/about.php
@@ -2,57 +2,57 @@
 
 
 return [
-    'when-title' => 'Em 2019, a Semana Europeia da Programa&ccedil;&atilde;o ir&aacute; decorrer entre 5 e 20 de outubro.',
-    'codeweek_in_numbers-title' => 'Semana da Programa&ccedil;&atilde;o em n&uacute;meros',
-    'run_by_volunteers-title' => 'Executada por volunt&aacute;rios',
-    'supported_by_commission-title' => 'Apoiada pela Comiss&atilde;o Europeia',
+    'when-title' => 'Em 2019, a Semana Europeia da Programação irá decorrer entre 5 e 20 de outubro.',
+    'codeweek_in_numbers-title' => 'Semana da Programação em números',
+    'run_by_volunteers-title' => 'Executada por voluntários',
+    'supported_by_commission-title' => 'Apoiada pela Comissão Europeia',
     'schools-title' => 'Escolas',
-    'why_coding-title' => 'Porqu&ecirc; a programa&ccedil;&atilde;o?',
-    'join_codeweek-title' => 'Participe na Semana Europeia da Programa&ccedil;&atilde;o',
+    'why_coding-title' => 'Porquê a programação?',
+    'join_codeweek-title' => 'Participe na Semana Europeia da Programação',
     'when-text' => '
         <p>
-            A Semana Europeia da Programa&ccedil;&atilde;o &eacute; uma iniciativa de base que celebra a criatividade, a resolu&ccedil;&atilde;o de problemas e a colabora&ccedil;&atilde;o atrav&eacute;s da programa&ccedil;&atilde;o e de outras atividades tecnol&oacute;gicas. A ideia &eacute; tornar a programa&ccedil;&atilde;o mais vis&iacute;vel, mostrar aos jovens, adultos e idosos a forma de dar vida &agrave;s ideias atrav&eacute;s da programa&ccedil;&atilde;o, desmistificar estas compet&ecirc;ncias e reunir pessoas motivadas para aprender.
+            A Semana Europeia da Programação é uma iniciativa de base que celebra a criatividade, a resolução de problemas e a colaboração através da programação e de outras atividades tecnológicas. A ideia é tornar a programação mais visível, mostrar aos jovens, adultos e idosos a forma de dar vida às ideias através da programação, desmistificar estas competências e reunir pessoas motivadas para aprender.
         </p>',
 
     'codeweek_in_numbers-text' => '
         <p>
-            Em 2018, 2,7 milhões de pessoas em mais de 70 países à volta do mundo participaram na Semana Europeia da Programação. Para além destes, mais 2,3 milhões de jovens participaram na <a href="http://africacodeweek.org/">Semana Africana da Programa&ccedil;&atilde;o</a>, uma iniciativa gerida pela SAP e por organizações sem fins lucrativos.
+            Em 2018, 2,7 milhões de pessoas em mais de 70 países à volta do mundo participaram na Semana Europeia da Programação. Para além destes, mais 2,3 milhões de jovens participaram na <a href="http://africacodeweek.org/">Semana Africana da Programação</a>, uma iniciativa gerida pela SAP e por organizações sem fins lucrativos.
         </p>
         <p>
-            Se o seu pa&iacute;s ainda n&atilde;o for participante, pode organizar atividades e coloc&aacute;-las no <a href="/events">mapa</a> ou oferecer-se como <a href="mailto:info@codeweek.eu">volunt&aacute;rio</a> para a miss&atilde;o de embaixador da Semana da Programa&ccedil;&atilde;o.
+            Se o seu país ainda não for participante, pode organizar atividades e colocá-las no <a href="/events">mapa</a> ou oferecer-se como <a href="mailto:info@codeweek.eu">voluntário</a> para a missão de embaixador da Semana da Programação.
         </p>',
 
     'run_by_volunteers-text' => '
         <p>
-            A Semana Europeia da Programa&ccedil;&atilde;o foi lan&ccedil;ada em 2013 pelo grupo Young Advisors for the Digital Agenda Europe. A Comiss&atilde;o Europeia apoia a Semana Europeia da Programa&ccedil;&atilde;o no &acirc;mbito da sua estrat&eacute;gia para o <a href="http://ec.europa.eu/priorities/digital-single-market/">Mercado &Uacute;nico Digital</a>. No <a href="https://ec.europa.eu/education/initiatives/european-education-area/digital-education-action-plan_en">Plano de A&ccedil;&atilde;o para a Educa&ccedil;&atilde;o Digital</a>, a Comiss&atilde;o incentiva especialmente as escolas a aderirem &agrave; iniciativa. O objetivo &eacute; alcan&ccedil;ar 50&nbsp;% do conjunto das escolas da Europa at&eacute; 2020.
+            A Semana Europeia da Programação foi lançada em 2013 pelo grupo Young Advisors for the Digital Agenda Europe. A Comissão Europeia apoia a Semana Europeia da Programação no âmbito da sua estratégia para o <a href="http://ec.europa.eu/priorities/digital-single-market/">Mercado Único Digital</a>. No <a href="https://ec.europa.eu/education/initiatives/european-education-area/digital-education-action-plan_en">Plano de Ação para a Educação Digital</a>, a Comissão incentiva especialmente as escolas a aderirem à iniciativa. O objetivo é alcançar 50 % do conjunto das escolas da Europa até 2020.
         </p>',
 
     'supported_by_commission-text' => '
         <p>
-            A Semana Europeia da Programa&ccedil;&atilde;o foi lan&ccedil;ada em 2013 pelo grupo Young Advisors for the Digital Agenda Europe. A Comiss&atilde;o Europeia apoia a Semana Europeia da Programa&ccedil;&atilde;o no &acirc;mbito da sua estrat&eacute;gia para o <a href="http://ec.europa.eu/priorities/digital-single-market/">Mercado &Uacute;nico Digital</a>. No <a href="https://ec.europa.eu/education/initiatives/european-education-area/digital-education-action-plan_en">Plano de A&ccedil;&atilde;o para a Educa&ccedil;&atilde;o Digital</a>, a Comiss&atilde;o incentiva especialmente as escolas a aderirem &agrave; iniciativa. O objetivo &eacute; alcan&ccedil;ar 50&nbsp;% do conjunto das escolas da Europa at&eacute; 2020.
+            A Semana Europeia da Programação foi lançada em 2013 pelo grupo Young Advisors for the Digital Agenda Europe. A Comissão Europeia apoia a Semana Europeia da Programação no âmbito da sua estratégia para o <a href="http://ec.europa.eu/priorities/digital-single-market/">Mercado Único Digital</a>. No <a href="https://ec.europa.eu/education/initiatives/european-education-area/digital-education-action-plan_en">Plano de Ação para a Educação Digital</a>, a Comissão incentiva especialmente as escolas a aderirem à iniciativa. O objetivo é alcançar 50 % do conjunto das escolas da Europa até 2020.
         </p>',
 
     'schools-text' => '
         <p>
-            As escolas de todos os n&iacute;veis de ensino e os professores de todas as disciplinas s&atilde;o particularmente convidados a participarem na Semana Europeia da Programa&ccedil;&atilde;o, para proporcionar a oportunidade aos seus alunos de explorarem a criatividade digital e a programa&ccedil;&atilde;o. Saiba mais sobre a iniciativa e sobre como organizar a sua atividade atrav&eacute;s p&aacute;gina Web dedicada aos professores: <a href="/schools">CodeWeek.eu/Schools</a>
+            As escolas de todos os níveis de ensino e os professores de todas as disciplinas são particularmente convidados a participarem na Semana Europeia da Programação, para proporcionar a oportunidade aos seus alunos de explorarem a criatividade digital e a programação. Saiba mais sobre a iniciativa e sobre como organizar a sua atividade através página Web dedicada aos professores: <a href="/schools">CodeWeek.eu/Schools</a>
         </p>',
 
     'why_coding-text' => '
-        <p>Tem a ver com a Pia, que achava que tinha de estudar direito, embora sempre tivesse gostado de matem&aacute;tica e de mexer em computadores. Tem a ver com o Mark, que tem uma ideia para uma rede social melhor, mas n&atilde;o consegue desenvolv&ecirc;-la sozinho. Tem a ver com a Alice, que sonha em fazer rob&ocirc;s porque os seus pais n&atilde;o a deixam ter um gato.</p>
-        <p>Tem a ver com aqueles que j&aacute; est&atilde;o a ajudar a tornar estes sonhos realidade.</p>
-        <p>Na verdade, tem a ver com todos n&oacute;s. O nosso futuro. A tecnologia est&aacute; a moldar a nossa vida, mas estamos a permitir que uma minoria decida para que finalidade a usamos e de que forma o fazemos. Podemos fazer melhor do que apenas partilhar e gostar de coisas. Podemos dar vida &agrave;s nossas ideias loucas, construir coisas que dar&atilde;o alegria aos outros.</p>
-        <p>Nunca foi t&atilde;o f&aacute;cil criar uma aplica&ccedil;&atilde;o, construir um rob&ocirc; ou inventar carros voadores, por que n&atilde;o! N&atilde;o &eacute; um caminho f&aacute;cil, mas &eacute; um caminho repleto de desafios criativos, com uma comunidade de apoio e muita divers&atilde;o. Est&aacute; pronto para aceitar o desafio e tornar-se inventor?</p>
-        <p>A programa&ccedil;&atilde;o tamb&eacute;m ajuda a desenvolver compet&ecirc;ncias como o pensamento computacional, a resolu&ccedil;&atilde;o de problemas, a criatividade e o trabalho em equipa &mdash; que s&atilde;o compet&ecirc;ncias verdadeiramente &uacute;teis em todos os quadrantes.</p>',
+        <p>Tem a ver com a Pia, que achava que tinha de estudar direito, embora sempre tivesse gostado de matemática e de mexer em computadores. Tem a ver com o Mark, que tem uma ideia para uma rede social melhor, mas não consegue desenvolvê-la sozinho. Tem a ver com a Alice, que sonha em fazer robôs porque os seus pais não a deixam ter um gato.</p>
+        <p>Tem a ver com aqueles que já estão a ajudar a tornar estes sonhos realidade.</p>
+        <p>Na verdade, tem a ver com todos nós. O nosso futuro. A tecnologia está a moldar a nossa vida, mas estamos a permitir que uma minoria decida para que finalidade a usamos e de que forma o fazemos. Podemos fazer melhor do que apenas partilhar e gostar de coisas. Podemos dar vida às nossas ideias loucas, construir coisas que darão alegria aos outros.</p>
+        <p>Nunca foi tão fácil criar uma aplicação, construir um robô ou inventar carros voadores, por que não! Não é um caminho fácil, mas é um caminho repleto de desafios criativos, com uma comunidade de apoio e muita diversão. Está pronto para aceitar o desafio e tornar-se inventor?</p>
+        <p>A programação também ajuda a desenvolver competências como o pensamento computacional, a resolução de problemas, a criatividade e o trabalho em equipa — que são competências verdadeiramente úteis em todos os quadrantes.</p>',
 
     'why_coding-quote' => '
         <p>
-            Alessandro Bogliolo, coordenador da equipa de embaixadores da Semana Europeia da Programa&ccedil;&atilde;o, disse:
+            Alessandro Bogliolo, coordenador da equipa de embaixadores da Semana Europeia da Programação, disse:
             <blockquote>
                 <p>
-                    «Desde o in&iacute;cio dos tempos fizemos muitas coisas utilizando pedra, ferro, papel e l&aacute;pis que transformaram nossas vidas. Agora, vivemos numa era diferente, em que o nosso mundo &eacute; moldado por meio da programa&ccedil;&atilde;o. As diferentes eras t&ecirc;m uma procura diferente de empregos e compet&ecirc;ncias. Durante a Semana Europeia da Programa&ccedil;&atilde;o queremos dar a todos os europeus a oportunidade de descobrirem a programa&ccedil;&atilde;o e de se divertirem. Vamos aprender a programar para moldar o nosso futuro.»
+                    «Desde o início dos tempos fizemos muitas coisas utilizando pedra, ferro, papel e lápis que transformaram nossas vidas. Agora, vivemos numa era diferente, em que o nosso mundo é moldado por meio da programação. As diferentes eras têm uma procura diferente de empregos e competências. Durante a Semana Europeia da Programação queremos dar a todos os europeus a oportunidade de descobrirem a programação e de se divertirem. Vamos aprender a programar para moldar o nosso futuro.»
                 </p>
             </blockquote>
         </p>',
     'join_codeweek-text' => '
-        <p>Participe na Semana Europeia da Programa&ccedil;&atilde;o <a href="/guide">organizando uma atividade de programa&ccedil;&atilde;o</a> na sua cidade, participando no <a href="/codeweek4all">desafio «Code Week 4 all</a>» ou ajudando-nos a transmitir a vis&atilde;o da Semana da Programa&ccedil;&atilde;o enquanto <a href="/ambassadors">embaixador da Semana Europeia da Programa&ccedil;&atilde;o</a> no seu pa&iacute;s!</p>'
+        <p>Participe na Semana Europeia da Programação <a href="/guide">organizando uma atividade de programação</a> na sua cidade, participando no <a href="/codeweek4all">desafio «Code Week 4 all</a>» ou ajudando-nos a transmitir a visão da Semana da Programação enquanto <a href="/ambassadors">embaixador da Semana Europeia da Programação</a> no seu país!</p>'
 ];
