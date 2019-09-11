7e9252a4f7a212321696db22d0390cf1a8a043b0
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2019-09-11 14:10:46 +0200

    thankyou reports page

--- resources/views/report/thankyou.blade.php
+++ resources/views/report/thankyou.blade.php
@@ -2,17 +2,24 @@
 
 @section('content')
 
-    <section>
+    <section id="codeweek-thankyou-report-page" class="codeweek-page">
 
-        <div class="container">
+        <section class="codeweek-content-header">
+
             <h1>@lang('report.thanks_page.title')</h1>
+
+        </section>
+
+        <section class="codeweek-content-wrapper">
+
             <h2>
                 @lang('report.thanks_page.certificate_ready')
                 <a href="{{$event->certificate_url}}">@lang('report.thanks_page.download_button')</a>
             </h2>
             <h4><a href="{{$event->path()}}">@lang('report.thanks_page.back_events')</a></h4>
 
-        </div>
+
+        </section>
 
     </section>
 
