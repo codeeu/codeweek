7be98f0b5b901b1c06dd0cc93db00d17c8698731
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2019-09-11 11:52:23 +0200

    translations of guide page

---
+++ resources/lang/hr/guide.php
@@ -0,0 +1,56 @@
+<?php
+
+
+return [
+
+    'title' => 'Vodič',
+    'organise_activity' => 'Organizirajte vlastito događanje uz #CodeWeek',
+    'register_activity' => 'Ovdje registrirajte svoje događanje',
+    'what' => [
+        'title' => 'Što je Europski tjedan programiranja?',
+        'content' => '<p>Europski tjedan programiranja društvena je inicijativa koju vode volonteri, a podržava je Europska komisija. Svatko – škole, nastavnici, knjižnice, klubovi za programiranje, društva, javna tijela – može organizirati #CodeWeek događanje i unijeti ga na kartu na stranici <a href="/events">codeweek.eu</a>.</p>'
+    ],
+    'what_you_need_organise' => [
+        'title' => 'Što vam je potrebno za organiziranje događanja?',
+        'items' => [
+            '1' => '<strong>Skupina ljudi koja želi učiti.</strong> Na primjer, vaši prijatelji, djeca, tinejdžeri, kolege odrasle dobi, roditelji ili bake i djedovi. Upamtite: već dvije osobe čine skupinu!',
+            '2' => '<strong>Nastavnici ili treneri</strong> kojima je poznato događanje programiranja te kako poučavati i nadahnuti druge. Broj ovisi o vrsti i veličini događanja.',
+            '3' => '<strong>Mjesto za učenje.</strong> Odlično mjesto za organizaciju događanja jesu učionice, knjižnice, sobe za sastanke i različita javna mjesta.',
+            '4' => '<strong>Računala i internetska veza</strong> Ovisno o vašoj ciljanoj skupini, možete zamoliti sudionike da ponesu vlastita prijenosna računala. ',
+            '5' => '<strong>Izvanmrežno programiranje.</strong> Zapravo vam ne trebaju računala i internetska veza za učenje računalnog razmišljanja. Pogledajte naš <a href="/training/coding-without-computers">Odjeljak o isključenoj nastavi (engl. Unplugged learning bit)</a> da biste krenuli.',
+            '6' => '<strong>Materijali za učenje.</strong> Pokažite sudionicima kako zabavno može biti samostalno stvaranje nečega. Provjerite našu <a
+                        href="/resources">stranicu s resursima</a> i <a href="/training">dijelove za učenje</a> s videotutorialima i nastavnim planovima te ih prilagodite potrebama svoje skupine.',
+            '7' => '<strong>Registracija sudionika.</strong> Ako je dostupan ograničen prostor, možete upotrijebiti internetske alate kao što su <a href="https://docs.google.com/forms/">Googleovi obrasci</a> i <a
+                        href="https://www.eventbrite.com/">Eventbrite</a> da biste registrirali sudionike.',
+            '8' => 'Nemojte zaboraviti <a href="/add">unijeti svoje događanje</a> na <a href="/events">kartu Tjedna programiranja</a>!',
+        ]
+    ],
+    'how_to' => [
+        'title' => 'Kako organizirati svoje događanje?',
+        'items' => [
+            '1' => 'Sami odlučujete o formatu svojeg događanja programiranja, no savjetujemo da uključite <strong>vrijeme za praktičan rad</strong> tijekom kojega sudionici mogu samostalno stvarati i/ili čeprkati po hardveru.',
+            '2' => 'Koristite se <strong>alatima i tehnologijama</strong> koje su primjerene vašoj ciljanoj skupini. Preporučujemo upotrebu <a href="http://codeweek.eu/resources/">besplatnog i lako dostupnog materijala otvorenog izvora</a>.',
+            '3' => 'Potaknite sudionike da si na kraju događanja međusobno <strong>pokažu i predstave</strong> što su stvorili.',
+            '4' => '<strong>Proširite glas!</strong> Promovirajte i podijelite što ste radili tijekom aktivnosti na društvenim mrežama uz oznaku #CodeWeek. Dijeliti možete i u <a
+                        href="https://www.facebook.com/groups/774720866253044/">grupi za nastavnike za EU Code Week</a> te na Twitteru (<a href="https://twitter.com/CodeWeekEU">@CodeWeekEU</a>). Govorite o njemu svojim prijateljima, kolegama podučavateljima, u lokalnom tisku i objavite priopćenje za medije.',
+            '5' => 'Nemojte zaboraviti <a href="/add">dodati svoje događanje</a> na <a href="/events">kartu Tjedna programiranja</a>!',
+        ]
+    ],
+    'material' => [
+        'title' => 'Promotivni materijal',
+        'text' => '<p>Na našem <a href="http://blog.codeweek.eu/">blogu</a> potražite najnovije informacije te slobodno prilagodite najnovije priopćenje za medije svojim potrebama ili izradite vlastito priopćenje:</p>',
+        'items' => [
+            '1' => '<a href="https://ec.europa.eu/digital-single-market/en/news/gearing-celebrate-eu-code-week-2019">Priprema za proslavu Europskog tjedna programiranja 2019.</a> (dostupno na 29 jezika)'
+        ]
+    ],
+    'toolkits' => [
+        'title' => 'Preuzmite sljedeće komplete alata koji će vam pomoći da krenete:',
+        'communication_toolkit' => 'Komplet alata za komunikaciju',
+        'teachers_toolkit' => 'Komplet alata za nastavnike'
+    ],
+    'questions' => [
+        'title' => 'Pitanja?',
+        'content' => '<p>Ako imate pitanja o organiziranju i promicanju svojeg #CodeWeek događanja, obratite se nekome od <a href="/ambassadors">ambasadora Europskog tjedna programiranja</a> u svojoj zemlji.</p>'
+    ]
+
+];
---
+++ resources/lang/ro/guide.php
@@ -0,0 +1,56 @@
+<?php
+
+
+return [
+
+    'title' => 'Ghid',
+    'organise_activity' => 'Organizați-vă propria activitate cu #CodeWeek',
+    'register_activity' => 'Înregistrați-vă activitatea aici',
+    'what' => [
+        'title' => 'Ce este Săptămâna UE a programării?',
+        'content' => '<p>Săptămâna UE a programării este o mișcare de bază organizată de voluntari și sprijinită de Comisia Europeană. Oricine – școli, profesori, biblioteci, cluburi de programare, întreprinderi, autorități publice – poate organiza o activitate #CodeWeek pe care să o adauge pe harta de pe site-ul <a href="/events">codeweek.eu</a>.</p>'
+    ],
+    'what_you_need_organise' => [
+        'title' => 'De ce aveți nevoie pentru a organiza o activitate?',
+        'items' => [
+            '1' => '<strong>Un grup de oameni dornici să învețe.</strong> De exemplu, prietenii dumneavoastră, copii, adolescenți, colegi adulți, părinți sau bunici. Rețineți, doi înseamnă deja un grup!',
+            '2' => '<strong>Formatori sau instructori</strong> care știu programare și cum să îi învețe și să îi inspire pe ceilalți. Numărul depinde de tipul și amploarea evenimentului.',
+            '3' => '<strong>Un loc de desfășurare.</strong> Săli de clasă, biblioteci, săli de conferință și diverse spații publice – toate sunt locuri excelente pentru evenimente.',
+            '4' => '<strong>Calculatoare și conexiune la internet.</strong> În funcție de grupul dumneavoastră țintă, le puteți cere participanților să își aducă propriile laptopuri.',
+            '5' => '<strong>Programare fără calculator.</strong> De fapt nu aveți nevoie de calculatoare și de conexiune la internet pentru a deprinde gândirea computațională. Uitați-vă la <a href="/training/coding-without-computers">Modulul nostru de învățare fără dispozitive electronice</a> pentru a începe.',
+            '6' => '<strong>Materiale de învățare.</strong> Arătați-le participanților cât de distractiv este să creeze ceva pe cont propriu. Studiați <a
+                        href="/resources">pagina de resurse</a> și <a href="/training">modulele de învățare</a> cu tutoriale video și planuri de lecții și adaptați-le nevoilor grupului dumneavoastră.',
+            '7' => '<strong>Înregistrați participanții.</strong> Dacă aveți un număr limitat de locuri disponibile, puteți utiliza instrumente online precum <a href="https://docs.google.com/forms/">formulare Google</a> și <a
+                        href="https://www.eventbrite.com/">Eventbrite</a> pentru a înregistra participanții.',
+            '8' => 'Amintiți-vă să <a href="/add">adăugați activitatea dumneavoastră</a> pe <a href="/events">harta Săptămânii programării</a>!',
+        ]
+    ],
+    'how_to' => [
+        'title' => 'Cum să vă organizați activitatea?',
+        'items' => [
+            '1' => 'Formatul evenimentului dumneavoastră de programare este la latitudinea dumneavoastră, dar vă recomandăm să includeți <strong>activități practice</strong>, care să le permită participanților să creeze pe cont propriu și/sau să meșterească cu componente hardware.',
+            '2' => 'Folosiți <strong>instrumente și tehnologii</strong> adecvate pentru grupul dumneavoastră țintă. Recomandăm utilizarea <a href="http://codeweek.eu/resources/">materialelor disponibile în mod gratuit</a>, de tip open source.',
+            '3' => 'Încurajați participanții <strong>să le arate și să le prezinte</strong> celorlalți ce au creat la sfârșitul evenimentului dumneavoastră.',
+            '4' => '<strong>Răspândiți vestea!</strong> Promovați și partajați pe rețelele de socializare ce ați făcut în timpul activității, folosind hashtagul #CodeWeek. Puteți partaja, de asemenea, în <a
+                        href="https://www.facebook.com/groups/774720866253044/">Grupul profesorilor Săptămânii UE a programării</a> și pe Twitter (<a href="https://twitter.com/CodeWeekEU">@CodeWeekEU</a>). Vorbiți-le prietenilor, colegilor cadre didactice, presei locale și dați un comunicat de presă.',
+            '5' => 'Nu uitați să vă <a href="/add">adăugați activitatea</a> pe <a href="/events">harta Săptămânii programării</a>!',
+        ]
+    ],
+    'material' => [
+        'title' => 'Material promoțional',
+        'text' => '<p>Consultați <a href="http://blog.codeweek.eu/">blogul</a> nostru pentru informații recente și adaptați nevoilor dumneavoastră cele mai recente comunicate de presă sau creați-vă propriul comunicat de presă:</p>',
+        'items' => [
+            '1' => '<a href="https://ec.europa.eu/digital-single-market/en/news/gearing-celebrate-eu-code-week-2019">Pregătiri pentru a sărbători Săptămâna UE a programării 2019</a> (disponibil în 29 de limbi)'
+        ]
+    ],
+    'toolkits' => [
+        'title' => 'Descărcați următoarele seturi de instrumente pentru a vă ajuta să începeți:',
+        'communication_toolkit' => 'Set de instrumente de comunicare',
+        'teachers_toolkit' => 'Set de instrumente pentru profesori'
+    ],
+    'questions' => [
+        'title' => 'Întrebări?',
+        'content' => '<p>Dacă aveți întrebări cu privire la organizarea și promovarea evenimentului dumneavoastră #CodeWeek, luați legătura cu unul dintre <a href="/ambassadors">ambasadorii Săptămânii UE a programării</a> din țara dumneavoastră.</p>'
+    ]
+
+];
