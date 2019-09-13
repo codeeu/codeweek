fed1e2cffc8705779a39909ca876a9f0b264c489
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2019-09-13 09:13:55 +0200

    updating training modules files on aws/s3

--- resources/views/static/training/module-1.blade.php
+++ resources/views/static/training/module-1.blade.php
@@ -23,15 +23,9 @@
             <section class="codeweek-content-wrapper-inside">
 
                 <p>
-                    @if(App::getLocale() == 'en' || App::getLocale() == 'al' ||
-                        App::getLocale() == 'ba' || App::getLocale() == 'it' ||
-                        App::getLocale() == 'mt' || App::getLocale() == 'pl')
-                        <a href="https://s3-eu-west-1.amazonaws.com/codeweek-s3/docs/training/04_EUCodeWeek_Learning+Bit+1_Unplugged_Coding_Video_script.docx">@lang('training.download_video_script')</a>
-                    @else
-                        <a href="https://s3-eu-west-1.amazonaws.com/codeweek-s3/docs/training/{{strtoupper(App::getLocale())}}/CNECT-2018-00222-00-17-{{strtoupper(App::getLocale())}}-TRA-00.DOCX">
-                            @lang('training.download_video_script')
-                        </a>
-                    @endif
+                    <a href="https://s3-eu-west-1.amazonaws.com/codeweek-s3/docs/training/{{strtoupper(App::getLocale())}}/CODEWEEK-TRAINING-001-VIDEO-SCRIPT-{{strtoupper(App::getLocale())}}.DOCX">
+                        @lang('training.download_video_script')
+                    </a>
                 </p>
 
                 <h2>@lang('training.ready_to_share')</h2>
@@ -41,33 +35,21 @@
                 </p>
 
                 <ul>
-                    @if(App::getLocale() == 'en' || App::getLocale() == 'al' || App::getLocale() == 'ba')
-                        <li>
-                            <a href="https://s3-eu-west-1.amazonaws.com/codeweek-s3/docs/training/01_EUCodeWeek_Learning+Bit+1_Unplugged_Coding_Lesson_plan+1_Primary.docx">@lang('training.lessons.1.activities.1')</a>
-                        </li>
-                        <li>
-                            <a href="https://s3-eu-west-1.amazonaws.com/codeweek-s3/docs/training/02_EUCodeWeek_Learning+Bit+1_Unplugged_Coding_Lesson_plan+2_Lower+Second....docx">@lang('training.lessons.1.activities.2')</a>
-                        </li>
-                        <li>
-                            <a href="https://s3-eu-west-1.amazonaws.com/codeweek-s3/docs/training/03_EUCodeWeek_Learning+Bit+1_Unplugged_Coding_Lesson+plan+3_Secondary.docx">@lang('training.lessons.1.activities.3')</a>
-                        </li>
-                    @else
-                        <li>
-                            <a href="https://s3-eu-west-1.amazonaws.com/codeweek-s3/docs/training/{{strtoupper(App::getLocale())}}/CNECT-2018-00222-00-00-{{strtoupper(App::getLocale())}}-TRA-00.DOCX">
-                                @lang('training.lessons.1.activities.1')
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://s3-eu-west-1.amazonaws.com/codeweek-s3/docs/training/{{strtoupper(App::getLocale())}}/CNECT-2018-00222-00-02-{{strtoupper(App::getLocale())}}-TRA-00.DOCX">
-                                @lang('training.lessons.1.activities.2')
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://s3-eu-west-1.amazonaws.com/codeweek-s3/docs/training/{{strtoupper(App::getLocale())}}/CNECT-2018-00222-00-03-{{strtoupper(App::getLocale())}}-TRA-00.DOCX">
-                                @lang('training.lessons.1.activities.3')
-                            </a>
-                        </li>
-                    @endif
+                    <li>
+                        <a href="https://s3-eu-west-1.amazonaws.com/codeweek-s3/docs/training/{{strtoupper(App::getLocale())}}/CODEWEEK-TRAINING-001-ACTIVITY-01-{{strtoupper(App::getLocale())}}.DOCX">
+                            @lang('training.lessons.1.activities.1')
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://s3-eu-west-1.amazonaws.com/codeweek-s3/docs/training/{{strtoupper(App::getLocale())}}/CODEWEEK-TRAINING-001-ACTIVITY-02-{{strtoupper(App::getLocale())}}.DOCX">
+                            @lang('training.lessons.1.activities.2')
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://s3-eu-west-1.amazonaws.com/codeweek-s3/docs/training/{{strtoupper(App::getLocale())}}/CODEWEEK-TRAINING-001-ACTIVITY-03-{{strtoupper(App::getLocale())}}.DOCX">
+                            @lang('training.lessons.1.activities.3')
+                        </a>
+                    </li>
                 </ul>
 
                 <h2>@lang('training.footer.title')</h2>
