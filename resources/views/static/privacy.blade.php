85d90d861828b800c3c6255ceaa7dc6384864123
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2019-09-16 10:47:22 +0200

    update concerning the last document

--- resources/views/static/privacy.blade.php
+++ resources/views/static/privacy.blade.php
@@ -6,142 +6,109 @@
 
         <section class="codeweek-content-wrapper" style="margin-top: 0;">
 
-            <h1>PROTECTION OF YOUR PERSONAL DATA</h1>
+            <h1 style="margin-bottom:10px;">PROTECTION OF YOUR PERSONAL DATA</h1>
 
-            <h3><strong>Introduction</strong></h3>
+            <h3><strong>1. Introduction</strong></h3>
 
-            <p>This privacy statement concerns the collection and publication of personal data, in particular names
-                and contact details as well as photos, on publicly accessible Commission websites such as
-                Codeweek.eu, of persons who serve as contact points for Commission activities (Code Week
-                ambassadors, Educational Ministry Coordinators, Leading Teachers, activity and event
-                organisers and general users of Code Week website) and for that purpose have given their consent
-                to the processing of their personal data</p>
+            <p>The European Commission (hereafter ‘the Commission’) is committed to protect your personal data and to respect your privacy. The Commission collects and further processes personal data pursuant to Regulation (EU) 2018/1725 of the European Parliament and of the Council of 23 October 2018 on the protection of natural persons with regard to the processing of personal data by the Union institutions, bodies, offices and agencies and on the free movement of such data (repealing Regulation (EC) No 45/2001).</p>
 
-            <p>This privacy statement explains the reason for the collection and processing, the way we collect,
-                handle and ensure protection of all personal data provided, how that information is used and what
-                rights you may exercise in relation to your data (the right to access, rectify, block etc.).</p>
+            <p>This privacy statement explains the reason for the collection and processing, the way we collect, handle and ensure protection of all personal data provided, how that information is used and what rights you may exercise in relation to your personal data (the right to access, rectify, block etc.). It also specifies the contact details of the responsible Data Controller with whom you may exercise your rights, the Data Protection Officer and the European Data Protection Supervisor.</p>
 
-            <p>
-                The European institutions are committed to protecting and respecting your privacy. As this
-                service/application collects and further processes personal data, Regulation (EC)
-                N°45/2001<sup><a href="#footer">1</a></sup>
-                and the
-                upcoming equivalent data protection rules for the European institutions and bodies (hereafter the
-                ‘Internal Data Protection Regulation’<sup><a href="#footer">2</a></sup>
-                ) are applicable.
-            </p>
-
-            <h3><strong>Why do we process your data?</strong></h3>
-            <p>
-                <span class="underline">Purpose of the processing operation:</span> The European Commission collects
-                and publishes your personal
-                information to facilitate the identification of contact points by stakeholders or project/programme
-                beneficiaries. Offering real persons as contact points is often the best and most efficient way to
-                ensure that interested people can get in contact with Commission services.
-
-            <ul>
-                <li>article 5 (a): processing is necessary for the performance of a task carried out in the public
-                    interest,
-                    namely the implementation of the EU Code Week as part of the Digital Single Market Strategy. It
-                    is
-                    therefore lawful under Article 5(a) of Regulation (EC) No 45/2001 and the upcoming Internal Data
-                    Protection Regulation.
-                </li>
-            </ul>
-            and/or<br/><br/>
+            <p>This privacy statement concerns the collection and publication of personal data on the publicly accessible Codeweek.eu website, of persons who serve as contact points for EU Code Week activities (Code Week ambassadors, Education Ministry Coordinators, Leading Teachers as well as activity and event organisers).</p>
 
 
-            <ul>
 
-                <li>
-                    article 5(d): The data subject has given his/her unambiguous prior-consent (Article 5(d) of
-                    Regulation (EC) No 45/2001 and the upcoming Internal Data Protection Regulation). The consent is
-                    provided and recorded in a written form.
-                </li>
-            </ul>
-            </p>
+            <h3><strong>2. Why do we process your data?</strong></h3>
+
+            <p>The European Commission collects and publishes your personal information to facilitate the identification of contact points by stakeholders or interested citizens. Offering real persons as contact points is the best and most efficient way to ensure that interested people can get in contact with Commission services.</p>
+
+            <p>Your personal data will not be used for any automated decision-making, including profiling.</p>
 
 
-            <h3><strong>Which data do we collect and process? </strong></h3>
-
-            <p>Personal data collected are information which make your identification as an contact point easier
-                and increase your visibility for the public. This includes in particular: title, name, surname,
-                position,
-                professional postal & e-mail addresses, phone number, picture, social media account, biography.</p>
-
-            <p>These personal data are provided by you on a voluntary basis, by filling out the application
-                form. </p>
 
 
-            <h3><strong>How long do we keep your data?</strong></h3>
+            <h3><strong>3. On what legal ground(s) do we process your personal data</strong></h3>
 
-            <p>DG Connect keeps the data for as long as you serve as an contact point. Personal data may be further
-                kept in a data base for contacting you for participating in future activities if you have agreed to
-                this.</p>
-            <p>
-                All personal data will be deleted from databases 1 year after you have put an end to your functions
-                as an contact point and if you have not given your agreement to be included in the database for
-                future activities.
-            </p>
+            <p>The processing operations on personal data for the publication of contact points are lawful under article 5(1)(d) of the Regulation (EU) 2018/1725 because you gave your consent to the processing of your personal data through a webform or when you agreed that we use your email and username if you signed up with a social network.</p>
 
-            <h3><strong>How do we protect your data?</strong></h3>
 
-            <p>All data in electronic format (e-mails, documents, uploaded batches of data etc.) are stored either
-                on
-                the servers of the European Commission or of its contractors in line with its decision C(2006)3602
-                of
-                16 August 2006 concerning the security of information systems used by the European Commission. </p>
-            <p>The Commission’s contractors are bound by a specific contractual clause for any processing
-                operations of your data on behalf of the Commission.</p>
 
-            <h3><strong>Who has access to your data and to whom is it disclosed?</strong></h3>
+            <h3><strong>4. Which personal data do we collect and further process?</strong></h3>
 
-            <p>All personal data provided by you can be accessed by the websites’ administrators (Commission
-                staff), as well as by other Commission staff on a need to know basis.</p>
-            <p>For the purpose of increasing the contact points’ visibility, your personal data are published on the
-                following website: https://codeweek.eu</p>
-            <p>Your personal data may become part of a database (please see notification DPO-3718) with the
-                purpose of facilitating our future interaction with you as a stakeholder in your areas of interest.
-                If
-                you do not wish to be included in this database, please write to <a
-                        href="mailto:CNECT-STAKEHOLDER-RELATIONS@ec.europa.eu">CNECT-STAKEHOLDER-RELATIONS@ec.europa.eu</a>
-            </p>
-            <p>Your personal data may be used by the European Commission for internal quantitative and
-                qualitative analyses of the stakeholders’ contributions. The results of such analyses may only be
-                published in an anonymised form.</p>
+            <p>Personal data collected are information which make your identification as a contact point easier and increase your visibility for the public, namely: title, name, surname, position, professional postal & e-mail addresses, phone number, picture, social media account, biography.</p>
 
-            <h3><strong>What are your rights and how can you exercise them?</strong></h3>
+            <p>You provided these personal data on a voluntary basis, by filling out the application form.</p>
 
-            <p>You are entitled to have access to your personal data and to request the deletion, blocking or
-                rectification if the data is inaccurate or incomplete. To do so, please contact the data controller as
-                provided in the contact information below (section 8).</p>
-            <p>You may lodge a complaint with the European Data Protection Supervisor:
-                <a href="http://edps.europa.eu">http://edps.europa.eu</a></p>
+            <p>The provision of certain personal data is mandatory for the publication of activities and/or contact information possible on the codeweek.eu website. If you do not provide your personal data, your activity would not be published, and/or you would not be able to take part in the aforementioned networks.</p>
 
-            <h3><strong>Contact information</strong></h3>
+            <p>You provide all other personal data on a voluntary basis.</p>
 
-            <p>If you have comments or questions, any concerns or a complaint regarding the collection and use of
-                your personal data, please feel free to contact the data controller using the following contact
-                information:</p>
-            <p>DG CONNECT Unit F4<br/>
-                Building BU25<br/>
-                B-1049 Brussels <br/>
-                Email: <a href="mailto:info@codeweek.eu">info@codeweek.eu</a></p>
-            <p>You can find more detailed information in the Register of the Data Protection Officer of the
-                Commission: <a href="https://ec.europa.eu/info/about-european-commission/service-standards-and-principles/transparency/data-processing-register_en">https://ec.europa.eu/info/about-european-commission/service-standards-and-principles/transparency/data-processing-register_en</a></p>
-            <p></p>
 
-            <hr>
-<small>
-<a name="footer"></a>
-            <sup>1</sup>
-<a href="http://eur-lex.europa.eu/legal-content/EN/ALL/?uri=CELEX:32001R0045">Regulation (EC) N° 45/2001</a> (OJ L8 of 12/01/2001).<br/>
-            <sup>2</sup>
-            The draft Regulation on the protection of natural persons with regard to the processing of personal data by
-            the Union institutions, bodies, offices and agencies and on the free movement of such data, and repealing
-            Regulation (EC) No 45/2001 and Decision No 1247/2002/EC<br/>
-<a href="http://data.consilium.europa.eu/doc/document/ST-9296-2018-INIT/en/pdf">http://data.consilium.europa.eu/doc/document/ST-9296-2018-INIT/en/pdf</a>
-</small>
+
+            <h3><strong>5. How long do we keep your personal data?</strong></h3>
+
+            <p>The Commission only keeps your personal data for the time necessary to fulfil the purpose of collection or further processing described in the point 2, namely for as long as you serve as a contact point.</p>
+
+            <p>Your personal data will be removed from the publicly available website as soon as you cease your contact point function unless you have given your agreement to be included in the database for future activities.</p>
+
+
+
+            <h3><strong>6. How do we protect and safeguard your personal data?</strong></h3>
+
+            <p>All personal data in electronic format (e-mails, documents, databases, uploaded batches of data, etc.) are stored either on the servers of the European Commission or of its contractor. All processing operations are carried out pursuant to the Commission Decision (EU, Euratom) 2017/46 of 10 January 2017 on the security of communication and information systems in the European Commission.</p>
+
+            <p>The Commission’s contractors are bound by a specific contractual clause for any processing operations of your data on behalf of the Commission, and by the confidentiality obligations deriving from the transposition of the General Data Protection Regulation in the EU Member States (‘GDPR’ Regulation (EU) 2016/679).</p>
+
+            <p>In order to protect your personal data, the Commission has put in place a number of technical and organisational measures in place. Technical measures include appropriate actions to address online security, risk of data loss, alteration of data or unauthorised access, taking into consideration the risk presented by the processing and the nature of the personal data being processed. Organisational measures include restricting access to the personal data solely to authorised persons with a legitimate need to know for the purposes of this processing operation.</p>
+
+
+
+
+            <h3><strong>7. Who has access to your data and to whom is it disclosed?</strong></h3>
+
+            <p>Access to your personal data is provided to the Commission staff responsible for carrying out this processing operation and to authorised staff according to the “need to know” principle. Such staff abide by statutory, and when required, additional confidentiality agreements.</p>
+
+            <p>Concretely, all personal data provided by you can be accessed by the websites’ administrators (Commission staff), as well as by other Commission staff on a need to know basis.</p>
+
+            <p>For the purpose of increasing the contact points’ visibility, your personal data are published without any access limitation on the public website: https://codeweek.eu.</p>
+
+            <p><i>Regarding transfer of data to third parties</i></p>
+
+            <p>The information we collect will not be given to any third party, except to the extent and for the purpose we may be required to do so by law.</p>
+
+
+
+
+            <h3><strong>8. What are your rights and how can you exercise them?</strong></h3>
+
+            <p>You have specific rights as a ‘data subject’ under Chapter III (Articles 14-25) of Regulation (EU) 2018/1725, in particular the right to access, rectify or erase your personal data and the right to restrict the processing of your personal data. Where applicable, you also have the right to object to the processing or the right to data portability.</p>
+            <p>You have consented to provide your personal data to us for the present processing operation and you can withdraw your consent at any time by notifying the Data Controller. The withdrawal will not affect the lawfulness of the processing carried out before you have withdrawn the consent.</p>
+            <p>You can exercise your rights by contacting the Data Controller, or in case of conflict the Data Protection Officer. If necessary, you can also address the European Data Protection Supervisor. Their contact information is given under Heading 9 below.</p>
+
+
+
+            <h3 style="margin-bottom: 10px;"><strong>9. Contact information</strong></h3>
+
+            <h4><strong>-The Data Controller</strong></h4>
+
+            <p>If you would like to exercise your rights under Regulation (EU) 2018/1725, or if you have comments, questions or concerns, or if you would like to submit a complaint regarding the collection and use of your personal data, please feel free to contact the Data Controller,</p>
+
+            <address style="margin-bottom: 10px;">
+                DG CONNECT Unit F4<br>
+                Building BU25<br>
+                B-1049 Brussels<br>
+                Email: <a href="mailto:info@codeweek.eu">info@codeweek.eu</a>
+            </address>
+
+            <h4><strong>-The Data Protection Officer (DPO) of the Commission</strong></h4>
+
+            <p>You may contact the Data Protection Officer (<a href="mailto:DATA-PROTECTION-OFFICER@ec.europa.eu">DATA-PROTECTION-OFFICER@ec.europa.eu</a>) with regard to issues related to the processing of your personal data under Regulation (EU) 2018/1725.</p>
+
+            <h4><strong>-The European Data Protection Supervisor (EDPS)</strong></h4>
+
+            <p>You have the right to have recourse (i.e. you can lodge a complaint) to the European Data Protection Supervisor (<a href="mailto:edps@edps.europa.eu">edps@edps.europa.eu</a>) if you consider that your rights under Regulation (EU) 2018/1725 have been infringed as a result of the processing of your personal data by the Data Controller.</p>
+
+
 
         </section>
 
