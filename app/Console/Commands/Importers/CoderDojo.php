b7e86c64161d68cfb6eca4b8b04fd00cf09b4674
Alain Van Driessche <alain.van-driessche@ext.ec.europa.eu>
2019-10-01 15:49:29 +0200

    CoderDojo Import

---
+++ app/Console/Commands/Importers/CoderDojo.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands\Importers;
+
+use App\Helpers\ImporterHelper;
+use App\Imports\CoderDojoEventsImport;
+use App\Imports\EventsImport;
+use App\Imports\RemoteImporter;
+use Exception;
+use Illuminate\Console\Command;
+use Feeds;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
+use Maatwebsite\Excel\Facades\Excel;
+
+
+class CoderDojo extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:coderdojo';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import the data from CoderDojo';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Log::info("Loading CoderDojo");
+
+        Excel::import(new CoderDojoEventsImport, 'coderdojo2019.xlsx','excel');
+
+
+        // Process the events
+
+    }
+}
