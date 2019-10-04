a36a885d19f2d2c7bcba4e9f17684b5b1e387608
Alain Van Driessche <alain.van-driessche@ext.ec.europa.eu>
2019-10-04 09:23:28 +0200

    Meet and code mapping of activities

--- app/MeetAndCodeRSSItem.php
+++ app/MeetAndCodeRSSItem.php
@@ -9,18 +9,24 @@ use Illuminate\Support\Str;
 class MeetAndCodeRSSItem extends Model
 {
 
-   public function getCounryIso(){
-       dump($this->country);
-       switch ($this->country){
-           case 'North Macedonia': return 'MK';
-           case 'Italia': return 'IT';
-           case 'Bosnia&Herzegovina': return 'BA';
-           default:
-               return Country::where('name','like',$this->country)->first()->iso;
-       }
+    public function getCounryIso()
+    {
 
-   }
-    public function createEvent($technicalUser){
+        switch ($this->country) {
+            case 'North Macedonia':
+                return 'MK';
+            case 'Italia':
+                return 'IT';
+            case 'Bosnia&Herzegovina':
+                return 'BA';
+            default:
+                return Country::where('name', 'like', $this->country)->first()->iso;
+        }
+
+    }
+
+    public function createEvent($technicalUser)
+    {
 
         $event = new Event([
             'status' => "APPROVED",
@@ -28,7 +34,8 @@ class MeetAndCodeRSSItem extends Model
             'slug' => Str::slug($this->title),
             'organizer' => $this->school_name,
             'description' => $this->description,
-            'organizer_type' => $this->organisation_type,
+            'organizer_type' => $this->mapOrganisationTypes($this->organisation_type),
+            'activity_type' => $this->mapActivityTypes($this->activity_type),
             'location' => $this->address,
             'event_url' => $this->link,
             'user_email' => $this->organiser_email,
@@ -47,6 +54,30 @@ class MeetAndCodeRSSItem extends Model
         ]);
 
         $event->save();
+
+    }
+
+    private function mapOrganisationTypes($organisation_type)
+    {
+        switch ($organisation_type) {
+            case "Non-Profit Organisation":
+                return "non profit";
+            default:
+                return "other";
+        }
+
+
+    }
+
+    private function mapActivityTypes($activity_type)
+    {
+        switch ($activity_type) {
+            case "offline and open":
+                return "open-in-person";
+            default:
+                return "other";
+        }
+
 
     }
 }
