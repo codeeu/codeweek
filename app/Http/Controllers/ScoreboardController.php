d7817be2e843f0bf356dddb5aefc33564850670e
Alain Van Driessche <alainvd@gmail.com>
2019-10-14 11:43:58 +0200

    Caching Scoreboard Results

--- app/Http/Controllers/ScoreboardController.php
+++ app/Http/Controllers/ScoreboardController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Event;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ScoreboardController extends Controller
 {
@@ -17,26 +18,38 @@ class ScoreboardController extends Controller
             $edition = Carbon::now('Europe/Brussels')->year;
         }
 
-        $total =  DB::table('events')
-            ->where('status',"=","APPROVED")
-            ->whereYear('end_date', '=', $edition)
-            ->count();
+        $cache_time = 60 * 60 * 24 * 365;
+        if ($edition == Carbon::now('Europe/Brussels')->year) {
+            $cache_time = 5 * 60;
+        };
 
-        $events = DB::table('events')
-            ->join('countries', 'events.country_iso', '=', 'countries.iso')
-            ->select('countries.iso as country_iso', 'countries.name as country_name','countries.population as country_population', DB::raw('count(*) as total'), DB::raw('countries.population / count(*) as rank'))
-            ->where('status',"=","APPROVED")
-            ->whereYear('start_date', '=', $edition)
-            ->groupBy('countries.iso')
-            ->orderBy('rank','asc')
-            ->get();
 
+        $total = Cache::remember('total_' . $edition, $cache_time, function () use ($edition) {
+            Log::info("Setting cache for scoreboard total in " . $edition);
+            return DB::table('events')
+                ->where('status', "=", "APPROVED")
+                ->whereYear('end_date', '=', $edition)
+                ->count();
+        });
+
+
+        $events = Cache::remember('events_' . $edition, $cache_time, function () use ($edition) {
+            Log::info("Setting cache for scoreboard events in " . $edition);
+            return DB::table('events')
+                ->join('countries', 'events.country_iso', '=', 'countries.iso')
+                ->select('countries.iso as country_iso', 'countries.name as country_name', 'countries.population as country_population', DB::raw('count(*) as total'), DB::raw('countries.population / count(*) as rank'))
+                ->where('status', "=", "APPROVED")
+                ->whereYear('start_date', '=', $edition)
+                ->groupBy('countries.iso')
+                ->orderBy('rank', 'asc')
+                ->get();
+        });
 
         $years = [2019, 2018, 2017, 2016, 2015, 2014];
         return view('scoreboard', [
-            'events'=>$events,
-            'total'=>$total,
-            'edition'=>$edition,
+            'events' => $events,
+            'total' => $total,
+            'edition' => $edition,
             'years' => $years
         ]);
     }
