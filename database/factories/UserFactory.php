8a9456924593a9f3e6a720e3ff8216f9194f3df8
Alain Van Driessche <alainvd@gmail.com>
2019-10-30 15:34:49 +0100

    Unit Tests Fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,8 +21,8 @@ $factory->define(App\User::class, function (Faker $faker) {
 
     return [
         'firstname' => $faker->firstName,
-        'lastname' => $faker->word,
-        'username' => $faker->word,
+        'lastname' => $faker->firstName,
+        'username' => $faker->firstName,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
