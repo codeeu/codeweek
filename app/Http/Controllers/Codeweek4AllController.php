e90253c1533c653112c0e6057111743360baf6f2
Alain Van Driessche <alainvd@gmail.com>
2020-01-06 14:35:46 +0100

    Add year 2020 to Codeweek

--- app/Http/Controllers/Codeweek4AllController.php
+++ app/Http/Controllers/Codeweek4AllController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Event;
 use App\Helpers\Codeweek4AllHelper;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class Codeweek4AllController extends Controller
 {
-    public function detail($code)
+    public function detail($code, $edition= 2019)
     {
-$edition = 2019;
+        $edition = 2019;
         $result = Codeweek4AllHelper::getDetailsByCodeweek4All([$code],$edition)->first();
         $countries = Codeweek4AllHelper::getCountriesByCodeweek4All($code, $edition);
         $initiator = Codeweek4AllHelper::getInitiatorByCodeweek4All([$code]);
