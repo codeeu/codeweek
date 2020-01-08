ef0e15fe2a6947da83bbd7fe10303bd3121af696
Alain Van Driessche <alainvd@gmail.com>
2020-01-08 11:05:32 +0100

    Certificate of Excellence 2019

---
+++ resources/lang/en/excellence.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'title' => 'Report Excellence for Codeweek edition of',
+    'generate' => 'Generate the Certificate of Excellence',
+    'required'=>'Required fields are marked with an * asterisk.',
+    'name_for_certificate' => [
+        'label' => 'Name for certificate',
+        'help' => 'Change this to the name of the activity organizer who will be issued a certificate of Excellence.'
+    ],
+
+
+];
