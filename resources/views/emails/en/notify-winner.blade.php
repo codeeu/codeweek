84fb50a2ad6e53f0f8d33adac87995a215cdbcd4
Alain Van Driessche <alain.van-driessche@ext.ec.europa.eu>
2020-01-10 07:39:40 +0100

    Update email CW4ALL

--- resources/views/emails/en/notify-winner.blade.php
+++ resources/views/emails/en/notify-winner.blade.php
@@ -1,19 +1,15 @@
 @component('mail::message')
 Dear Code Week 4 All challenge participant,
 
-
-
 We are delighted to share with you today that you have successfully completed the Code Week 4 All challenge.<br/>
-
-
 
 We would like to extend our warmest congratulations and thank you for playing a major part in making last year’s Code Week, <a href="https://ec.europa.eu/digital-single-market/en/news/eu-code-week-2018-breaks-all-time-record-27-million-participants-and-nearly-44000-events">the most successful edition yet.</a><br/>
 
-It is our belief that the creation of networks between teachers and educators all over Europe is vital to bringing coding to more students across Europe – and you have helped up move closer to this goal!<br/>
+It is our belief that the creation of alliances between teachers and educators all over Europe is vital to bringing coding to more students across Europe – and you have helped up move closer to this goal!<br/>
 
 
-
-You can personalize and download your Code Week 4 all certificate by following this link:<br/>
+You can personalise and download your Code Week 4 all certificate by clicking on the link in this button:
+<br/>
 
 
 @component('mail::button', ['url' => env('APP_URL') . "/certificates/excellence/" . $edition])
@@ -21,8 +17,7 @@ You can personalize and download your Code Week 4 all certificate by following t
 @endcomponent
 
 
-
-Thank you very much for helping us bring coding into more schools than ever before, and advance digital literacy all over Europe! We hope to work together again in the 2019 edition of EU Code Week, which will take place from October 5-20.<br/><br/>
+Thank you very much for helping us advance digital literacy all over Europe! We hope to work together again in the 2020 edition of EU Code Week, which will take place from 10-25 October.<br/><br/>
 
 
 
