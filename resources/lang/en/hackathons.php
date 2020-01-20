ae66709e8229ede59cbb9d5d986c9f9332b7f86b
Alain Van Driessche <alainvd@gmail.com>
2020-01-20 17:51:35 +0100

    First layout with translations

--- resources/lang/en/hackathons.php
+++ resources/lang/en/hackathons.php
@@ -2,7 +2,32 @@
 
 return [
 
-    'title' => 'Hackathons',
-    'intro' => 'Welcome to Codeweek Hackathons Landing page'
+    'title' => 'Be a true digital creator!',
+    'intro' => '6 events, 6 challenges',
+
+    '1' =>  [
+        'title'=>"Italy",
+        'details'=>'Italian Details'
+    ],
+    '2' =>  [
+        'title'=>"Italy",
+        'details'=>'Italian Details'
+    ],
+    '3' =>  [
+        'title'=>"Italy",
+        'details'=>'Italian Details'
+    ],
+    '4' =>  [
+        'title'=>"Italy",
+        'details'=>'Italian Details'
+    ],
+    '5' =>  [
+        'title'=>"Italy",
+        'details'=>'Italian Details'
+    ],
+    '6' =>  [
+        'title'=>"Italy",
+        'details'=>'Italian Details'
+    ],
 
 ];
--- resources/views/hackathons/index.blade.php
+++ resources/views/hackathons/index.blade.php
@@ -18,12 +18,81 @@
 
         <section class="codeweek-content-wrapper">
 
+            <section class="codeweek-content-wrapper-inside">
 
-            @lang("hackathons.intro")
+                <h1>@lang('hackathons.intro')</h1>
 
 
+
+            </section>
+
+            <section class="hackathons-content-grid">
+                <div class="codeweek-card-grid">
+                    <a href="/training/coding-without-computers">
+                        <img src="/img/learning/coding-without-computers.png">
+                        <div class="title">@lang('hackathons.1.title')</div>
+                        <div class="author">@lang('hackathons.1.details')</div>
+                        <div class="more"><a href="https://www.42.us.org/" target="_blank" class="codeweek-action-link-button" style="margin:5px;">View</a></div>
+                    </a>
+                </div>
+
+                <div class="codeweek-card-grid">
+                    <a href="/training/coding-without-computers">
+                        <img src="/img/learning/coding-without-computers.png">
+                        <div class="title">@lang('hackathons.2.title')</div>
+                        <div class="author">@lang('hackathons.2.details')</div>
+                    </a>
+                </div>
+
+                <div class="codeweek-card-grid">
+                    <a href="/training/coding-without-computers">
+                        <img src="/img/learning/coding-without-computers.png">
+                        <div class="title">@lang('hackathons.3.title')</div>
+                        <div class="author">@lang('hackathons.3.details')</div>
+                    </a>
+                </div>
+
+                <div class="codeweek-card-grid">
+                    <a href="/training/coding-without-computers">
+                        <img src="/img/learning/coding-without-computers.png">
+                        <div class="title">@lang('hackathons.4.title')</div>
+                        <div class="author">@lang('hackathons.4.details')</div>
+                    </a>
+                </div>
+
+                <div class="codeweek-card-grid">
+                    <a href="/training/coding-without-computers">
+                        <img src="/img/learning/coding-without-computers.png">
+                        <div class="title">@lang('hackathons.5.title')</div>
+                        <div class="author">@lang('hackathons.5.details')</div>
+                    </a>
+                </div>
+
+                <div class="codeweek-card-grid">
+                    <a href="/training/coding-without-computers">
+                        <img src="/img/learning/coding-without-computers.png">
+                        <div class="title">@lang('hackathons.6.title')</div>
+                        <div class="author">@lang('hackathons.6.details')</div>
+                    </a>
+                </div>
+
+
+
+            </section>
+
+            <section class="codeweek-content-wrapper-inside">
+
+                @lang('hackathons.text')
+
+            </section>
 
         </section>
+
+
+
+
+
+
     </section>
 
 @endsection
