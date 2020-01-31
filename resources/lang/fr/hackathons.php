24475777565b0b92599824e59ce357a81290dcd7
Alain Van Driessche <alain.van-driessche@ext.ec.europa.eu>
2020-01-31 19:14:40 +0100

    About Translations

---
+++ resources/lang/fr/hackathons.php
@@ -0,0 +1,91 @@
+<?php
+
+return [
+
+    'title' => 'Hackathons de la Semaine européenne du code',
+    'subtitle' => 'Donnez vie à vos idées!',
+    'more-info' => 'more info',
+    'sections' => [
+        1 => [
+            'title' => '6 hackathons, 6 défis',
+            'content' => [
+                1 => 'Vous habitez en Lettonie, en Irlande, en Italie, en Roumanie, en Slovénie ou en Espagne? C’est l’occasion de faire appel à votre créativité, votre curiosité, votre dynamisme et vos compétences techniques. Participez à l’un des hackathons de la Semaine européenne du code et développez une solution innovante qui profitera à la société.',
+                2 => 'En 2020, la Semaine européenne du code propose six hackathons extraordinaires dans six pays européens et invite les élèves des lycées (16-19 ans) à utiliser leurs compétences en codage pour proposer des solutions concernant un enjeu local, en 36 heures.'
+            ]
+        ],
+        2 => [
+            'title' => 'Comment puis-je participer?',
+            'content' => [
+                1 => 'Sélectionnez le hackathon de votre pays et suivez simplement les quelques étapes permettant de vous inscrire. Si vous souhaitez vous inscrire avec des amis ou des camarades de classe, n’oubliez pas d’indiquer le nom de votre équipe. Chaque hackathon ouvrira sa procédure d’inscription séparément.'
+            ]
+        ],
+        3 => [
+            'title' => 'Qui sont les organisateurs?',
+            'content' => [
+                1 => 'Les hackathons de la Semaine européenne du code sont co-organisés par la Commission européenne et par les ',
+                2 => 'ambassadeurs locaux de la Semaine européenne du code',
+                3 => ', et ils sont financés par le Parlement européen. L’objectif consiste à montrer comment des solutions concrètes prennent vie grâce à la créativité des jeunes, ainsi qu’à leur enthousiasme, leurs idées nouvelles et leurs compétences en matière de codage.'
+            ]
+        ],
+        4 => [
+            'title' => 'À quoi ressemble un hackathon?',
+            'content' => [
+                1 => 'Chaque hackathon verra 60 lycéens s’affronter par équipes pendant 36 heures. À la fin, chaque équipe présentera sa solution à un jury d’experts. Des mentors expérimentés encadreront les équipes et des ateliers seront organisés pour permettre aux participants d’acquérir de nouvelles compétences et de s’amuser. Les hackathons offrent également aux participants une excellente occasion de nouer des contacts et de discuter avec des personnes du milieu technologique local.',
+                2 => 'L’équipe gagnante de chaque hackathon local sera récompensée par différents prix, notamment la possibilité de se rendre à Bruxelles et de présenter sa solution à un public international le 12 octobre 2020.',
+            ]
+        ],
+        5 => [
+            'title' => 'Je ne connais pas le codage – puis-je faire quelque chose?',
+            'content' => [
+                1 => 'Parallèlement au hackathon, il y aura des ateliers dédiés aux débutants en codage, au bricolage de matériel, à la robotique, etc. pour que les participants acquièrent quelques bases en matière de pensée computationnelle et de codage. Pour plus d’informations sur les inscriptions, consultez la page locale qui vous concerne.'
+            ]
+        ],
+        6 => [
+            'title' => 'Prenez part au jeu!',
+            'content' => [
+                1 => '',
+                2 => '',
+                3 => '',
+                4 => '',
+                5 => '',
+                6 => '',
+            ]
+        ],
+
+        7 => [
+            'title' => 'Partenaires',
+        ],
+
+    ],
+
+    'cities' => [
+        1 => [
+            'title' => "HACKATHON 01",
+            'subtitle' => "BRUSSELS",
+            'date' => "April 25-26, 2020",
+            'location' => "Brussels, Belgium"
+        ],
+        2 => [
+            'title' => "HACKATHON 02",
+            'subtitle' => "DÜSSELDORF",
+            'date' => "May 8-9, 2020",
+            'location' => "Düsseldorf, Germany"
+        ],
+        3 => [
+            'title' => "HACKATHON 03",
+            'subtitle' => "Coming soon...",
+        ],
+        4 => [
+            'title' => "HACKATHON 04",
+            'subtitle' => "Coming soon...",
+        ], 5 => [
+            'title' => "HACKATHON 05",
+            'subtitle' => "Coming soon...",
+        ], 6 => [
+            'title' => "HACKATHON 06",
+            'subtitle' => "Coming soon...",
+        ],
+    ],
+
+
+];
