3a6f3e738f2419804da9447101e70c1697dff13d
Alain VD <alainvd@gmail.com>
2020-04-30 16:38:44 +0200

    Pages with lessons

--- app/Http/Controllers/CodingAthomeController.php
+++ app/Http/Controllers/CodingAthomeController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class CodingAtHomeController extends Controller
 {
     public function show(){
-        return view('codingathome');
+        return view('codingathome.codingathome');
     }
+
+
 }
---
+++ resources/views/codingathome/banner.blade.php
@@ -0,0 +1,9 @@
+<section class="codeweek-banner learn-teach">
+    <div class="text">
+        <h1>#CodeWeek</h1>
+        <h2>@lang('coding-at-home.title')</h2>
+    </div>
+    <div class="image">
+        <img src="/images/banner_training.svg" class="static-image">
+    </div>
+</section>
