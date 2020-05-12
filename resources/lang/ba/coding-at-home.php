b4e15acd8cb19673ffdd0774e9bccd3d90e97120
Alain VD <alainvd@gmail.com>
2020-05-12 17:10:56 +0200

    All the translations for Coding@Home

---
+++ resources/lang/ba/coding-at-home.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+
+    'title' => 'Coding@Home',
+    'questions' => 'Pitanja',
+    'material' => [
+        "required" => "Potrebni materijal",
+        "chequered" => "šahovska ploča",
+        "footprint" => "pločice sa slikama otiska stopala"
+    ],
+    'intro' => [
+        'title' => 'Uvod u Coding@Home',
+    ],
+    'explorer' => [
+        'title' => 'Istraživač',
+        'text' => 'Istraživač je prva aktivnost u Coding@Home. Pomjerajte istraživača preko ploče da dosegnete metu nakon što posjetite što je moguće više kvadratića. ',
+        'questions' => [
+            'content' =>
+                [
+                    1 => 'P1. Sa polaznim i završnim pozicijama prikazanim u video snimku, da li je moguće da istraživač posjeti sva polja na ploči?',
+                    2 => 'P2. Koje polazne i završne pozicije spriječavaju istraživača da posjeti najveći broj polja na ploči?'
+                ]
+
+        ]
+
+    ],
+
+    'right-and-left' => [
+        'title' => 'Desno i lijevo',
+        'text' => 'Desno i lijevo je takmičarska igra saradnje. Dva tima sarađuju da kreiraju putanju prema meti, dok se takmiče da upotrijebe što je moguće više polja koja sui ma dodijeljena: žuti tim pokušava da ubaci što je moguće više skretanja ulijevo a crveni tim pokušava da ubaci što je moguće više skretanja udesno.',
+        'questions' => [
+            'content' =>
+                [
+                    1 => 'P1. Sa početkom i ciljem postavljenim kao u prvom meču u ovom video snimku, da li je moguće da jedan od ova dva tima pobjedi?',
+                    2 => 'P2. Sa početkom i ciljem postavljenim kao u igri u kojoj je pobijedila Ana, može li biti neriješen rezultat?',
+                    3 => 'P3. Postoje li početne i završne postavke koje više odgovaraju jednom od timova?',
+                    4 => 'P4. Uzimajući u obzir raspored između početnih i završnih pozicija, da li je moguće predvidjeti koliki će biti razmak između tima koji pobjeđuje i tima koji gubi?'
+                ]
+
+        ]
+
+    ],
+
+    'texts' => [
+        1 => '"Coding@Home" su kratki video snimci sa uradi sam materijalima, slagalicama, zanimljivim igrama i izazovima kodiranja za svakodnevnu upotrebu u kući kao i u školi. Nije vam potrebno nikakvo predznanje u kodiranju niti su vam potrebni blo kakvi elektronski uređaji da bi uradili aktivnosti. Aktivnosti će podstaknuti računarsko razmišljanje i njegovati vještine učenika, roditelja i učitelja u kući ili u školi.',
+        2 => 'Serija Coding@Home EU Sedmice Kodiranja se zasniva na <a href="https://www.raicultura.it/speciali/codinginfamiglia/" target="_blank">“Coding in famiglia”</a> inicijativi Urbino Univerziteta i CodeMOCCnet Udruženja u saradnji sa RAI Cultura. Aktivnosti predstavlja Alessandro Bogliolo profesor Informacionih Procesnih Sistema na Urbino Univerzitetu, <a href="/ambassadors?country_iso=IT" target="_blank">Italijanski ambasador EU Sedmice Kodiranja</a> i koordinator svim ambasadorima kao i članovima Upravnog odbora Digital Skills and Jobs Koalicije.',
+        3 => 'Ako vas interesuje više o unplugged aktivnostima, ili aktivnostima u različitim programskim jezicima, robotici, micro:bit, itd., pogledajte <a href="/training">EU Sedmica Kodiranja “Learning Bits”</a> sa video vježbama i planovim lekcija za osnovne i srednje škole. Takođe pogledajte stranicu sa resursima EU Sedmice Kodiranja za <a href="/resources/learn">učenike</a> i <a href="/resources/teach">učitelje</a>. '
+    ]
+];
---
+++ resources/lang/rs/coding-at-home.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+
+    'title' => 'Coding@Home',
+    'questions' => '',
+    'material' => [
+        "required" => "Materialet e nevojshme",
+        "chequered" => "tabela me katrorë",
+        "footprint" => "pllakëza me imazhe të gjurmëve"
+    ],
+    'intro' => [
+        'title' => 'Hyrje në Coding@Home',
+    ],
+    'explorer' => [
+        'title' => 'Eksploruesi',
+        'text' => 'Eksploruesi është aktiviteti i parë i Coding@Home. Zhvendoseni eksploruesin nëpër tabelë për të arritur objektivin pasi të përshkoni sa më shumë katrorë. ',
+        'questions' => [
+            'content' =>
+                [
+                    1 => 'P1. Me pozicionet e fillimit dhe mbarimit të paraqitura në video, a është e mundur që eksploruesi të përshkojë të gjitha kutitë në tabelë?',
+                    2 => 'P2. Cilat pozicione fillimi dhe mbarimi e pengojnë eksploruesin të përshkojë numrin më të madh të kutive në tabelë?'
+                ]
+
+        ]
+
+    ],
+
+    'right-and-left' => [
+        'title' => 'Djathtas dhe majtas',
+        'text' => 'Djathtas dhe e majtas është një lojë konkurruese dhe bashkëpunuese. Të dy ekipet bashkëpunojnë për të krijuar një rrugëtim drejt objektivit, ndërsa ata garojnë të përdorin sa më shumë të pllakëza që u janë caktuar: skuadra e verdhë përpiqet të futë sa më shumë kthesa në të majtë dhe skuadra e kuqe përpiqet të futë sa më shumë kthesa në të djathtë të jetë e mundur.',
+        'questions' => [
+            'content' =>
+                [
+                    1 => 'P1. Me fillimin dhe mbarimin e vendosur si në lojën e parë në këtë video, a është e mundur që një nga dy skuadrat të fitojë?',
+                    2 => 'P2. Me fillimin dhe mbarimin e vendosur si në lojën e fituar nga Ana, a mund të ketë barazim?',
+                    3 => 'P3. A ka ndonjë vendosje fillimi dhe mbarimi që favorizon njërën nga dy skuadrat?',
+                    4 => 'P4. Duke pasur parasysh prirjen ndërmjet pozicioneve të fillimit dhe të mbarimit, a është e mundur të parashikoni sa do të jetë boshllëku midis ekipit fitues dhe humbës?'
+                ]
+
+        ]
+
+    ],
+
+    'texts' => [
+        1 => 'Coding@Home është një koleksion i videove të shkurtra, modeleve që i bëni vetë, gjëegjëzave, lojërave dhe sfidave të kodimit për përdorim të përditshëm në familje, si dhe në shkollë. Ju nuk duhet të keni njohuri të mëparshme ose pajisje elektronike për të kryer aktivitetet. Aktivitetet stimulojnë mendimin llogaritës dhe kultivojnë aftësitë e nxënësve, prindërve dhe mësuesve në shtëpi ose në shkollë.',
+        2 => 'Seria Coding@Home e EU Code Week bazohet në iniciativës <a href="https://www.raicultura.it/speciali/codinginfamiglia/" target="_blank">“Coding in famiglia”</a> të Universitetit të Urbinos dhe Shoqatës CodeMOOCnet në bashkëpunim me Rai Cultura. Alessandro Bogliolo është Profesori i Sistemeve të Përpunimit të Informacionit në Universitetin e Urbinos, <a href="/ambassadors?country_iso=IT" target="_blank">Ambasador i EU Code Week</a> dhe koordinator i të gjithë ambasadorëve si dhe anëtar i Bordit Drejtues të Koalicionit të Aftësive Dixhitale dhe Punës. ',
+        3 => 'Nëse jeni të interesuar për aktivitete më të shkëputura ose aktivitete në gjuhë të ndryshme programimi, robotikë, mikro:bit etj., vizitoni <a href="/training">EU Code Week “Mësimet”</a> me video mësimore dhe plane mësimore për shkollat e ciklit fillor, të mesme të ulëta dhe të mesme. Shikoni gjithashtu faqen e burimeve të EU Code Week për <a href="/resources/learn">nxënësit</a> dhe <a href="/resources/teach">mësuesit</a>. '
+    ]
+];
