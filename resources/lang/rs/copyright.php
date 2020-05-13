b3edec6aeb426c02b8e25d9528197d27128fe936
Alain VD <alainvd@gmail.com>
2020-05-13 16:47:21 +0200

    Copyright translations

---
+++ resources/lang/rs/copyright.php
@@ -0,0 +1,18 @@
+<?php
+
+
+return [
+    'title' => 'Obaveštenje o autorskim pravima',
+    'training' => [
+        0 => 'Veb stranica EU Code Veek za škole',
+        1 => 'je usluga koju podržava Evropska komisija'
+    ],
+    'licence' => [
+        0 => 'Osim ako je drugačije navedeno, sadržaj dostupan na ovoj veb lokaciji licenciran je pod licencom',
+        1 => 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.sr_LATN',
+        2 => 'Creative Commons Attribution-NonCom Commercial-ShareAlike 4.0 International (CC BI-NC-SA 4.0)'
+    ],
+    'creative-commons' => 'Licenciranje pod Creative Commons licencama samo po sebi ne utiče na vlasništvo nad autorskim pravima',
+    'third-party' => 'Sadržaj sa veb lokacija trećih strana podleže sopstvenim ograničenjima autorskih prava; molimo posetite veb lokaciju za dodatne informacije'
+
+];
---
+++ resources/lang/sv/copyright.php
@@ -0,0 +1,23 @@
+<?php
+
+
+return [
+    'title' => 'Upphovsrättsmeddelande',
+    'training' => [
+        0 => 'EU:s Code Week-webbplats för skolor',
+        1 => 'är en tjänst som stöds av Europeiska kommissionen'
+    ],
+    'licence' => [
+        0 => 'Om inget annat anges är innehåll som görs tillgängligt på denna webbplats licenserat under en',
+        1 => 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.sv',
+        2 => 'Erkännande-IckeKommersiell-DelaLika  4.0 Internationell licens (CC BY-NC-SA 4.0)'
+    ],
+    'creative-commons' => 'Licensiering under Creative Commons-licenser påverkar inte i sig äganderätten till upphovsrätten',
+    'third-party' => 'Innehåll från tredje parts webbplatser är underkastade sina egna upphovsrättsbegränsningar; se ursprungssidan för mer information'
+
+
+];
+
+
+
+
