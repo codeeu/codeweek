2782d67b9b9be8542f7b7ec8fe0f67a6a49c7f6f
Alain VD <alainvd@gmail.com>
2020-05-18 17:19:43 +0200

    - ALbanian - Setup for other languages

---
+++ resources/lang/al/mooc.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'free-online-courses' => 'Kurse falas në internet',
+    'intro' => 'EU Code Week ofron mundësi të zhvillimit profesional në formën e kurseve në internet. Qëllimi është të mbështesim mësuesit në klasë të sjelljes së kodimit dhe mendimit llogaritës.',
+    'icebreaker' =>
+        [
+            'title' => 'Kursi prezantues “Icebreaker”',
+            'text' => [
+                0 => 'EU Code Week ofron mundësi të zhvillimit profesional në formën e kurseve në internet. Qëllimi është të mbështesim mësuesit në klasë të sjelljes së kodimit dhe mendimit llogaritës.',
+                1 => 'Kursi Icebreaker në EU Code Week',
+                2 => 'është një kurs pesë-orësh në anglisht që synon të ndiqet nga gjithë të interesuarit në bazat e kodifikimit dhe mendimin llogaritës. Pjesëmarrësit mësojnë se si të frymëzojnë kuriozitet dhe një frymë inovative tek të rinjtë, ndërsa i fuqizojnë të bëhen krijues dixhital. Kursi ndihmon pjesëmarrësit të zbulojnë përfitimet dhe rëndësinë e mendimit llogaritës dhe kodimit në jetën tonë të përditshme. Gjithashtu ofron ide, materiale trajnimi falas dhe burime për të organizuar aktivitete argëtuese dhe edukative për fëmijët, në çdo kohë, kudo - veçanërisht gjatë Code Week.',
+                3 => 'Ju nuk keni nevojë për përvojë ose njohuri të mëparshme në kodim për të marrë pjesë në këtë kurs, thjesht një mendje kureshtare.',
+
+            ],
+            'registration' => [
+                0 => 'Regjistrimet janë të hapura',
+                1 => 'këtu për kursin që zhvillohet midis 11 majit dhe 15 qershorit 2020. Ju lutemi vini re se ju duhet të krijoni një llogari në European Schoolnet Academy për t\'u regjistruar. ',
+            ],
+            'check-out' => 'Shikoni botimin 2019'
+        ],
+    "deep-dive" => [
+        'title' => 'Kursi i thelluar i "Deep Dive"',
+        'text' => [
+            0 => 'Kursi në internet Deep Dive nga EU Code Week është një kurs njëzet e pesë orësh në anglisht që u ofron mësuesve mundësinë të njihen me parimet e lidhura me kodimin dhe të fitojnë njohuri dhe besim për të organizuar veprimtari të lehta dhe argëtuese, ndërvepruese të kodimit me studentët e tyre. Mësuesit zbulojnë ',
+            1 => 'burimet',
+            2 => 'falas të EU Code Week dhe materiale trajnimi të disponueshme në 29 gjuhë dhe aspekte të veçanta të kodimit, të tilla si mendimin llogaritës, aktivitete të shkëputura dhe mundësi të pafundme të robotikës, modifikimin dhe bërjen, gjuhët e programimit vizual, krijimin e aplikacioneve dhe shumë më tepër.'
+        ],
+        'course-link' => "Shikoni kursin “Deep dive” 2019"
+    ],
+    "social-media" => [
+        0 => 'Ndiqni',
+        1 => 'EU Code Week në mediat sociale',
+        2 => 'për të zbuluar se kur do të fillojë kursi tjetër.',
+    ]
+
+];
