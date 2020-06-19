0ccb281db0896f0b4a3238bf0aaf3e41189d582f
Alain VD <alainvd@gmail.com>
2020-06-19 11:15:45 +0200

    - FI - HR - HU

---
+++ resources/lang/hr/values.php
@@ -0,0 +1,65 @@
+<?php
+
+
+return [
+    'title' => 'Izjava o vrijednostima Europskog tjedna programiranja',
+    'description' => [
+        1 =>
+            [
+                1 => 'Europski tjedan programiranja pokret je koji vode volonteri –',
+                2 => 'ambasadori',
+                3 => ', učitelji i zaljubljenici u programiranje iz cijelog svijeta. Podupiru ga',
+                4 => 'Europska komisija',
+                5 => 'i ministarstva obrazovanja zemalja Europskoj uniji i Zapadnog Balkana.',
+            ],
+
+        2 => 'U središtu je Europskog tjedna programiranja zajednica volontera koji organiziraju aktivnosti, a radi se učiteljima, mentorima, klubovima za programiranje, knjižnicama, privatnim poduzećima, roditeljima i nevladinim organizacijama. Svoje vrijeme, energiju i vještine dajemo kako bismo računalno razmišljanje, programiranje, robotiku, rad s hardverom, računalstvo i digitalne vještine općenito približili što većem broju ljudi u Europi i diljem svijeta.',
+        3 => 'Europski tjedan programiranja čine i milijuni sudionika, ljudi svih spolova, dobi, porijekla, kultura i različitih vještina diljem svijeta.',
+        4 => 'Ponosni smo što smo Europski tjedan programiranja. Ovo su naše vrijednosti. Njima se vodimo u radu i odlučivanju.',
+    ],
+    '1' => [
+        'title' => 'Europski je tjedan programiranja neovisan',
+        'content' => 'Europski je tjedan programiranja zajednica koju pokreću volonteri. U postupanju se ne vodimo ekonomskim interesom. Poštujemo i cijenimo ideje, vjerovanja i kulturu drugih ljudi. Na Europskom tjednu programiranja sudjelujemo zato što vjerujemo da je ono što radimo važno. To je naša snaga. Računalno razmišljanje, kodiranje, programiranje, računalstvo, robotiku, rad s hardverom i digitalne vještine općenito želimo učiniti što dostupnijima svim ljudima.',
+    ],
+    '2' => [
+        'title' => 'Europski je tjedan programiranja za svakoga',
+        'content' => 'Svatko može organizirati aktivnosti Europskog tjedna programiranja. Razumijemo da je bitno da priredbe Europskog tjedna programiranja budu organizirane za sve ljude neovisno o njihovoj dobi, boji kože, spolu, kulturi ili intelektualnim sposobnostima. Aktivnosti mogu biti polazna točka koja će potaknuti zanimanje i znatiželju ili će čak pomoći u daljnjem razvoju vještina.  Jednakost, poštovanje, sloboda i uključivost naša su načela. Želimo da svi sudionici na zabavan način steknu iskustvo i stvaraju s tehnologijom.',
+    ],
+    '3' => [
+        'title' => 'Europski je tjedan programiranja glokalan',
+        'content' =>
+            [
+                1 => 'Europski tjedan programiranja ima ambiciju doći do vas neovisno o tome živite li u velegradu, glavnom gradu regije ili zabačenom selu. Posebnu pozornost posvećujemo tome da lokalnim organizatorima pomognemo',
+                2 => 'materijalima',
+                3 => 'na njihovom jeziku koji će ispuniti njihove potrebe.',
+                4 => 'Karta Europskog tjedna programiranja',
+                5 => 'ključni je alat na kojem se registrira aktivnost s preciznim položajem. Istovremeno Europski je tjedan programiranja globalan i u svoju zajednicu prihvaća ljude iz cijelog svijeta. Mi smo “glokalni”.',
+            ]
+    ],
+    '4' => [
+        'title' => 'U Europskom tjednu programiranja važna je suradnja',
+        'content' => [
+            1 => 'Članovi zajednice Europskog tjedna programiranja međusobno surađuju, izmjenjuju informacije, uče jedni od drugih i zajedno grade znanje. Međusobno smo povezani i pomažemo jedni drugima. Dijelimo resurse, ideje i najbolju praksu. Potičemo partnerstvo i dijeljenje znanja.',
+            2 => 'Izazov EU Code Week 4 All',
+            3 => 'želi graditi mostove preko granica i povezivati zajednice. On pomaže u širenju pozitivne energije i povezuje ljude koji mogu učiti jedni od drugih.'
+        ]
+    ],
+    '5' => [
+        'title' => 'Europski je tjedan programiranja neprofitan',
+        'content' =>
+            [
+                1 => 'Sve su aktivnosti u tjednu programiranja neprofitne. Svi su',
+                2 => 'resursi',
+                3 => 'na internetskoj stranici Europskog tjedna programiranja besplatni. Sve su obuke i online tečajevi Europskog tjedna programiranja besplatni.'
+            ]
+    ],
+    '6' => [
+        'title' => 'Europski tjedan programiranja potiče kreativnost i inovativnost',
+        'content' => 'Europski tjedan programiranja potiče kreativnost, inovativnost i dugoročno razmišljanje. Otvoreni smo i zahvalni na povratnim informacijama i kritici kako bismo mogli donositi informirane odluke i razvijati Europski tjedan programiranja. Čak će i neuspjesi dovesti do toga da napredujemo kao pojedinci i zajednica.'
+    ],
+    '7' => [
+        'title' => 'Europski tjedan programiranja zahvalan je svojoj zajednici',
+        'content' => 'Zahvalni smo na podršci učitelja, trenera i organizatora drugih aktivnosti, studenata, roditelja, klubova za programiranje i svih partnera s kojima radimo. Svi imamo zajednički cilj. Računalno razmišljanje, programiranje, računalstvo i digitalne vještine općenito želimo približiti što većem broju ljudi.'
+    ],
+
+];
