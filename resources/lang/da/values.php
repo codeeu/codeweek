f409bb6bec51f0993e81b684b8545fbf4e1329ff
Alain VD <alainvd@gmail.com>
2020-06-19 10:37:54 +0200

    - CS - DA - DE - EL - ES - ET

---
+++ resources/lang/da/values.php
@@ -0,0 +1,65 @@
+<?php
+
+
+return [
+    'title' => 'EU Code Week - Værdierklæring',
+    'description' => [
+        1 =>
+            [
+                1 => 'EU’s kodeuge er en græsrodsbevægelse, der drives af frivillige –',
+                2 => 'ambassadører',
+                3 => 'lærere og kodningsentusiaster verden over. Bevægelsen støttes af',
+                4 => 'Europa-Kommissionen',
+                5 => 'og  undervisningsministerier i den Europæiske Union og på Vestbalkan.',
+            ],
+
+        2 => 'Kernen i EU\'s kode er de frivillige aktivitetsarrangører, dvs lærere, mentorer, kodeklubber, biblioteker, private virksomheder, forældre samt ikke-statslige organisationer. Vi dedikerer vores tid, energi og evner til at udbrede datalogisk tænkning, kodning, robotteknologi, hardwaretilpasning, computervidenskab og digitale færdigheder i det hele taget til så mange som muligt i Europa og resten af verden.',
+        3 => 'EU’s kodeuge er også millioner af deltagere; mennesker fra hele verden i alle aldre og af forskellige køn, baggrunde, kulturer og færdigheder.',
+        4 => 'Vi er stolte over at være en del af EU’s kodeuge. Dette er vores værdier. Værdier der til alle tider er styrende for vores arbejde og de beslutninger vi træffer.',
+    ],
+    '1' => [
+        'title' => 'EU’s kodeuge er uafhængig',
+        'content' => 'EU\'s kodeuge drives af frivillige. Vi handler ikke ud fra økonomiske interesser. Vi respekterer og værdsætter andre menneskers idéer, overbevisninger og kulturer. Vi tager del i EU\'s kodeuge, fordi vi tror på vigtigheden af det vi gør. Det er vores styrke. Vi ønsker at gøre datalogisk tænkning, kodning, programmering, computervidenskab, robotteknologi, hardwaretilpasning og generelle digitale færdigheder tilgængelig for alle.',
+    ],
+    '2' => [
+        'title' => 'EU’s kodeuge er for alle',
+        'content' => 'Alle kan tilrettelægge aktiviteter under EU\'s kodeuge. Vi erkender at det er essentielt at begivenhederne i EU’s kodeuge arrangeres for alle, uanset alder, farve, køn, kulturel baggrund eller intellektuelle evner. Aktiviteterne kan være en indgang til at skabe interesse og nysgerrighed eller en hjælp til at videreudvikle færdigheder.  Vores principper er ligestilling, respekt, frihed og inklusion. Vi ønsker at alle deltagerne skal have det sjovt med at opleve og konstruere med teknologi.',
+    ],
+    '3' => [
+        'title' => 'EU’s kodeuge er global',
+        'content' =>
+            [
+                1 => 'Ambitionen med EU\'s kodeuge er at nå ud til alle, uanset om man bor i en storby, en regionshovedstad eller en fjerntliggende landsby. Vi lægger især vægt på at støtte vores lokale organisatorer med',
+                2 => 'materialer',
+                3 => 'på deres eget sprog og tilpasset deres behov.',
+                4 => 'Kodeuge-kortet',
+                5 => 'er et nøgleredskab, hvor aktiviteter registreres med nøjagtig placering. Samtidigt er kodeugen global og byder mennesker fra hele verden velkommen i fællesskabet. Vi er “glokale”.',
+            ]
+    ],
+    '4' => [
+        'title' => 'EU’s kodeuge handler om samarbejde',
+        'content' => [
+            1 => 'Medlemmerne af kodeuge-fællesskabet interagerer, udveksler oplysninger, lærer af hinanden og opbygger viden sammen. Vi er forbundne og vi støtter hinanden. Vi deler ressourcer, idéer og ’best practice’. Vi opmuntrer til partnerskaber og vidensdeling.',
+            2 => 'Kodeugens Udfordring for Alle (4 All challenge)',
+            3 => 'sigter på at bygge bro henover grænser og lokalsamfund. Kodeugen hjælper med at sprede den positive energi og forbinde mennesker, der kan lære af hinanden.'
+        ]
+    ],
+    '5' => [
+        'title' => 'EU’s kodeuge er en non-profit bevægelse',
+        'content' =>
+            [
+                1 => 'Ingen tjener penge på kodeugens aktiviteter. Samtlige',
+                2 => 'ressourcer',
+                3 => 'på kodeugens hjemmeside kan tilgås gratis. Kodeugens undervisning og onlinekurser er alt sammen gratis.'
+            ]
+    ],
+    '6' => [
+        'title' => 'EU\'s kodeuge fremmer kreativitet og innovation',
+        'content' => 'EU’s kodeuge fremmer kreativitet, innovation og visionær tænkning. For at vi videre kan træffe beslutninger på et informeret grundlag og videreudvikle EU\'s kodeuge, modtager vi meget gerne feedback og kritik. Selv vores fejltrin kan hjælpe os med at vokse, både som mennesker og som fællesskab.'
+    ],
+    '7' => [
+        'title' => 'EU\'s kodeuge er taknemmelig for fællesskabet',
+        'content' => 'Vi er taknemmelige for støtten fra lærere, undervisere og andre arrangerer af aktiviteter, elever/studerende, forældre, kodeklubber og alle de partnere vi samarbejder med. Vi deler alle det samme mål. Vi ønsker at udbrede datalogisk tænkning, kodning, computervidenskab og digitale færdigheder til så mange muligt.'
+    ],
+
+];
---
+++ resources/lang/de/values.php
@@ -0,0 +1,65 @@
+<?php
+
+
+return [
+    'title' => 'Leitbild der EU Code Week',
+    'description' => [
+        1 =>
+            [
+                1 => 'Die EU Code Week ist eine von Freiwilligen –',
+                2 => 'Botschaftern',
+                3 => ', leitenden Lehrern und enthusiastischen Kodierern auf der ganzen Welt – getragene Bewegung. Sie wird von der',
+                4 => 'Europäischen Kommission',
+                5 => 'sowie Bildungsministerien in EU-Mitgliedstaaten und Ländern des westlichen Balkans unterstützt.',
+            ],
+
+        2 => 'Das Herzstück der EU Code Week ist die Gemeinschaft der freiwilligen Aktivitätsorganisatoren, unter ihnen Lehrer, Mentoren, Kodierungs-Clubs, Bibliotheken, Privatunternehmen, Eltern und Nichtregierungsorganisationen. Wir setzen unsere Zeit, Energie und unsere Fähigkeiten dazu ein, so vielen Menschen wie möglich die Bereiche rechnerisches Denken, Kodierung, Robotik, Tüftelei an Hardware, Informatik und digitale Kenntnisse näherzubringen – in Europa und auf der ganzen Welt. ',
+        3 => 'Die EU Code Week besteht aber auch aus ihren Millionen Teilnehmern; diese Menschen ganz unterschiedlichen Geschlechts, Alters und Hintergrunds kommen aus den verschiedensten Kulturen auf der ganzen Welt und bringen ganz unterschiedliche Vorkenntnisse mit.',
+        4 => 'Wir sind stolz darauf, ein Teil der EU Code Week zu sein. Dies ist unser Leitbild. Wir folgen diesen Werten in unserer Arbeit und in unseren Entscheidungen zu jeder Zeit.',
+    ],
+    '1' => [
+        'title' => 'Die EU Code Week ist unabhängig',
+        'content' => 'Die EU Code Week ist eine von Freiwilligen geführte Gemeinschaft. Wir handeln nicht aus wirtschaftlichem Interesse heraus. Wir respektieren und wertschätzen die Ideen, den Glauben und die Kultur anderer Menschen. Wir nehmen an der EU Code Week teil, weil wir an die Wichtigkeit unseres Handelns glauben. Darin liegt unsere Kraft. Wir arbeiten daran, rechnerisches Denken, Kodieren, Programmieren, Informatik, Robotik, Tüftelei an Hardware und digitale Kenntnisse für jedermann zugänglich zu machen.',
+    ],
+    '2' => [
+        'title' => 'Die EU Code Week ist für jeden zugänglich',
+        'content' => 'Jeder kann Aktivitäten im Rahmen der EU Code Week organisieren. Unserer Meinung nach ist es darüber hinaus wesentlich, dass Veranstaltungen der EU Code Week für alle Menschen organisiert werden, unabhängig von Alter, Hautfarbe, Geschlecht, kulturellem Hintergrund oder intellektuellen Fähigkeiten. Aktivitäten können als Einstieg in das Themengebiet fungieren und Interesse sowie Neugier entfachen, oder sie helfen dabei, bereits bestehende Kenntnisse weiterzuentwickeln. Gleichberechtigung, Respekt, Freiheit und Inklusivität sind unsere Prinzipien. Wir möchten, dass alle Teilnehmer Spaß daran haben, Technologie zu erleben und zu entwickeln.',
+    ],
+    '3' => [
+        'title' => 'Die EU Code Week ist glokal',
+        'content' =>
+            [
+                1 => 'Die EU Code Week möchte jeden erreichen, ganz gleich ob Sie in einer großen Stadt, einer Regionalhauptstadt oder einem abgelegenen Dorf leben. Wir achten besonders drauf, lokale Organisatoren mit',
+                2 => 'Materialien',
+                3 => 'in ihrer jeweiligen Sprache und für ihre jeweiligen Bedürfnisse zu unterstützen. Die',
+                4 => 'EU-Code-Week-Karte',
+                5 => 'ist hierbei ein entscheidendes Instrument; in sie werden sämtliche Aktivitäten mit dem Veranstaltungsort verzeichnet. Gleichzeitig ist die EU Code Week global und heißt Menschen aus der ganzen Welt in der Gemeinschaft willkommen. Wir sind „glokal“.',
+            ]
+    ],
+    '4' => [
+        'title' => 'Die EU Code Week ist kooperativ',
+        'content' => [
+            1 => 'Mitglieder der EU-Code-Week-Gemeinschaft interagieren, tauschen Informationen aus, lernen voneinander und bauen gemeinsam Wissen auf. Wir sind miteinander verbunden und unterstützen uns gegenseitig. Wir teilen Ressourcen, Ideen und bewährte Methoden. Wir fördern Partnerschaften und den Wissensaustausch. ',
+            2 => 'Die EU Code Week 4 All Challenge',
+            3 => 'zielt darauf ab, Brücken Grenzen hinweg und zwischen den verschiedenen Gemeinschaften zu schlagen. Sie hilft dabei, positive Energie zu verbreiten und verbindet Menschen miteinander, sodass diese voneinander lernen können.'
+        ]
+    ],
+    '5' => [
+        'title' => 'Die EU Code Week ist gemeinnützig',
+        'content' =>
+            [
+                1 => 'Alle Aktivitäten der EU Code Week sind gemeinnützig. Sämtliche ',
+                2 => 'Ressourcen',
+                3 => 'auf der Website der EU Code Week sind kostenlos verfügbar. Darüber hinaus sind alle Training- und Onlinekurse der EU Code Week kostenlos.'
+            ]
+    ],
+    '6' => [
+        'title' => 'Die EU Code Week fördert Kreativität und Innovation',
+        'content' => 'Die EU Code Week fördert Kreativität, Innovation und zukunftsorientiertes Denken. Wir sind offen und dankbar für Feedback und Kritik, sodass wir fundierte Entscheidungen treffen und die EU Code Week weiterentwickeln können – denn auch Fehler helfen uns beim Wachstum, auf individueller wie auf gemeinschaftlicher Ebene.'
+    ],
+    '7' => [
+        'title' => 'Die EU Code Week ist ihrer Gemeinschaft dankbar',
+        'content' => 'Wir sind dankbar für die Unterstützung von Lehrern, Coaches und andere Aktivitätsorganisatoren, Studenten, Schülern, Kodierungs-Clubs sowie sämtlichen Partner, mit denen wir zusammenarbeiten. Wir alle haben dasselbe Ziel. Wir möchten so vielen Menschen wie möglich rechnerisches Denken, Kodieren, Informatik und digitale Kenntnissen zugänglich machen.'
+    ],
+
+];
