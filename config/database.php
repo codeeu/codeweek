3c59f2ad34896f8aeefab97af7d55d6f93df6623
Alain Van Driessche <alainvd@gmail.com>
2020-09-08 10:21:26 +0200

    Fix issue if no event is found in the current month

--- config/database.php
+++ config/database.php
@@ -53,6 +53,20 @@ return [
             'strict' => false,
             'engine' => null,
         ],
+        'mysql_testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE_TESTING', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+            'engine' => null,
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
