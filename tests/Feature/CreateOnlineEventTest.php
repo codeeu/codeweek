ce6e8e01e7c14ac305380c83cf97083efc50b68e
Alain Van Driessche <alainvd@gmail.com>
2020-09-18 12:05:53 +0200

    Allow address for online events

--- tests/Feature/CreateOnlineEventTest.php
+++ tests/Feature/CreateOnlineEventTest.php
@@ -45,6 +45,36 @@ class CreateOnlineEventTest extends TestCase
         $this->assertEquals("fi",$event->fresh()->language);
     }
 
+    /** @test */
+    public function an_authenticated_user_can_create_private_online_event_without_location()
+    {
+        //$this->withoutExceptionHandling();
+        $this->seed('RolesAndPermissionsSeeder');
+        $this->signIn();
+
+        $event = make('App\Event');
+        create('App\Audience',[] ,3);
+        create('App\Theme', [],3);
+
+        $event->theme = "1";
+        $event->tags = "tag:foo,tag:bar";
+        $event->audience = "2, 3";
+        $event->privacy = true;
+        $event->location = null;
+        $event->activity_type = "invite-online";
+        $event->event_url = "http://lesoir.be";
+        $event->geoposition = null;
+        $event->language = "fi";
+
+        $this->post('/events', $event->toArray());
+
+        $event = Event::where('title', $event->title)->first();
+
+        $this->assertEquals("online",$event->fresh()->location);
+        $this->assertEquals("http://lesoir.be",$event->fresh()->event_url);
+        $this->assertEquals("fi",$event->fresh()->language);
+    }
+
 }
 
 
