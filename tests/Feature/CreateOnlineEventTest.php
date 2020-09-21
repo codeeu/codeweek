749c5fc671b3a06ae5d51f9e985d5865e32ed1a1
Alain Van Driessche <alainvd@gmail.com>
2020-09-21 12:04:17 +0200

    Get Location if provided even for online events

--- tests/Feature/CreateOnlineEventTest.php
+++ tests/Feature/CreateOnlineEventTest.php
@@ -73,6 +73,40 @@ class CreateOnlineEventTest extends TestCase
         $this->assertEquals("online",$event->fresh()->location);
         $this->assertEquals("http://lesoir.be",$event->fresh()->event_url);
         $this->assertEquals("fi",$event->fresh()->language);
+        $this->assertEquals(0,$event->fresh()->latitude);
+        $this->assertEquals(0,$event->fresh()->longitude);
+    }
+
+    /** @test */
+    public function online_event_can_have_a_location()
+    {
+        $this->withoutExceptionHandling();
+        $this->seed('RolesAndPermissionsSeeder');
+        $this->signIn();
+
+        $event = make('App\Event');
+        create('App\Audience',[] ,3);
+        create('App\Theme', [],3);
+
+        $event->theme = "1";
+        $event->tags = "tag:foo,tag:bar";
+        $event->audience = "2, 3";
+        $event->privacy = true;
+        $event->location = "somewhere in Belgium";
+        $event->activity_type = "invite-online";
+        $event->event_url = "http://lesoir.be";
+        $event->geoposition = null;
+        $event->language = "fi";
+        $event->geoposition = "4.321,1.234";
+
+        $this->post('/events', $event->toArray());
+
+        $event = Event::where('title', $event->title)->first();
+
+        $this->assertEquals("somewhere in Belgium",$event->fresh()->location);
+        $this->assertEquals(4.321,$event->fresh()->latitude);
+        $this->assertEquals(1.234,$event->fresh()->longitude);
+
     }
 
 }
