2a2feadafe69d54c1ce4c7ac9531afa82565681e
Alain Van Driessche <alainvd@gmail.com>
2020-10-02 16:18:34 +0200

    Remove error on IE check

--- app/Http/Middleware/CheckBrowser.php
+++ app/Http/Middleware/CheckBrowser.php
@@ -17,12 +17,13 @@ class CheckBrowser
     public function handle($request, Closure $next)
     {
 
-
-        $ua = htmlentities($_SERVER['HTTP_USER_AGENT'], ENT_QUOTES, 'UTF-8');
-        if (preg_match('~MSIE|Internet Explorer~i', $ua) || (strpos($ua, 'Trident/7.0') !== false && strpos($ua, 'rv:11.0') !== false)) {
-            // do stuff for IE
-            Log::info($ua);
-            abort(403, 'We are sorry but we do not support Internet Explorer. Please use Microsoft Edge, Chrome or Firefox to access Codeweek.eu');
+        if (isset($_SERVER) && isset($_SERVER['HTTP_USER_AGENT'])) {
+            $ua = htmlentities($_SERVER['HTTP_USER_AGENT'], ENT_QUOTES, 'UTF-8');
+            if (preg_match('~MSIE|Internet Explorer~i', $ua) || (strpos($ua, 'Trident/7.0') !== false && strpos($ua, 'rv:11.0') !== false)) {
+                // do stuff for IE
+                Log::info($ua);
+                abort(403, 'We are sorry but we do not support Internet Explorer. Please use Microsoft Edge, Chrome or Firefox to access Codeweek.eu');
+            }
         }
 
         return $next($request);
