64e27b1aa552ec33e0094f410f71cb1118752e3e
Alain Van Driessche <alainvd@gmail.com>
2020-10-13 11:19:01 +0200

    Scratch Tutorial

--- resources/views/static/_cw2020-common.blade.php
+++ resources/views/static/_cw2020-common.blade.php
@@ -7,5 +7,5 @@
 <li><a href="https://codeweek-s3.s3-eu-west-1.amazonaws.com/cw2020/%23CodeWeekDance-Instructions.jpg">@lang('cw2020.common.moves')</a></li>
 <li><a href="http://bit.ly/CWDance">@lang('cw2020.common.dance-example')</a></li>
 <li>
-    <a href="https://codeweek-s3.s3-eu-west-1.amazonaws.com/docs/Scratch%23CodeWeekDanceTutorial.docx">@lang('cw2020.dance.title') - @lang('cw2020.common.scratch-tutorial')</a>
+    <a href="https://codeweek-s3.s3-eu-west-1.amazonaws.com/docs/Scratch%23CodeWeekDanceTutorial.docx">@lang('cw2020.common.scratch-tutorial')</a>
 </li>
