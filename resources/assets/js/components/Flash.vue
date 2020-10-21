0e17183e6b649cc08312281c002fec0129cedd2e
Alain Van Driessche <alainvd@gmail.com>
2020-10-21 14:12:53 +0200

    MAx upload filesize

--- resources/assets/js/components/Flash.vue
+++ resources/assets/js/components/Flash.vue
@@ -30,7 +30,6 @@
 
         methods: {
             flash(data) {
-              console.log(data);
                 if (data) {
                     this.body = data.message;
                     this.level = data.level;
