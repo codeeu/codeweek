f5242f4d612d6d360dbbdb363cdb61a9e99804da
Alain Van Driessche <alainvd@gmail.com>
2021-02-24 17:11:09 +0100

    Leading Teachers Admin List Update with new fields

--- app/Http/Livewire/LeadingTeachersListTable.php
+++ app/Http/Livewire/LeadingTeachersListTable.php
@@ -3,6 +3,9 @@
 namespace App\Http\Livewire;
 
 use App\Country;
+use App\LeadingTeacherExpertise;
+use App\ResourceLevel;
+use App\ResourceSubject;
 use App\User;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
@@ -25,9 +28,11 @@ class LeadingTeachersListTable extends LivewireDatatable
     {
         return [
 
+            Column::callback(['id', 'approved'], function ($id, $approved) {
+                return view('livewire.leading-teachers-list-table-actions', ['id' => $id, 'approved' => $approved]);
+            }),
             Column::name('firstname')
                 ->label('firstname')
-
                 ->filterable(),
 
 
@@ -39,6 +44,15 @@ class LeadingTeachersListTable extends LivewireDatatable
                 ->label('Country')
                 ->filterable(),
 
+            Column::name('city.city')
+                ->label('City')
+                ->hide(),
+
+            Column::name('email')
+                ->label('email')
+                ->filterable(),
+
+
             BooleanColumn::name('approved')
                 ->label('Approved ?')
                 ->filterable()
@@ -47,22 +61,58 @@ class LeadingTeachersListTable extends LivewireDatatable
 
 
             DateColumn::name('updated_at')
-                ->label('updated at'),
+                ->label('updated at')
+            ,
 
 
-            Column::callback(['id','approved'], function ($id, $approved) {
-                return view('livewire.leading-teachers-list-table-actions', ['id' => $id, 'approved' => $approved]);
-            })
+
+
+            Column::name('levels.name')
+                ->filterable($this->levels->pluck('name'))
+                ->label('Levels')
+                ->hide(),
+
+            Column::name('expertises.name')
+                ->filterable($this->levels->pluck('name'))
+                ->label('expertises')
+                ->hide(),
+
+            Column::name('subjects.name')
+                ->filterable($this->subjects->pluck('name'))
+                ->label('subjects')
+                ,
+
+            //Hidden Fields
+            Column::name('twitter')
+                ->label('twitter')
+                ->hide(),
 
         ];
     }
 
-    public function delete($id){
-        User::where('id','=',$id)->first()->removeRole('leading teacher');
+    public function delete($id)
+    {
+        User::where('id', '=', $id)->first()->removeRole('leading teacher');
     }
 
-    public function approve($id){
-        User::where('id','=',$id)->update(['approved'=>true]);
+    public function approve($id)
+    {
+        User::where('id', '=', $id)->update(['approved' => true]);
+    }
+
+    public function getLevelsProperty()
+    {
+        return ResourceLevel::all();
+    }
+
+    public function getExpertisesProperty()
+    {
+        return LeadingTeacherExpertise::all();
+    }
+
+    public function getSubjectsProperty()
+    {
+        return ResourceSubject::all();
     }
 
     //Exception thrown by livewire datatable when inside the 'content' section
