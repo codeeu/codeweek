c49caddc2748ebb8109f45f3c1c7c4a76e307f87
Alain Van Driessche <alainvd@gmail.com>
2021-04-08 08:19:49 +0200

    Ireland registration back

--- resources/views/hackathons/before/hackathon-ireland.blade.php
+++ resources/views/hackathons/before/hackathon-ireland.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.base')
 
 @section('hackathons.header')
-    @include('hackathons.before.header', ["enabled_language" => "en", "registration_link"=>""])
+    @include('hackathons.before.header', ["enabled_language" => "en", "registration_link"=>"https://ec.europa.eu/eusurvey/runner/c20f3e59-fc39-7248-0347-d8e736025fb2 "])
 @endsection
 
 @section('content')
@@ -93,7 +93,8 @@
             <h1>@lang('hackathon-ireland.sections.9.title')</h1>
 
             @lang('hackathon-ireland.sections.9.content.0')<br/>
-            @lang('hackathon-ireland.sections.9.content.1')
+            @lang('hackathon-ireland.sections.9.content.1')<br/>
+            @lang('hackathon-ireland.sections.9.content.2')<br/>
 
 
             <br/><br/>
