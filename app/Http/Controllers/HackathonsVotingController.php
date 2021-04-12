df755abd6962f8739d6e9c7ef7d50a93b1906312
Alain Van Driessche <alainvd@gmail.com>
2021-04-12 14:57:55 +0200

    Export Results

--- app/Http/Controllers/HackathonsVotingController.php
+++ app/Http/Controllers/HackathonsVotingController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\VotesExport;
 use App\Vote;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class HackathonsVotingController extends Controller
 {
@@ -26,5 +28,11 @@ class HackathonsVotingController extends Controller
 
         return redirect()->back()->with('success', 'thanks for voting');
         //return back();
+    }
+
+    public function results(Request $request)
+    {
+        return Excel::download(new VotesExport(), 'votes.xlsx');
+
     }
 }
