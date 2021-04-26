70964e8964e1521d6cbf2eea88032adecf2ca227
Alain Van Driessche <alainvd@gmail.com>
2021-04-26 14:19:00 +0200

    Latvian Sponsor

--- resources/views/hackathons/before/hackathon-latvia.blade.php
+++ resources/views/hackathons/before/hackathon-latvia.blade.php
@@ -219,6 +219,12 @@
 
                 <div class="item">
                     <div class="flex justify-center">
+                        <img src="{{asset('/images/hackathons/partners/learn-it.png')}}">
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="flex justify-center">
                         <img src="{{asset('/images/hackathons/partners/microsoft.png')}}">
                     </div>
                 </div>
