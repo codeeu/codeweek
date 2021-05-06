1a4eab36170d54d5ad5ebf3154975a190c70c50a
Alain Van Driessche <alainvd@gmail.com>
2021-05-06 15:51:55 +0200

    Find Ally

--- resources/views/ambassadors.blade.php
+++ resources/views/ambassadors.blade.php
@@ -15,9 +15,11 @@
             <div class="image">
 
                 <img src="images/banner_ambassadors.svg" class="static-image">
+                @if(request()->get("country_iso") == "IT" || request()->get("country_iso") == "GR")
                 <div class="mt-16 mr-8 absolute">
                     <img src="{{asset('images/ally.png')}}" width="100px">
                 </div>
+                    @endif
             </div>
 
 
