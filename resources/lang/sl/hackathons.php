3567343e7986d7864df82c842409d4057149b2c4
Alain Van Driessche <alainvd@gmail.com>
2021-05-17 09:34:55 +0200

    Date for SL in EN/SL

--- resources/lang/sl/hackathons.php
+++ resources/lang/sl/hackathons.php
@@ -74,7 +74,7 @@ return [
         5 => [
             'city' => "TBA",
             'country' => "Slovenija",
-            'date' => "15.–16. maj 2021"
+            'date' => "18.–19. september 2021"
         ],
         6 => [
             'city' => "TBA",
