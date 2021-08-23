472e04b7fd6fd6cf72aa8ee112276f3d77b6c5a7
Alain Van Driessche <alainvd@gmail.com>
2021-08-23 13:35:45 +0200

    Chatbot challenge page

--- resources/views/2021/challenges/chatbot.blade.php
+++ resources/views/2021/challenges/chatbot.blade.php
@@ -21,12 +21,96 @@
         <section class="codeweek-content-wrapper">
             <div class="m-6">
 
-                <div class="text-blue-500 text-2xl">
-                    Chatbot Challenge
+                <section class="grid grid-cols-1 gap-6 md:grid-cols-4 mx-6 my-4">
+
+
+                    <div>
+                        <div class="text-xl text-left text-blue-600">Target audience</div>
+                        <div>Teachers</div>
+                        <div>Students (12-18)</div>
+                    </div>
+
+                    <div>
+                        <div class="text-xl text-left text-blue-600">Experience</div>
+                        <div>Advanced</div>
+                    </div>
+
+                    <div>
+                        <div class="text-xl text-left text-blue-600">Duration</div>
+                        <div>1 hour</div>
+                    </div>
+
+                    <div>
+                        <div class="text-xl text-left text-blue-600">Recommended materials</div>
+                        <div><a href="https://pencilcode.net/" target="_blank">Pencil code</a></div>
+                        <div><a href="https://machinelearningforkids.co.uk/" target="_blank">Machine learning for
+                                kids</a></div>
+                    </div>
+
+
+                </section>
+
+
+                <div class="orange text-3xl mt-2">
+                    Make a chatbot
                 </div>
 
+                <div class="leading-6 text-base text-left">
+                    <div class="mt-2">
+                        Code a conversation between a chatbot and a user who is trying to solve a riddle. Try to make a
+                        chatbot that can chat like a person. Instead of a riddle you can create a dialogue between a
+                        chatbot and a user.
+                    </div>
 
+                    <section class="bg-blue-200 p-2 mt-6">
+                        <div class="orange text-3xl">Purpose of the challenge</div>
+                        <div class="mt-2">To code interactive riddles. To use code to create dialogues between a chatbot and a
+                            user.
+                        </div>
+                    </section>
+
+
+                    <section>
+                        <div class="leading-8">
+                            <div class="mt-6 orange text-3xl">Instructions</div>
+                            <ul class="leading-7 ml-2 mt-0 checklist mt-2">
+                                <li>Think of a riddle.</li>
+                                <li>Log in to <a href="https://pencilcode.net/" target="_blank">Pencil Code</a> or
+                                    create a new account. (When creating a new account keep in mind that real names are
+                                    not allowed on Pencil Code due to privacy reasons.)
+                                </li>
+                                <li>Click Imagine and Make your own.</li>
+                                <li>Use blocks or text-based mode to write your riddle.</li>
+                                <li>Alternatively, you can use <a href="https://abfromz.pencilcode.net/edit/Riddle"
+                                                                  target="_blank">this code</a> and adapt it to your
+                                    riddle or you can select Answering a riddle from the menu and edit it.
+                                </li>
+                            </ul>
+
+                        </div>
+                    </section>
+
+                    <section>
+                        <div class="mt-6 orange text-3xl">Example</div>
+                        <div class="mt-2">Check out this  example of a riddle. <iframe src="//abfromz.pencilcode.net/home/Riddle" width="640" height="640" frameborder="0" allowfullScreen></iframe>
+                        </div>
+                    </section>
+
+
+                </div>
+
+            </div>
         </section>
     </section>
 
+@endsection
+
+@section('extra-css')
+    <style>
+        ul.checklist li:before {
+            content: '• ';
+            color: #ee6a2c;
+            font-weight: bold;
+        }
+    </style>
 @endsection
