d20f45e728ff8c2c34736ac607f2bf3791ed2795
Alain Van Driessche <alainvd@gmail.com>
2021-09-02 11:04:14 +0200

    Author and links

--- resources/views/2021/challenges/_author.blade.php
+++ resources/views/2021/challenges/_author.blade.php
@@ -1,3 +1,3 @@
 <div class="justify-center bg-blue-200 p-2">
-    This challenge has been submitted by <span class="italic">{{$author}}</span> and is part of the <a href="{{route('challenges')}}" class="text-blue-800 underline">EU Code Week Challenges</a>.
+    This challenge has been written by <span class="italic">{{$author}}</span> and is part of the <a href="{{route('challenges')}}" class="text-blue-800 underline">EU Code Week Challenges</a>.
 </div>
