91046f1616e9ca538bc760fb502d25ce215ac8f1
Alain Van Driessche <alainvd@gmail.com>
2021-09-13 16:53:59 +0200

    create a dance party

--- resources/views/2021/challenges/create-a-dance.blade.php
+++ resources/views/2021/challenges/create-a-dance.blade.php
@@ -7,16 +7,18 @@
     <section id="codeweek-schools-page" class="codeweek-page">
 
 
-        <section class="flex flex-row justify-between" style="background-color: #E3E3E3">
+        <section class="flex flex-row justify-between" style="background-color: #E759FD">
             <div class="flex justify-center items-center w-full">
                 <div class="text-center m-12">
-                    <div class="text-xl text-white w-full"><a href="{{route('challenges')}}">EU CODE WEEK CHALLENGES</a></div>
-                    <div class="text-5xl mt-2" style="color: #6970a9">Computational Thinking and Computational Fluency with ScratchJr.</div>
+                    <div class="text-xl text-white w-full"><a class="text-white" href="{{route('challenges')}}">EU CODE
+                            WEEK CHALLENGES</a></div>
+                    <div class="text-5xl mt-2" style="color: #fdeefd">Create a dance with the Ode to Code!</div>
                 </div>
             </div>
 
             <div class="md:w-10/12 md:flex hidden">
-                <img src="{{asset('img/2021/challenges/thumbnails/computational-thinking-and-computational-fluency.png')}}">
+                <img src="{{asset('img/2021/challenges/thumbnails/create-a-dance.png')}}">
+
 
             </div>
 
@@ -25,16 +27,15 @@
         <section class="codeweek-content-wrapper">
             <div class="m-6">
 
-                @include('2021.challenges._author', ['author' => 'Stamatis Papadakis – EU Code Week Ambassador Greece'])
+                @include('2021.challenges._author', ['author' => 'Code.org'])
 
-                <section class="grid grid-cols-1 gap-6 md:grid-cols-4 mx-6 my-4">
+                <section class="grid grid-cols-1 gap-6 md:grid-cols-5 mx-6 my-4">
 
 
                     <div>
                         <div class="text-xl text-left text-blue-600">Target audience</div>
                         <ol class="list-disc ml-5">
-                            <li>Teachers</li>
-                            <li>Pre-primary students (3 to 6 years)</li>
+                            <li>Primary School students (6 to 12 years)</li>
                         </ol>
                     </div>
 
@@ -48,33 +49,30 @@
                     <div>
                         <div class="text-xl text-left text-blue-600">Duration</div>
                         <ol class="list-disc ml-5">
-                            <li>90 minutes</li>
+                            <li>1 hour</li>
                         </ol>
                     </div>
 
                     <div>
                         <div class="text-xl text-left text-blue-600">Recommended materials</div>
                         <ol class="list-disc ml-5">
-                            <li>The free app <a href="www.scratchjr.org/">ScratchJr</a> works on various operating systems and types of smart devices.</li>
-                            <li>Also, the <a href="www.scratchjr.org/">ScratchJr</a> website offers plenty of free educational material.</li>
+                            <li><a href="https://code.org/dance">The Code.org tutorial</a></li>
+                        </ol>
+
+
+                    </div>
+
+                    <div>
+                        <div class="text-xl text-left text-blue-600">Purpose</div>
+                        <ol class="list-disc ml-5">
+                            <li>To practice coding in a fun way and feel connected with the EU Code Week community.</li>
                         </ol>
 
 
                     </div>
 
 
-
                 </section>
-
-                <div class="mx-6 my-4">
-                    <div class="text-xl text-left text-blue-600">Purpose</div>
-                    <ol class="list-disc ml-5">
-                        <li>To get familiar with new commands and interface.</li>
-                        <li>To create simple programs with simple cause-and-effect commands.</li>
-                        <li>To perform simple debugging through trial and error.</li>
-                    </ol>
-
-                </div>
 
 
                 <div class="leading-6 text-base text-left">
@@ -83,44 +81,49 @@
                         <div class="orange text-3xl mt-2">
                             Description
                         </div>
-
+                        <div class="mt-2 w-1/2">
+                            <img src="{{asset('img/2021/challenges/create-a-dance-2.jpg')}}"/>
+                        </div>
 
                         <div class="mt-2">
-                            In this challenge children will integrate CT concepts into their projects by using the ScratchJr app to make their stories more engaging, exciting, and emotional.
+                            Create a dance with the Ode to Code! Use the <a href="https://code.org/dance">Dance Party tutorial</a> to code a
+                            dance to the Ode to Code. The official EU Code Week anthem is listed as a selection in Dance
+                            Party.
                         </div>
                     </section>
-
 
 
                     <section class="p-2">
                         <div class="leading-6">
                             <div class="mt-6 orange text-3xl">Instructions</div>
                             <ul class="leading-7 ml-2 checklist mt-2">
-                                <li>ScratchJr does not require children to be literate. All instructions and menu options are identifiable through symbols and colours. The challenge can be completed within the classroom, the lab or even in an open space as no internet is required.</li>
-                                <li>Children use city as background and use coding blocks to make a car drive across the city.</li>
+                                <li>Instructions are shown as videos within <a href="https://code.org/dance">the
+                                        tutorial</a> and also written at the top of each level.
+                                </li>
                             </ul>
                         </div>
                     </section>
 
-@include('2021.challenges._share')
+                    @include('2021.challenges._share')
 
                     <section class="p-2">
                         <div class="orange text-3xl">Example</div>
-                        <div class="mt-2">Children can use sound and motion blocks and start again blocks to make characters dance.<br/>
-                            Children pick a background and a character and use a motion block to make a car drive across the city Children can use the speed block to speed up or slow down a character.</div>
-                        <div class="mt-2">
+                        <div class="mt-2">Featured student creations can be found on the following page: <a
+                                    href="https://code.org/dance">https://code.org/dance</a>
+                            <div class="mt-2">
+                                <a href="https://code.org/dance">
+                                    <img src="{{asset('img/2021/challenges/create-a-dance-1.gif')}}"/>
+                                </a>
 
-                            <img src="{{asset('img/2021/challenges/computational-thinking-and-computational-fluency.png')}}"/>
 
-
-                        </div>
+                            </div>
 
                     </section>
 
 
                 </div>
 
-                @include('2021.challenges._download',['url'=>'https://codeweek-s3.s3.eu-west-1.amazonaws.com/cw2021/Computational+Thinking+and+Computational+Fluency+with+ScratchJr..docx'])
+                @include('2021.challenges._download',['url'=>'https://codeweek-s3.s3.eu-west-1.amazonaws.com/cw2021/Create+a+dance+with+the+Ode+to+Code!.docx'])
             </div>
         </section>
     </section>
