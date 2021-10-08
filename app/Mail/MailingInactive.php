90d0df2137c6d447ce231e8b1a49f7a12606f85d
Alain Van Driessche <alainvd@gmail.com>
2021-10-08 15:45:54 +0200

    Mail Inactive Organizers

---
+++ app/Mail/MailingInactive.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class MailingInactive extends Mailable {
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build() {
+        return $this->subject('Pin activities for EU Code Week 2021')->markdown(
+            'emails.en.mailing-inactive'
+        );
+    }
+}
