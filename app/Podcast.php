2df23cd6e4097ae1bee430a774e5cdd8c60fc7aa
Alain Van Driessche <alainvd@gmail.com>
2021-10-25 15:45:21 +0200

    Podcasts images + RSS

--- app/Podcast.php
+++ app/Podcast.php
@@ -18,15 +18,33 @@ class Podcast extends Model implements Feedable {
         return $query->where('active', true);
     }
 
+    /* public function toFeedItem(): FeedItem {
+        return FeedItem::create([
+            'id' => $this->id,
+            'title' => $this->title,
+            'summary' => $this->description,
+            'updated' => $this->updated_at,
+            'link' => $this->filename,
+            'authorName' => 'Max Bailey',
+            'authorEmail' => 'm.bailey@mcgroup.com'
+        ]);
+    }*/
+
     public function toFeedItem(): FeedItem {
         return FeedItem::create()
             ->id($this->id)
+            ->link('https://codeweek.eu/feed/podcasts')
             ->title($this->title)
             ->summary($this->description)
             ->updated($this->updated_at)
-            ->link($this->filename)
+            ->enclosure(
+                'https://dts.podtrac.com/redirect.mp3/files.serialpodcast.org/sites/default/files/podcast/1422481890/serials01-e09.mp3'
+            )
+            ->enclosureType('audio/mpeg')
+            ->enclosureLength(21830720)
             ->authorName('Max Bailey')
-            ->authorEmail('m.bailey@mcgroup.com');
+            ->authorEmail('m.bailey@mcgroup.com')
+            ->image('pom.jpg');
     }
 
     public static function getFeedItems() {
