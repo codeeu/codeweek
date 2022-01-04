3d4cea481815c0917751a6165601c505579f1223
Alain Van Driessche <alainvd@gmail.com>
2022-01-04 12:16:13 +0100

    Add Transcript information to Podcast

--- resources/views/2021/_podcast.blade.php
+++ resources/views/2021/_podcast.blade.php
@@ -12,7 +12,19 @@
     </div>
 
     <div class="flex-1 align-items-stretch h-full align-content-stretch {{$bg}} pl-4">
-        <h2 class="subtitle">{{$podcast->title}}</h2>
+        <div class="flex justify-between">
+            <div><h2 class="subtitle">{{$podcast->title}}</h2></div>
+            @if($podcast->transcript)
+                <div class="self-end">
+                    <a href="https://codeweek-podcasts.s3.eu-west-1.amazonaws.com/transcripts/{{$podcast->transcript}}">
+                        <button class="background-transparent font-bold uppercase px-3 py-1 text-xs outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+                            Download Transcript</button>
+                    </a>
+                </div>
+            @endif
+        </div>
+
+
         <div class="text-black pb-2 pr-4 text-base leading-5">{{$podcast->description}}</div>
         <div class="flex flex-row">
             <div>
@@ -23,9 +35,7 @@
                 </audio>
             </div>
 
-            @if($podcast->transcript)
-                <div class="self-center text-sm ml-4"><a href="https://codeweek-podcasts.s3.eu-west-1.amazonaws.com/transcripts/{{$podcast->transcript}}">Download Transcript</a></div>
-            @endif
+
         </div>
 
 
