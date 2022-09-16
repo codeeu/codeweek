fd64fd96a29d674ade4531bed4489476dbedc353
Alain Van Driessche <alainvd@gmail.com>
2022-09-16 12:38:50 +0200

    Privacy statement contact-points merge

---
+++ resources/lang/en/privacy-statement-contact.php
@@ -0,0 +1,110 @@
+<?php
+
+return [
+    'title' => 'PROTECTION OF YOUR PERSONAL DATA',
+    'subtitle' => 'This privacy statement provides information about the processing and the protection of your personal data.',
+    'processing-operation' => [
+        'Processing operation',
+        'management of Code Week Ambassadors, Leading Teachers and registration of activities on the website'
+    ],
+    'data-processors' => [
+        'Data Processors',
+        'GOPA Com. S.A. and EUN Partnership AISBL (European Schoolnet), info@codeweek.eu'
+    ],
+    '1-intro' => [
+            'The information in relation to processing of personal data for the management of Code Week Ambassadors, Leading Teachers and registration of activities on the website.'
+    ],
+
+    '3-legal_process' => [
+       'Lawfulness of the processing operation: Unit F.4 collects, stores and publishes your personal data, because you gave your explicit consent by ticking a box when you signed up to become a Code Week Ambassador, a Leading Teacher or when you applied to register an event on <a href="https://codeweek.eu/register">Europe Code Week.</a>',
+
+    ],
+    '4-collect_data' => [
+        'The <strong>mandatory</strong> personal data collected for <strong>Ambassadors and Leading Teachers</strong> are:',
+        'Name',
+        'Email address',
+        'Password',
+        'Picture',
+        'Biography',
+        'Title',
+        'Field of expertise for Leading Teachers.',
+        'When you <strong>register an activity, the mandatory</strong> personal data collected are:',
+        'Name of the organisation',
+        'Type of organisation',
+        'Contact e-mail address',
+        'The provision of this data is mandatory to be a Code Week Ambassador, Leading Teacher or to register an activity on the Code Week website.',
+        'In addition, <strong>Ambassadors and Leading Teachers</strong> can <strong>voluntarily</strong> provide the following personal data:',
+        'Social media accounts',
+        '<strong>Activity organisers</strong> can <strong>voluntarily</strong> provide the following data:',
+        'Public contact e-mail address',
+        'Organiser’s website',
+        'Code Week for All Code',
+        'For activity organisers, please note that the relevant Ambassador for your country or Unit F.4 can use the personal data you provided to register an activity to contact you with regard to said activity.',
+        'You provided your personal data on the',
+        'Code Week Website login page',
+        'and on the',
+        'activity registration page.',
+
+
+    ],
+    '5-how_long' => [
+        'title' => 'How long do we keep your personal data?',
+        'items' => [
+            'Unit F.4 only keeps your personal data for the time necessary to fulfil the purpose of collection or further processing, namely for the duration of the browsing session. In addition, IP addresses might be saved for one year in the log files of the services for security reasons. As to the analytics tool, the IP address and the device ID (e.g. IMEI number and WIFI MAC address) are deleted immediately at disconnection.',
+
+
+        ]
+    ],
+    '6-protect_data' => [
+        'title' => 'How do we protect and safeguard your personal data?',
+        'items' => [
+            'All personal data in electronic format (e-mails, documents, databases, uploaded batches of data, etc.) are stored on the servers of the European Commission. All processing operations are carried out pursuant to the',
+            'Commission Decision (EU, Euratom) 2017/46',
+            ', of 10 January 2017, on the security of communication and information systems in the European Commission.',
+            'In order to protect your personal data, the Commission has put in place a number of technical and organisational measures. Technical measures include appropriate actions to address online security, risk of data loss, alteration of data or unauthorised access, taking into consideration the risk presented by the processing and the nature of the personal data being processed. Organisational measures include restricting access to the personal data solely to authorised persons with a legitimate need to know for the purposes of this processing operation.',
+
+        ]
+    ],
+    '7-access_data' => [
+        'title' =>
+            'Who has access to your personal data and to whom is it disclosed?',
+        'items' => [
+            'Access to your personal data is provided to the Commission staff responsible for carrying out this processing operation and to authorised staff according to the “need to know” principle. Such staff abide by statutory, and when required, additional confidentiality agreements.',
+            'Furthermore, access to your personal data can be provided to external contractors working on behalf of and under contractual agreement with the Commission service owning the websites and engaged in creation, maintenance, management, and archiving of websites according to the “need to know” principle.',
+            'The information we collect will not be given to any third party, except to the extent and for the purpose we may be required to do so by law.'
+        ]
+    ],
+    '8-rights' => [
+        'title' => 'What are your rights and how can you exercise them?',
+        'items' => [
+            'You have specific rights as a ‘data subject’ under Chapter III (Articles 14-25) of Regulation (EU) 2018/1725, in particular the right to access, rectify or erase your personal data and the right to restrict the processing of your personal data. Where applicable, you also have the right to object to the processing or the right to data portability.',
+            'You have the right to object to the processing of your personal data, which is lawfully carried out pursuant to Article 5(1)(a).',
+            'You have consented to provide your personal data for the present processing operation. You can withdraw your consent at any time by notifying Unit F.4. The withdrawal will not affect the lawfulness of the processing carried out before you withdrew your consent.',
+            'You can exercise your rights by contacting the Data Controller, or in case of conflict the Data Protection Officer. If necessary, you can also address the European Data Protection Supervisor. Their contact information is given under Heading 9 below.',
+            'Where you wish to exercise your rights in the context of one or several specific processing operations, please provide their description (i.e. their Record reference(s) as specified under Heading 10 below) in your request.'
+        ]
+    ],
+    '9-contact' => [
+        'title' => 'Contact information',
+        'data-controller' => [
+            'title' => 'The Data Controller',
+            'text' =>  'If you would like to exercise your rights under Regulation (EU) 2018/1725, or if you have comments, questions or concerns, or if you would like to submit a complaint regarding the collection and use of your personal data, please feel free to contact the Data Controller, Unit F.4, at'
+
+        ],
+        'data-protection-officer' => [
+            'title' => 'The Data Protection Officer (DPO) of the Commission',
+            'text' => "You may contact the Data Protection Officer (<a href='mailto:DATA-PROTECTION-OFFICER@ec.europa.eu'>DATA-PROTECTION-OFFICER@ec.europa.eu</a>) with regard to issues related to the processing of your personal data under Regulation (EU) 2018/1725."
+        ],
+        'european-data-protection' => [
+            'title' => 'The European Data Protection Supervisor (EDPS)',
+            'text' => 'You have the right to have recourse (i.e. you can lodge a complaint) to the European Data Protection Supervisor (<a href="mailto:edps@edps.europa.eu">edps@edps.europa.eu</a>) if you consider that your rights under Regulation (EU) 2018/1725 have been infringed as a result of the processing of your personal data by the Data Controller.'
+        ]
+    ],
+    '10-detailed-info' => [
+        'title' => 'Where to find more detailed information?',
+        'items' => [
+            'The Commission Data Protection Officer (DPO) publishes the register of all processing operations on personal data by the Commission, which have been documented and notified to him. You may access the register via the following link: <a href="https://ec.europa.eu/dpo-register">https://ec.europa.eu/dpo-register</a>',
+            'This specific processing operation has been included in the DPO’s public register with the following Record reference: <strong>DPR-EC-09706.1</strong>.'
+        ]
+    ]
+];
--- resources/views/static/privacy.blade.php
+++ resources/views/static/privacy.blade.php
@@ -16,26 +16,13 @@
                 DPR-EC-09706.1
             </div>
 
-            <h3 style="margin-top:10px">@lang('privacy-statement.table-of-contents')</h3>
-            <ol style="margin-bottom:20px">
-                <li><a href="#header-1">@lang('privacy-statement.1-intro.title')</a></li>
-                <li style="margin-top:4px"><a href="#header-2">@lang('privacy-statement.2-why.title')</a></li>
-                <li style="margin-top:4px"><a href="#header-3">@lang('privacy-statement.3-legal_process.title')</a></li>
-                <li style="margin-top:4px"><a href="#header-4">@lang('privacy-statement.4-collect_data.title')</a></li>
-                <li style="margin-top:4px"><a href="#header-5">@lang('privacy-statement.5-how_long.title')</a></li>
-                <li style="margin-top:4px"><a href="#header-6">@lang('privacy-statement.6-protect_data.title')</a>
-                </li>
-                <li style="margin-top:4px"><a href="#header-7">@lang('privacy-statement.7-access_data.title')</a></li>
-                <li style="margin-top:4px"><a href="#header-8">@lang('privacy-statement.8-rights.title')</a>
-                </li>
-                <li style="margin-top:4px"><a href="#header-9">@lang('privacy-statement.9-contact.title')</a></li>
-                <li style="margin-top:4px"><a href="#header-10">@lang('privacy-statement.10-detailed-info.title')</a></li>
-            </ol>
+            <x-privacy-toc></x-privacy-toc>
 
 
             <h3 id="header-1"><strong>1. @lang('privacy-statement.1-intro.title')</strong></h3>
             <p>@lang('privacy-statement.1-intro.items.1')</p>
             <p>@lang('privacy-statement.1-intro.items.2')</p>
+
             <p>@lang('privacy-statement.1-intro.items.3.0') <span style="font-style: italic">“@lang('privacy-statement.1-intro.items.3.1')”</span> @lang('privacy-statement.1-intro.items.3.2')</p>
 
 
