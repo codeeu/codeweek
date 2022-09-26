581c5f901c23ea52ae3b03cc6c3c46a9a765c908
Alain Van Driessche <alainvd@gmail.com>
2022-09-26 12:08:02 +0200

    Bulk Pauline Maas

---
+++ app/Console/Commands/excel/Generic.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands\excel;
+
+use App\Imports\DutchDanceEventsImport;
+use App\Imports\GenericEventsImport;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Maatwebsite\Excel\Facades\Excel;
+
+class Generic extends Command {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'excel:generic';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generic Excel Importer';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle() {
+        Log::info('Loading Dutch Dance File');
+
+        Excel::import(
+            new GenericEventsImport(),
+            'bulk-pauline.xlsx',
+            'excel'
+        );
+    }
+}
