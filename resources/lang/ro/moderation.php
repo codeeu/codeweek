cfaff1d302a484da77cae36eb375f179ebfa0c51
Alain Van Driessche <alainvd@gmail.com>
2022-09-28 14:17:39 +0200

    Moderation Translations

---
+++ resources/lang/ro/moderation.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'description' => [
+        'title' => 'Missing proper descriptions',
+        'text' => 'Buna ziua, pentru a putea aproba activitatea avem nevoie de cateva detalii in descriere si sa fie pe tema programarii. Multumim!'
+    ],
+    'missing-details' => [
+        'title' => 'Missing important details',
+        'text' => 'Bună ziua,
+
+Apreciem implicarea dvoastră, dar puteți adăuga vă rugăm mai multe informații? Ce activități plănuiți să organizați?
+
+Vă mulțumim, suntem aici dacă aveți orice întrebări.'
+    ],
+    'duplicate' => [
+        'title' => 'Duplicate',
+        'text' => 'Buna. Am aprobat unul din evenimentele cu acelasi Nume, imagine si timeline. Vad ca sunt cateva duplicate (printre care si acesta)
+
+Va multumim pentru implicare!'
+    ],
+    'not-related' => [
+        'title' => 'Not programming related',
+        'text' => 'Bună ziua,
+
+Apreciem implicarea dvoastră, dar nu înțelegem care este legătura dintre eveniment și programarea sau competențele digitale. Ne puteți lămuri adăugând mai multe informații?
+
+Vă mulțumim, suntem aici dacă aveți orice întrebări.'
+    ],
+
+];
