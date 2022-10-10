59c4602f24d061846c89a2aef75f92c6f1a6ed10
Alain VD <alainvd@gmail.com>
2022-10-10 16:20:22 +0200

    Dutch Reported

---
+++ app/Console/Commands/excel/Reported.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands\excel;
+
+use App\Imports\DutchDanceEventsImport;
+use App\Imports\GenericEventsImport;
+use App\Imports\ReportedEventsImport;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Maatwebsite\Excel\Facades\Excel;
+
+class Reported extends Command {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'excel:reported';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Reported Events Excel Importer';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle() {
+        Log::info('Loading Generic Excel');
+
+        Excel::import(
+            new ReportedEventsImport(),
+            'dutch-reported.xlsx',
+            'excel'
+        );
+    }
+}
---
+++ app/Imports/ReportedEventsImport.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Imports;
+
+use App\Event;
+use App\Tag;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use PhpOffice\PhpSpreadsheet\Cell\Cell;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use PhpOffice\PhpSpreadsheet\Cell\DefaultValueBinder;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+
+class ReportedEventsImport extends DefaultValueBinder implements
+    WithCustomValueBinder,
+    ToModel,
+    WithHeadingRow {
+    public function parseDate($date) {
+        return Date::excelToDateTimeObject($date);
+    }
+
+    public function loadUser($email) {
+        return User::firstOrCreate(
+            [
+                'email' => $email
+            ],
+            [
+                'firstname' => '',
+                'lastname' => '',
+                'username' => '',
+                'password' => bcrypt(Str::random())
+            ]
+        );
+    }
+
+    /**
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function model(array $row) {
+
+        $event = new Event([
+            'status' => 'APPROVED',
+            'title' => $row['activity_title'],
+            'slug' => str_slug($row['activity_title']),
+            'organizer' => $row['name_of_organisation'],
+            'description' => $row['description'],
+            'organizer_type' => $row['type_of_organisation'],
+            'activity_type' => $row['activity_type'],
+            'location' => $row['address'],
+            'event_url' => $row['organiser_website'],
+            'user_email' => '',
+            'creator_id' => $this->loadUser($row['contact_email'])->id,
+            'contact_person' => $row['contact_email'],
+            'country_iso' => $row['country'],
+            'picture' => $row['image_path'],
+            'pub_date' => now(),
+            'created' => now(),
+            'updated' => now(),
+            'codeweek_for_all_participation_code' => 'NL-Deursen-001',
+            'start_date' => $this->parseDate($row['start_date']),
+            'end_date' => $this->parseDate($row['end_date']),
+            'geoposition' => $row['latitude'] . ',' . $row['longitude'],
+            'longitude' => $row['longitude'],
+            'latitude' => $row['latitude'],
+            'language' => strtolower($row['language']),
+            'participants_count' => 30,
+            'average_participant_age' => 10,
+            'percentage_of_females' => 50,
+        ]);
+
+        $event->save();
+
+        if ($row['audience_comma_separated_ids']) {
+            $event
+                ->audiences()
+                ->attach(explode(',', $row['audience_comma_separated_ids']));
+        }
+        if ($row['theme_comma_separated_ids']) {
+            $event
+                ->themes()
+                ->attach(explode(',', $row['theme_comma_separated_ids']));
+        }
+
+        Log::info($event->slug);
+        return $event;
+    }
+}
