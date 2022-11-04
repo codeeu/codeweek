9e8b42300f16ba2d95461fee5ec0b4d17d0d71db
Alain Van Driessche <alainvd@gmail.com>
2022-11-04 17:25:24 +0100

    Cleansing email

---
+++ app/Console/Commands/CertificatesIssues.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Participation;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class CertificatesIssues extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'certificate:issues';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check for certificates with issues being generated';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        $issues = Participation::whereNull('participation_url')->where('created_at','<', Carbon::now()->subMinutes(10))->get();
+
+        if(count($issues) > 0){
+            //Send warning Email
+            Mail::to(config('codeweek.administrator'))->queue(new \App\Mail\WarningEmail("We have ". count($issues). " certificates of participation that have not been generated"));
+        }
+
+        return Command::SUCCESS;
+    }
+}
---
+++ resources/views/emails/en/warning.blade.php
@@ -0,0 +1,4 @@
+@component('mail::message')
+    <h1>Warning Message</h1>
+    <h3>{{$message}}</h3>
+@endcomponent
