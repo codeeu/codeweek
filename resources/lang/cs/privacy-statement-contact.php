7b07824b9454673b0481ab60fb72cd284e430b93
Alain Van Driessche <alainvd@gmail.com>
2022-11-10 16:07:02 +0100

    23 languages - privacy statement contact points

---
+++ resources/lang/cs/privacy-statement-contact.php
@@ -0,0 +1,68 @@
+<?php
+
+return [
+    'subtitle' => 'V tomto prohlášení o ochraně soukromí jsou obsaženy informace ohledně zpracování a ochrany vašich osobních údajů.',
+    'processing-operation' => [
+        'Operace zpracování',
+        'správa ambasadorů Týdne programování, vedoucích učitelů a registrací aktivit na těchto internetových stránkách'
+    ],
+    'data-processors' => [
+        'Zpracovatelé údajů',
+        'GOPA Com. S.A. a EUN Partnership AISBL (European Schoolnet), info@codeweek.eu'
+    ],
+    '1-intro' => [
+        'Informace týkající se zpracování osobních údajů pro účely správy ambasadorů Týdne programování, vedoucích učitelů a registrací aktivit na těchto internetových stránkách.'
+    ],
+
+    '3-legal_process' => [
+        'Zákonnost operace zpracování údajů: oddělení G.2 shromažďuje, ukládá a zveřejňuje vaše osobní údaje, protože jste zaškrtnutím políčka udělili výslovný souhlas, když jste se registrovali jako ambasador Týdne programování nebo vedoucí učitel nebo když jste registrovali akci v rámci <a href="https://codeweek.eu/register">Evropského týdne programování.</a>',
+
+    ],
+    '4-collect_data' => [
+        '<strong>Povinně</strong> shromažďované osobní údaje <strong>ambasadorů a vedoucích učitelů</strong> jsou:',
+        'jméno',
+        'e-mailová adresa',
+        'heslo',
+        'fotografie',
+        'životopis',
+        'titul',
+        'u vedoucích učitelů odbornost.',
+        'Když <strong>registrujete aktivitu, povinně</strong> shromažďované osobní údaje jsou:',
+        'název organizace',
+        'druh organizace',
+        'kontaktní e-mailová adresa',
+        'Poskytnutí těchto údajů je povinné, pokud chcete být ambasadorem Týdne programování či vedoucím učitelem, a pro registraci aktivity na internetových stránkách Týdne programování.',
+        '<strong>Ambasadoři a vedoucí učitelé</strong> mohou navíc <strong>dobrovolně</strong> poskytnout tyto osobní údaje:',
+        'účty na sociálních médiích',
+        '<strong>Organizátoři aktivit</strong> mohou <strong>dobrovolně</strong> poskytnout tyto údaje:',
+        'veřejnou kontaktní e-mailovou adresu',
+        'internetové stránky organizátora',
+        'kód pro soutěž Code Week for All',
+        'Pokud jste organizátorem aktivity, vezměte prosím na vědomí, že příslušný ambasador ve vaší zemi nebo oddělení G.2 mohou použít osobní údaje, které jste poskytli při registraci aktivity, k tomu, aby vás v souvislosti s touto aktivitou kontaktovali.',
+        'Své osobní údaje jste poskytli na',
+        'přihlašovací stránce Týdne programování',
+        'a na',
+        'stránce pro registraci aktivit.',
+    ],
+    '5-how_long' => [
+        'Všechny osobní údaje budou z databází vymazány, pokud z iniciativy vystoupíte, nebo 5 let po jejím ukončení.',
+    ],
+
+    '6-protect_data' => [
+        'Všechny osobní údaje v elektronické podobě (e-mailové zprávy, dokumenty, databáze, nahrané soubory dat atd.) jsou uloženy na serverech Evropské komise nebo jejích smluvních dodavatelů. Veškeré operace zpracování se řídí <a href="https://eur-lex.europa.eu/legal-content/EN/TXT/?qid=1548093747090&uri=CELEX:32017D0046">rozhodnutím Komise (EU, Euratom) 2017/46</a> ze dne 10. ledna 2017 o bezpečnosti komunikačních a informačních systémů v Evropské komisi.',
+        'Smluvní dodavatelé Komise jsou vázáni zvláštním smluvním ustanovením o zpracovávání vašich údajů jménem Komise a povinnostmi týkajícími se důvěrnosti, jež vyplývají z provedení obecného nařízení o ochraně osobních údajů („GDPR“ – <a href="https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX%3A32016R0679">nařízení (EU) 2016/679</a>) do vnitrostátních předpisů členských států EU.',
+        'Za účelem ochrany vašich osobních údajů zavedla Komise řadu technických a organizačních opatření. Technická opatření zahrnují náležité kroky k zajištění bezpečnosti na internetu a ochrany před ztrátou údajů, jejich pozměněním nebo neoprávněným přístupem k nim, přičemž se přihlíží k riziku spojenému se zpracováním údajů a k povaze zpracovávaných osobních údajů. Organizační opatření zahrnují omezení přístupu k osobním údajům výlučně na oprávněné osoby, jež mají legitimní důvod se s nimi seznámit pro účely této operace zpracování.',
+    ],
+    '7-access_data' => [
+        'K veškerým osobním údajům, které nám poskytnete, mohou zaměstnanci Komise přistupovat na základě zásady „vědět jen to nejnutnější“. Přístup k vašim osobním údajům je kromě toho na základě zásady „vědět jen to nejnutnější“ poskytnut našemu smluvnímu dodavateli.',
+        'V zájmu zviditelnění ambasadora Týdne programování, vedoucích učitelů nebo registrovaných aktivit jsou všechny vaše osobní údaje zveřejněny na <a href="https://codeweek.eu/">této stránce</a>.',
+        'Oddělení G.2 může navíc sdílet vaše kontaktní údaje s osobami z řad veřejnosti nebo zúčastněných stran, které vás potřebují kontaktovat v souvislosti s Týdnem programování. Pokud jste organizátorem aktivity, vezměte prosím na vědomí, že příslušný ambasador ve vaší zemi nebo oddělení G.2 mohou použít osobní údaje, které jste poskytli při registraci aktivity, k tomu, aby vás v souvislosti s touto aktivitou kontaktovali.',
+    ],
+    '8-rights' => [
+        'S poskytnutím svých osobních údajů oddělení G.2 k této operaci zpracování údajů jste udělili souhlas. Svůj souhlas můžete kdykoli odvolat tak, že to oznámíte oddělení G.2 na adresu CNECT-G2@ec.europa.eu. Odvolání souhlasu nijak neovlivní zákonnost zpracování provedeného předtím, než jste souhlas odvolali.'
+    ],
+
+    '10-detailed-info' => [
+        'Tato konkrétní operace zpracování údajů byla zanesena do veřejného rejstříku pověřence pro ochranu osobních údajů pod tímto referenčním číslem: DPR-EC-02631 Správa kontaktů pro politiky, programy a projekty GŘ CNECT.'
+    ]
+];
---
+++ resources/lang/lt/privacy-statement-contact.php
@@ -0,0 +1,68 @@
+<?php
+
+return [
+    'subtitle' => 'Šiame pareiškime apie privatumo apsaugą pateikiama informacija apie jūsų asmens duomenų tvarkymą ir apsaugą.',
+    'processing-operation' => [
+        'Duomenų tvarkymo operacija:',
+        'administravimas, susijęs su programavimo savaitės ambasadoriais, veiklą koordinuojančiais mokytojais ir veiklos registravimu svetainėje.'
+    ],
+    'data-processors' => [
+        'Duomenų tvarkytojai:',
+        '„GOPA Com. S.A.“ ir „EUN Partnership AISBL“ („European Schoolnet“), info@codeweek.eu'
+    ],
+    '1-intro' => [
+        'Informacija apie asmens duomenų tvarkymą, atliekamą vykdant administravimą, susijusį su programavimo savaitės ambasadoriais, veiklą koordinuojančiais mokytojais ir veiklos registravimu svetainėje.'
+    ],
+
+    '3-legal_process' => [
+        'Duomenų tvarkymo teisėtumas. G.2 skyrius jūsų asmens duomenis renka, saugoja ir skelbia, nes tam davėte aiškų sutikimą pažymėdamas langelį tada, kai registravotės tapti programavimo savaitės ambasadoriumi ar veiklą koordinuojančiu mokytoju arba kai teikėte prašymą užregistruoti veiklą <a href="https://codeweek.eu/register">Europos programavimo savaitės</a> svetainėje.',
+
+    ],
+    '4-collect_data' => [
+        'Kai registruojamasi tapti <strong>ambasadoriumi ar veiklą koordinuojančiu mokytoju,</strong> renkami šie <strong>privalomi</strong> asmens duomenys:',
+        'vardas ir pavardė;',
+        'e. pašto adresas;',
+        'slaptažodis;',
+        'atvaizdas;',
+        'biografija;',
+        'kreipinys;',
+        'kompetencijos sritis (jei registruojamasi tapti veiklą koordinuojančiu mokytoju).',
+        'Kai <strong>registruojama veikla,</strong> renkami šie <strong>privalomi</strong> asmens duomenys:',
+        'organizacijos pavadinimas;',
+        'organizacijos rūšis;',
+        'kontaktinis e. pašto adresas.',
+        'Šiuos duomenis pateikti privaloma, kad galėtumėte tapti ambasadoriumi ar veiklą koordinuojančiu mokytoju arba užregistruoti veiklą.',
+        'Be to, <strong>ambasadoriai ir veiklą koordinuojantys mokytojai</strong> gali <strong>savanoriškai</strong> pateikti šiuos asmens duomenis:',
+        'nuorodas į socialinių tinklų paskyras.',
+        '<strong>Veiklos organizatoriai</strong> gali <strong>savanoriškai</strong> pateikti šiuos duomenis:',
+        'ryšiams su visuomene skirtą e. pašto adresą;',
+        'nuorodą į organizatorių svetainę;',
+        '„Code Week for All“ kodą.',
+        'Jei esate veiklos organizatorius, atkreipkite dėmesį į tai, kad asmens duomenimis, kuriuos pateikėte registruodamas veiklą, gali naudotis su jumis dėl tos veiklos norintis susisiekti jūsų šaliai atstovaujantis ambasadorius arba G.2 skyrius.',
+        'Savo asmens duomenis pateikėte',
+        'Programavimo savaitės svetainės prisijungimo puslapyje',
+        'ir',
+        'veiklos registravimu puslapyje.',
+    ],
+    '5-how_long' => [
+        'Visi jūsų asmens duomenys iš duomenų bazių bus pašalinti, jei pasitrauksite iš šios iniciatyvos arba praėjus penkeriems metams nuo iniciatyvos pabaigos.',
+    ],
+
+    '6-protect_data' => [
+        'Visi elektroniniai asmens duomenys (e. laiškai, dokumentai, duomenų bazės, įkelti duomenų rinkiniai ir pan.) saugomi Europos Komisijos arba jos rangovų serveriuose. Visos duomenų tvarkymo operacijos vykdomos laikantis 2017 m. sausio 10 d. <a href="https://eur-lex.europa.eu/legal-content/EN/TXT/?qid=1548093747090&uri=CELEX:32017D0046">Komisijos sprendimo (ES, Euratomas) 2017/46</a> dėl Europos Komisijos ryšių ir informacinių sistemų saugumo.',
+        'Komisijos rangovai, tvarkydami jūsų duomenis Komisijos vardu, yra teisiškai įpareigoti laikytis ir konkrečių sutarties sąlygų, ir konfidencialumo pareigų, nustatytų į ES valstybių narių teisę perkėlus Bendrąjį duomenų apsaugos reglamentą (BDAR, <a href="https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX%3A32016R0679">Reglamentas (ES) 2016/679)</a>.',
+        'Kad apsaugotų jūsų asmens duomenis, Komisija taiko tam tikras technines ir organizacines priemones. Prie techninių priemonių priskiriami atitinkami veiksmai, kuriais užtikrinamas kibernetinis saugumas ir mažinama duomenų praradimo, jų pakeitimo ar neteisėtos prieigos prie jų rizika, atsižvelgiant į su asmens duomenų tvarkymu susijusią riziką ir tvarkomų asmens duomenų pobūdį. Prie organizacinių priemonių priskiriamas prieigos prie asmens duomenų suteikimas tik įgaliotiems asmenims, turintiems teisėtą poreikį su jais susipažinti šios duomenų tvarkymo operacijos tikslais.',
+    ],
+    '7-access_data' => [
+        'Su visais jūsų pateiktais asmens duomenimis pagal būtinybės žinoti principą gali susipažinti Komisijos darbuotojai. Be to, pagal tą patį principą su jais susipažinti leidžiama ir mūsų rangovui.',
+ 'Siekiant didinti programavimo savaitės ambasadorių, veiklą koordinuojančių mokytojų ir užregistruotos veiklos žinomumą, visi jūsų asmens duomenys skelbiami šioje <a href="https://codeweek.eu/">svetainėje</a>.',
+ 'Be to, G.2 skyrius jūsų kontaktiniais duomenimis gali pasidalyti su bet kuriuo visuomenės nariu ar suinteresuotuoju subjektu, kuriam reikia su jumis susisiekti programavimo savaitės klausimu. Jei esate veiklos organizatorius, atkreipkite dėmesį į tai, kad asmens duomenimis, kuriuos pateikėte registruodamas veiklą, gali naudotis su jumis dėl tos veiklos norintis susisiekti jūsų šaliai atstovaujantis ambasadorius arba G.2 skyrius.',
+    ],
+    '8-rights' => [
+ 'Jūs sutikote šios duomenų tvarkymo operacijos tikslais pateikti savo asmens duomenis G.2 skyriui. Savo sutikimą galite bet kuriuo metu atšaukti, apie tai pranešdamas G.2 skyriui adresu CNECT-G2@ec.europa.eu. Sutikimo atšaukimas nedarys poveikio duomenų tvarkymo operacijų, atliktų iki jums atšaukiant sutikimą, teisėtumui.'
+    ],
+
+    '10-detailed-info' => [
+ 'Ši konkreti duomenų tvarkymo operacija viešame duomenų apsaugos pareigūno registre užregistruota šiuo ankstesnio pranešimo numeriu: DPR-EC-02631 – Ryšių tinklų, turinio ir technologijų GD politikos, programų ir projektų kontaktinių punktų valdymas.'
+    ]
+];
