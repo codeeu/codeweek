b6d7b2eea370ac0c942f09a2e12760045b6aa028
Alain Van Driessche <alainvd@gmail.com>
2022-12-22 09:31:54 +0100

    Lux import

--- app/Console/Commands/excel/Luxembourg.php
+++ app/Console/Commands/excel/Luxembourg.php
@@ -39,6 +39,6 @@ class Luxembourg extends Command {
     public function handle() {
         Log::info('Loading Luxembourg Excel File');
 
-        Excel::import(new LuxembourgEventsImport(), 'lux1.xlsx', 'excel');
+        Excel::import(new LuxembourgEventsImport(), 'lux22-1.xlsx', 'excel');
     }
 }
