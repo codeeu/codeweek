2882a16dfad46b510e46cb295df5f9877c12ac20
Alain Van Driessche <alainvd@gmail.com>
2023-06-16 15:54:08 +0200

    Super organisers certificates

--- resources/views/emails/en/notify-super-organiser.blade.php
+++ resources/views/emails/en/notify-super-organiser.blade.php
@@ -1,9 +1,9 @@
 @component('mail::message')
 Dear Code Week activity organiser,
 
-We are delighted to let you know that you successfully completed the 2021 Code Week Super Organiser Challenge. <br/>
+We are delighted to let you know that you successfully completed the 2022 Code Week Super Organiser Challenge. <br/>
 
-That is organising 10 or more Code Week activities in 2021. Congratulations and thank you for playing a major part in making last year’s Code Week, <a href="https://digital-strategy.ec.europa.eu/en/news/eu-code-week-breaks-record-number-activities-2021-78000-events">the most successful edition yet.</a><br/>
+That is organising 10 or more Code Week activities in 2022. Congratulations and thank you for playing a major part in <a href="https://blog.codeweek.eu/code-week-2022-wrap-up-in-numbers/">last year’s Code Week</a>.<br/>
 
 We believe it is vital to bring coding, computing and computational thinking to as many people as possible – and you have helped up us reach this goal!<br/>
 
@@ -16,7 +16,7 @@ You can personalise and download your Super Organiser certificate by clicking on
     Get your Certificate
 @endcomponent
 
-Thank you very much for helping us advance digital literacy! We hope to work together again in the 2022 edition of EU Code Week, which will take place from 8-23 October.<br/><br/>
+Thank you very much for helping us advance digital literacy! We hope to work together again in the 2023 edition of EU Code Week, which will take place from 7-22 October.<br/><br/>
 
 
 
