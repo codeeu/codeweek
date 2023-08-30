79a8eaeb300270bb5b3ff9d94987e8b7358e8e88
Alain Van Driessche <alainvd@gmail.com>
2023-08-30 17:15:21 +0200

    Online courses

---
+++ resources/lang/en/online-courses.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => 'EU CODE WEEK ONLINE COURSES',
+    'description' => [
+        "EU Code Week offers professional development opportunities in the form of massive open online courses (MOOCs) with the aim to support teachers in effectively incorporating coding and computational thinking into their teaching practice.",
+        "EU Code Week MOOCs are open to all educators, regardless of their students' age or the subject they teach, and no prior experience or knowledge is required to participate.",
+        "EU Code Week MOOCs offer free and accessible resources, materials, ideas and best practice examples to find inspiration and empower students by introducing coding and computational thinking, emerging technologies and artificial intelligence safely into the classroom."
+    ],
+    'more-info' => "More information about the EU Code Week MOOCs: "
+];
