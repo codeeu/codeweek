afa79208373919e4d6893212cb863c7d47920405
Alain Van Driessche <alainvd@gmail.com>
2023-10-09 08:27:18 +0200

    share box update text

--- resources/views/2021/challenges/_share.blade.php
+++ resources/views/2021/challenges/_share.blade.php
@@ -2,28 +2,28 @@
 
     <div class="leading-6 p-4">
         @lang('challenges.share-box.0') <span class="font-bold">@lang('challenges.share-box.1')</span>: @lang('challenges.share-box.2') <span class="italic font-bold">@lang('challenges.share-box.3')</span> @lang('challenges.share-box.4')“<span class="font-bold">@lang('challenges.share-box.5')</span>”, @lang('challenges.share-box.6').<br/>
-        @lang('challenges.share.9') @lang('challenges.share-box.8') <span class="font-bold">@lang('challenges.share-box.9') <a href="https://www.facebook.com/codeEU/">@lang('challenges.share-box.10')</a></span> . @lang('challenges.share-box.11').<br/>
-        @lang('challenges.share-box.12').
+{{--        @lang('challenges.share.9') @lang('challenges.share-box.8') <span class="font-bold">@lang('challenges.share-box.9') <a href="https://www.facebook.com/codeEU/">@lang('challenges.share-box.10')</a></span> . @lang('challenges.share-box.11').<br/>--}}
+{{--        @lang('challenges.share-box.12').--}}
 
-        <div class="leading-6 text-base text-left mt-2">
+{{--        <div class="leading-6 text-base text-left mt-2">--}}
 
-            <strong>@lang('challenges.bingo.0')</strong><br/>
-            <div>
-                @lang('challenges.bingo.1') <a
-                        href="https://codeweek-s3.s3.eu-west-1.amazonaws.com/cw2021/Bingo+card.pdf">@lang('challenges.bingo.2')</a> @lang('challenges.bingo.3')
-                <br/>
-            </div>
-        </div>
+{{--            <strong>@lang('challenges.bingo.0')</strong><br/>--}}
+{{--            <div>--}}
+{{--                @lang('challenges.bingo.1') <a--}}
+{{--                        href="https://codeweek-s3.s3.eu-west-1.amazonaws.com/cw2021/Bingo+card.pdf">@lang('challenges.bingo.2')</a> @lang('challenges.bingo.3')--}}
+{{--                <br/>--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
-        <div class="leading-6 text-base text-left mt-2">
+{{--        <div class="leading-6 text-base text-left mt-2">--}}
 
-            <strong>@lang('challenges.bingo.4')</strong><br/>
-            <div class="mt-2">
-                @lang('challenges.bingo.5') <strong>@lang('challenges.bingo.6')</strong><br/>
-                @lang('challenges.bingo.7') <strong>#CodeWeekChallengesBingo</strong><br/>
-            </div>
+{{--            <strong>@lang('challenges.bingo.4')</strong><br/>--}}
+{{--            <div class="mt-2">--}}
+{{--                @lang('challenges.bingo.5') <strong>@lang('challenges.bingo.6')</strong><br/>--}}
+{{--                @lang('challenges.bingo.7') <strong>#CodeWeekChallengesBingo</strong><br/>--}}
+{{--            </div>--}}
 
-        </div>
+{{--        </div>--}}
 
     </div>
 
