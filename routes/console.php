096c05b4ac5b13c98d280df371ffd761a2242232
Shift <shift@laravelshift.com>
2024-04-26 13:37:32 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,35 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::command('delete:unactiveusers')->weeklyOn(4, '8:00');
+
+Schedule::command('remind:ambassadors')->dailyAt('9:00');
+
+Schedule::command('remind:creators')->dailyAt('10:00');
+
+Schedule::command('clean:remote')->dailyAt('12:05');
+
+Schedule::command('import:eeducation')->dailyAt('04:00');
+
+Schedule::command('notify:administrators')->hourlyAt(30);
+
+Schedule::command('rss:meetandcode')->hourlyAt(5);
+
+Schedule::command('api:germany')->hourlyAt(10);
+
+Schedule::command('magic:key')->hourlyAt(13);
+
+Schedule::command('relocate')->hourlyAt(30);
+
+Schedule::command('relocate:country')->everyTwoMinutes();
+
+Schedule::command('certificate:issues')->hourlyAt(33);
+
+Schedule::command('certificates:fix')->everyFiveMinutes();
