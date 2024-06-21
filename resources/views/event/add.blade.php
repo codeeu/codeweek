ad985996ce486c78d9f300ecaa901613a293b4d2
Alain Van Driessche <alainvd@gmail.com>
2024-06-21 17:12:11 +0200

    Add Activity WiP

--- resources/views/event/add.blade.php
+++ resources/views/event/add.blade.php
@@ -455,3 +455,11 @@
     </script>
 
 @endpush
+
+@section('extra-css')
+    <link rel="stylesheet" href="{{asset('css/MultiSelect.css')}}">
+@endsection
+
+@section('extra-js')
+    <script src="{{asset('js/MultiSelect.js')}}"></script>
+@endsection
