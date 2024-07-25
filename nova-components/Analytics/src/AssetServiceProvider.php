2377385cad1a29b8c5e2f03b6ce533fa37325edf
Alain Van Driessche <alainvd@gmail.com>
2024-07-25 13:45:50 +0200

    Laravel Nova 4

---
+++ nova-components/Analytics/src/AssetServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Acme\Analytics;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Nova;
+
+class AssetServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::serving(function (ServingNova $event) {
+            Nova::script('analytics', __DIR__.'/../dist/js/asset.js');
+            Nova::style('analytics', __DIR__.'/../dist/css/asset.css');
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
