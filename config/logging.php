f16f41c02adcc30b9945104e637f31eb8b4e0cff
Alain Van Driessche <alainvd@gmail.com>
2024-08-09 14:27:00 +0200

    Logging Configuration was lost

--- config/logging.php
+++ config/logging.php
@@ -3,6 +3,22 @@
 return [
 
     'channels' => [
+        "stack" => [
+            "driver" => "stack",
+            "channels" => ["single", "daily"],
+            "ignore_exceptions" => false,
+        ],
+        "single" => [
+            "driver" => "single",
+            "path" => storage_path("logs/laravel.log"),
+            "level" => env("LOG_LEVEL", "debug"),
+        ],
+        "daily" => [
+            "driver" => "daily",
+            "path" => storage_path("logs/laravel.log"),
+            "level" => env("LOG_LEVEL", "debug"),
+            "days" => 14,
+        ],
         'mails_sent' => [
             'driver' => 'single',
             'path' => storage_path('logs/mails_sent.log'),
