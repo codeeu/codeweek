fdcb4b6105f7ff98975e6a7bbc6e098fa577a603
Jakub Zalewski <jakub@insidenewcity.com>
2024-10-08 12:02:29 +0200

    apple events 2024

---
+++ app/Console/Commands/excel/AppleEvents2024.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands\excel;
+
+use App\Imports\AppleEventsImport;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Maatwebsite\Excel\Facades\Excel;
+
+class AppleEvents extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'excel:apple';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Apple Events 2024 From Excel File';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        Log::info('Loading Apple Excel File');
+
+        Excel::import(
+            new AppleEventsImport(),
+            'Apple_events_code_week_2024_BATCH1.xlsx',
+            'excel'
+        );
+    }
+}
