a6d17b6c5a96872087abdd3c6241632cd408f2cc
bernardhanna <bernard@matrixinternet.ie>
2024-10-18 15:07:05 +0100

    Added minecraft and tailwind

---
+++ resources/views/homepage/minecraft.blade.php
@@ -0,0 +1,28 @@
+<section id="minecraft" class="w-full px-4 tablet:px-[50px]">
+  <div class="w-full mx-auto mb-6 max-tablet:mt-6 max-w-[1181px]">
+    <div class="flex flex-row justify-between max-tablet:pb-8 tablet:py-8 lg:h-[512px] tablet:px-8 w-full bg-grey max-tablet:flex-col max-tablet:min-h-[auto] tablet:gap-[64px]">
+      <div class="flex flex-col justify-center max-w-[735px] max-tablet:max-w-full">
+          <picture>
+            <source type="image/avif" srcset="/images/avif/homepage/minecraft1.avif">
+            <source type="image/webp" srcset="/images/webp/homepage/minecraft1.webp">
+            <img class="object-cover h-auto max-h-[419px] max-w-full tablet:max-w-[735px] w-full aspect-[1.75] max-tablet:max-w-full" src="/images/homepage/minecraft1.png" alt="Minecraft Education visual representation" loading="lazy">
+          </picture>
+      </div>
+
+      <div class="flex flex-col justify-center tablet:max-w-[322px] max-tablet:w-full max-tablet:min-w-0 max-sm:w-full max-tablet:text-center max-tablet:px-8">
+        <div class="flex flex-col w-full max-tablet:pt-[15px]">
+          <img loading="lazy" src="/images/homepage/minecraftlogo.svg" alt="Minecraft Education logo" class="max-w-[350px] object-contain mx-auto w-full aspect-[2.4]" />
+        </div>
+        <p class="text-xl font-bold leading-truly-normal text-secondary">
+          Team up with Minecraft Education for EU Code Week! Discover how Minecraft Education can teach coding and AI skills.
+        </p>
+        <p class="pt-0 pb-0 text-xl font-bold leading-truly-normal text-secondary">
+          Sign up to bring fun, live play-alongs direct to your classroom!
+        </p>
+        <a taget="_blank" href=" https://www.codeweekcoders.eu/" class="mt-2 self-start px-6 text-base font-bold text-white uppercase bg-secondary hover:bg-blue-primary leading-truly-normal rounded-lg min-h-[60px] lg:max-w-[263px] flex items-center justify-center max-tablet:px-5 max-tablet:w-full max-tablet:max-w-full max-lg:text-center max-tablet:mx-auto" role="button">
+          Start your Minecraft Education Journey HERE
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
