c9fc70967b45571caba9c741c7335f9503e3e145
bernardhanna <bernard@matrixinternet.ie>
2024-11-11 14:42:53 +0000

    Added new partner and assosciated files and content

---
+++ resources/lang/es/partners.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'ja_spain_name' => 'Junior Achievement España',
+    'ja_spain_description' => 'La Fundacioìn Junior Achievement nace en EEUU en el año 1919. Con más de 100 años de historia se ha convertido en una de las organizaciones internacionales sin ánimo de lucro, dedicada a promover la formación emprendedora, la educación financiera y la preparación laboral, más grandes del mundo. Creada en España en el año 2001, la Fundación Junior Achievement España trabaja con centros educativos, organizaciones empresariales y gobiernos para proporcionar a los jóvenes experiencias que los ayuden a desarrollar las habilidades y competencias necesarias para triunfar en una economía global. Para ello, cuenta con un plan educativo desarrollado por pedagogos expertos dirigido a alumnos de todas las edades, desde los 7 a los 30 años.',
+    // Add more translations as needed
+];
