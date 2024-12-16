53e96a7f60f646f8496f41059d40c65934b49e12
payme <siroumi@matrixinternet.ie>
2024-12-16 16:15:27 +0000

    Cookie Policy Changes and removing old cookies

--- resources/views/static/cookie.blade.php
+++ resources/views/static/cookie.blade.php
@@ -3,18 +3,15 @@
 @section('content')
 
     <section id="codeweek-privacy-page" class="codeweek-page">
-
         <section class="codeweek-content-wrapper cookies" style="margin-top: 0;">
-
+            <h1 style="margin-bottom:10px;">@lang('cookie_policy.title')</h1>
              <!-- Cookie Declaration Script -->
              <script id="CookieDeclaration" 
                     src="https://consent.cookiebot.com/719385d2-f5d2-4806-8352-72e5ebe53996/cd.js" 
                     type="text/javascript" 
                     async>
             </script>
-
         </section>
-
     </section>
 
 @endsection
