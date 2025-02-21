864dbf8c1f37941bd8cf30832aa12aa547de7165
bernardhanna <bernard@matrixinternet.ie>
2025-02-21 14:38:52 +0000

    added community text

--- resources/views/community.blade.php
+++ resources/views/community.blade.php
@@ -106,8 +106,7 @@
                                 </h4>
                                 <p
                                     class="text-xl  md:text-2xl leading-8 text-[#333E48] p-0 mb-4 max-md:max-w-full max-w-[525px]">
-                                    vibrant network of educators, volunteers and ambassadors driving EU Code Week forward. Connect, collaborate and inspire the next generation of digital innovators in your country — and beyond.
-
+                                    A vibrant network of educators, volunteers and ambassadors driving EU Code Week forward. Connect, collaborate and inspire the next generation of digital innovators in your country — and beyond.
                                 </p>
                                 <a class="hidden bg-primary hover:bg-hover-orange rounded-full py-4 px-6 md:px-10 font-semibold text-base w-full md:w-auto text-center text-[#20262C] transition-all duration-300"
                                     href="#">
