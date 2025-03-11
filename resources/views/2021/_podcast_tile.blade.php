5670a5aac1cf12fdaee6ccfd53020b0ccc5eb281
stevan <stevan.popovicx@gmail.com>
2025-03-11 15:01:54 +0100

    Podcasts page

--- resources/views/2021/_podcast_tile.blade.php
+++ resources/views/2021/_podcast_tile.blade.php
@@ -1,20 +1,16 @@
-<div class="bg-gray-200 w-90 shadow-lg rounded p-2">
-    <div class="group relative">
-        <img alt="Placeholder" class="block w-full rounded"  src="https://codeweek-podcasts.s3.eu-west-1.amazonaws.com/art/{{$podcast->image}}">
-        <div x-on:click="window.location='{{route('podcast', $podcast)}}'" class="cursor-pointer absolute bg-black rounded bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 duration-700 transition justify-evenly">
-            <button class="hover:scale-110 text-white outline-none opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-20 w-20" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
-                </svg>
-            </button>
-
-
-        </div>
-    </div>
-    <div class="p-2">
-        <a href="{{route('podcast', $podcast)}}">
-        <h2 class="text-center orange py-1 text-base justify-center">{{$podcast->title}}</h2>
+<div class="rounded-lg bg-white overflow-hidden">
+    <div class="relative">
+        <img alt="Placeholder" class="block w-full rounded" src="https://codeweek-podcasts.s3.eu-west-1.amazonaws.com/art/{{$podcast->image}}" />
+        <a href="{{route('podcast', $podcast)}}" class="bg-white rounded-full w-12 h-12 flex justify-center items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <img class="w-6 ml-1" src="/images/fi_play.svg" />
         </a>
-{{--        <div class="text-gray-400 text-sm">By @ax-production</div>--}}
+        @if($podcast->time)
+            <div class="absolute bg-[#1C4DA1CC] px-2 py-1 rounded-md bottom-2.5 right-2.5 text-white font-semibold text-sm">
+                {{ $podcast->time }}
+            </div>
+        @endif
+    </div>
+    <div class="px-6 py-8">
+        <a href="{{route('podcast', $podcast)}}" class="text-dark-blue text-center text-lg p-0 font-semibold mb-2 font-['Montserrat'] block">{{$podcast->title}}</a>
     </div>
 </div>
