d7ac239a74eebdc8707a6886cea2fefd93c6cad6
Matrix Dev <testing@matrixinternet.ie>
2025-03-20 20:52:06 +0530

    Spanish translations for homepage

---
+++ resources/lang/es/footer.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+    'privacy_policy' => 'Política de privacidad',
+    'cookies_policy' => 'Política de cookies',
+    'about_us' => 'Sobre Nosotros',
+    'about_code_week' => 'Sobre la Semana de la Programación',
+    'our_values' => 'Nuestros valores',
+    'statistics' => 'Estadísticas',
+    'partners_sponsors' => 'Socios y patrocinadores',
+    'community' => 'Comunidad',
+    'quick_links' => 'Enlaces rápidos',
+    'register' => 'Registrarse',
+    'activities_events' => 'Actividades y Eventos',
+    'learn_teach' => 'Aprender y Enseñar',
+    'news' => 'Noticias',
+    'newsletter_signup' => 'Suscripción al boletín informativo',
+    'educational_resources' => 'Recursos educativos',
+    'coding_home' => 'Programación en casa',
+    'podcast' => 'Podcasts',
+    'challenges' => 'Desafíos',
+    'hackathons' => 'Hackatones',
+    'designed_and_developed_by' => 'Diseñado y desarrollado por.',
+    'all_rights_reserved' => 'Todos los derechos reservados'
+];
