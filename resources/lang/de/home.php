113a11a9dcdb6e2066f49c8d69eef73721108e7c
Matrix Dev <testing@matrixinternet.ie>
2025-04-25 18:50:12 +0530

    Banner typo changes for FR and DE

--- resources/lang/de/home.php
+++ resources/lang/de/home.php
@@ -3,7 +3,7 @@
 return [
     'about' => 'Die EU Code Week ist eine Breiteninitiative, die der Bevölkerung das Programmieren und digitale Kompetenzen auf spaßige und ansprechende Weise näherbringen soll.',
     'when' => '14.-27. Oktober 2024',
-    'when_text' => '>Programmieren zu lernen, hilft uns, den Sinn der sich schnell verändernden Welt um uns herum zu verstehen, unser Verständnis für die Technologie zu erweitern und Kompetenzen und Fähigkeiten zu entwickeln, um neue Ideen und Innovationen umzusetzen.',
+    'when_text' => 'Programmieren zu lernen, hilft uns, den Sinn der sich schnell verändernden Welt um uns herum zu verstehen, unser Verständnis für die Technologie zu erweitern und Kompetenzen und Fähigkeiten zu entwickeln, um neue Ideen und Innovationen umzusetzen.',
     'school_banner_title' => 'Beteiligen Sie sich!',
     'school_banner_text' => 'Sind Sie Lehrerin oder Lehrer?',
     'school_banner_text2' => 'Klicken Sie hier, um herauszufinden, wie Sie sich beteiligen können!',
