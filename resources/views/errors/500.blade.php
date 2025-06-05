45740626cfb315a57d132a114f68f16f2f8367d7
payme <siroumi@matrixinternet.ie>
2025-06-05 08:55:43 +0100

    Mobile responsiveness

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -5,7 +5,8 @@
     <section id="codeweek-error-page">
         <div class="error-container">
             <div class="error-robot">
-                <img src="{{ asset('images/404_robot.svg') }}" alt="Error Robot">
+                <img src="{{ asset('images/404_robot.svg') }}" alt="Error Robot"  class="desktop-robot">
+                <img src="{{ asset('images/404_robot_mobile.svg') }}" alt="Error Robot Mobile" class="mobile-robot">
             </div>
             <div class="error-box">
                 <h1>Error!</h1>
@@ -13,6 +14,12 @@
                 <a href="/">Go back to homepage</a>
             </div>
         </div>
-        <div class="footer-ellipse"></div>
+        <div class="footer-ellipse" style="
+            background: url('{{ asset('images/cream_ellipse.svg') }}');
+            background-position: top center;
+            background-repeat: no-repeat;
+            background-size: cover;">
+        </div>
+
     </section>
 @endsection
