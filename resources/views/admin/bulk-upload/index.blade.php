b3f590b364b971b09e865498e34faed2c71c5a76
bernardhanna <bernard@matrixinternet.ie>
2026-02-03 13:10:12 +0000

    upload feature for bulk upload

--- resources/views/admin/bulk-upload/index.blade.php
+++ resources/views/admin/bulk-upload/index.blade.php
@@ -15,6 +15,12 @@
                 </div>
             @endif
 
+            @if (session('info'))
+                <div class="mb-4 p-4 rounded bg-blue-50 border border-blue-200 text-blue-800">
+                    {{ session('info') }}
+                </div>
+            @endif
+
             @if ($errors->any())
                 <div class="mb-4 p-4 rounded bg-red-50 border border-red-200">
                     <ul class="list-disc list-inside text-red-700">
