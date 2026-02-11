fe386958766ec3f744c57da513191aabf37eccda
bernardhanna <bernard@matrixinternet.ie>
2026-02-11 14:10:49 +0000

    dynamic GID

--- app/Http/View/Composers/GirlsInDigitalComposer.php
+++ app/Http/View/Composers/GirlsInDigitalComposer.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\View\Composers;
 
-use App\GirlsInDigitalButton;
 use App\GirlsInDigitalPage;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\View\View;
@@ -11,11 +10,7 @@ class GirlsInDigitalComposer
 {
     public function compose(View $view): void
     {
-        $buttons = collect();
-        if (Schema::hasTable('girls_in_digital_buttons')) {
-            $buttons = GirlsInDigitalButton::enabled()->ordered()->get();
-        }
         $page = Schema::hasTable('girls_in_digital_page') ? GirlsInDigitalPage::config() : null;
-        $view->with(compact('buttons', 'page'));
+        $view->with(compact('page'));
     }
 }
