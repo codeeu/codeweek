03878fbd7974d9a8c47ca4b862f4794812d94d05
bernardhanna <bernard@matrixinternet.ie>
2026-02-16 13:39:41 +0000

    leaflet

--- resources/views/_tookits.blade.php
+++ resources/views/_tookits.blade.php
@@ -14,10 +14,10 @@
 {{--        @endforeach--}}
 
 {{--        @if($locale !== 'en')--}}
-{{--            - <a href="https://codeweek-s3.s3-eu-west-1.amazonaws.com/docs/toolkits/2023/communications-toolkit-2023-EN.zip">@lang('base.languages.en')</a>--}}
+{{--            - <a href="https://codeweek-s3.s3-eu-west-1.amazonaws.com/docs/toolkits/2023/communications-toolkit-2023.zip">@lang('base.languages.en')</a>--}}
 {{--        @endif--}}
 
-       {{--  <a href="https://codeweek-s3.s3-eu-west-1.amazonaws.com/docs/toolkits/2025/communications-toolkit-2025-{{strtoupper($lang)}}.zip">@lang('base.languages.en')</a>--}}
+       {{--  <a href="https://codeweek-s3.s3-eu-west-1.amazonaws.com/docs/toolkits/communications-toolkit-{{strtoupper($lang)}}.zip">@lang('base.languages.en')</a>--}}
 
         <a class="text-center no-underline w-full lg:w-fit bg-[#F95C22] rounded-full py-2.5 px-6 font-['Blinker'] hover:bg-hover-orange duration-300" href="https://codeweek-s3.s3-eu-west-1.amazonaws.com/docs/toolkits/communications-toolkit.zip">
             <span class="text-base font-semibold leading-7 text-black normal-case">
@@ -42,14 +42,14 @@
 
         @if($locale !== 'en')
             -
-            <a class="text-center no-underline w-full lg:w-fit bg-[#F95C22] rounded-full py-2.5 px-6 font-['Blinker'] hover:bg-hover-orange duration-300" href="https://codeweek-s3.s3-eu-west-1.amazonaws.com/docs/toolkits/2025/teachers-toolkit-2025-EN.zip">
+            <a class="text-center no-underline w-full lg:w-fit bg-[#F95C22] rounded-full py-2.5 px-6 font-['Blinker'] hover:bg-hover-orange duration-300" href="https://codeweek-s3.s3-eu-west-1.amazonaws.com/docs/toolkits/teachers-toolkit.zip">
             <span class="text-base font-semibold leading-7 text-black normal-case">
                 @lang('base.languages.en')
             </span>
             </a>
         @endif
 
-{{--        <a href="https://codeweek-s3.s3-eu-west-1.amazonaws.com/docs/toolkits/2023/teachers-toolkit-2023-EN.zip">@lang('base.languages.en')</a>--}}
+{{--        <a href="https://codeweek-s3.s3-eu-west-1.amazonaws.com/docs/toolkits/2023/teachers-toolkit-2023.zip">@lang('base.languages.en')</a>--}}
 
     </div>
 
@@ -69,7 +69,7 @@
             <a class="text-center no-underline w-full lg:w-fit bg-[#F95C22] rounded-full py-2.5 px-6 font-['Blinker'] hover:bg-hover-orange duration-300" href="https://codeweek-s3.s3.eu-west-1.amazonaws.com/docs/leaflet/codeweek-leaflet.pdf"><span class="text-base font-semibold leading-7 text-black normal-case">@lang('base.languages.en')</span></a>
         @endif
 
-{{--                    <a href="https://codeweek-s3.s3.eu-west-1.amazonaws.com/docs/leaflet/2024/codeweek-leaflet-2024-EN.pdf">@lang('base.languages.en')</a>--}}
+{{--                    <a href="https://codeweek-s3.s3.eu-west-1.amazonaws.com/docs/leaflet/2024/codeweek-leaflet-2024.pdf">@lang('base.languages.en')</a>--}}
 
     </div>
 
